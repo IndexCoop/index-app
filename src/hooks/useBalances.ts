7de5ef777305dc11cee2602497b0214212f75a8f
Zachary Parrish <zachary@parrish.io>
2022-01-17 16:55:57 -0500

    adding useBalances hook for getting user balances from our tokens

---
+++ src/hooks/useBalances.ts
@@ -0,0 +1,84 @@
+import {
+  ChainId,
+  useEtherBalance,
+  useEthers,
+  useTokenBalance,
+} from '@usedapp/core'
+
+import {
+  BedIndex,
+  Bitcoin2xFlexibleLeverageIndex,
+  DataIndex,
+  DefiPulseIndex,
+  Ethereum2xFlexibleLeverageIndex,
+  Ethereum2xFLIP,
+  GmiIndex,
+  MetaverseIndex,
+  ProductToken,
+} from 'constants/productTokens'
+import { MAINNET_CHAIN_DATA, POLYGON_CHAIN_DATA } from 'utils/connectors'
+
+const getChainAddress = (
+  token: ProductToken,
+  chainId: ChainId = MAINNET_CHAIN_DATA.chainId
+) => {
+  if (chainId === POLYGON_CHAIN_DATA.chainId) return token.polygonAddress
+  return token.address
+}
+
+export const useBalances = () => {
+  const { account, chainId } = useEthers()
+
+  const daiBalance = useTokenBalance(
+    getChainAddress(DefiPulseIndex, chainId),
+    account
+  )
+  const usdcBalance = useTokenBalance(
+    getChainAddress(DefiPulseIndex, chainId),
+    account
+  )
+  const dpiBalance = useTokenBalance(
+    getChainAddress(DefiPulseIndex, chainId),
+    account
+  )
+  const mviBalance = useTokenBalance(
+    getChainAddress(MetaverseIndex, chainId),
+    account
+  )
+  const bedBalance = useTokenBalance(
+    getChainAddress(BedIndex, chainId),
+    account
+  )
+  const dataBalance = useTokenBalance(
+    getChainAddress(DataIndex, chainId),
+    account
+  )
+  const gmiBalance = useTokenBalance(
+    getChainAddress(GmiIndex, chainId),
+    account
+  )
+  const ethFliBalance = useTokenBalance(
+    getChainAddress(Ethereum2xFlexibleLeverageIndex, chainId),
+    account
+  )
+  const btcFliBalance = useTokenBalance(
+    getChainAddress(Bitcoin2xFlexibleLeverageIndex, chainId),
+    account
+  )
+  const ethFliPBalance = useTokenBalance(
+    getChainAddress(Ethereum2xFLIP, chainId),
+    account
+  )
+  return {
+    daiBalance,
+    usdcBalance,
+    dpiBalance,
+    mviBalance,
+    bedBalance,
+    dataBalance,
+    gmiBalance,
+    ethFliBalance,
+    btcFliBalance,
+    ethFliPBalance,
+  }
+}
