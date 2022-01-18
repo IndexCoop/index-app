7dee0843dc2616593591d00aed87ae27f42f11d4
Zachary Parrish <zachary@parrish.io>
2022-01-18 09:00:22 -0500

    fixing build issues

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react'
-
 import { Button } from 'styles/button'
+
+import { extendTheme, ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
