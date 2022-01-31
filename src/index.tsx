a899f8a0cbb44ab06760cce6c10958b171e5bc09
Zachary Parrish <zachary@parrish.io>
2022-01-31 16:29:06 -0500

    dammit, it was hash routing...

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import App from 'App'
 import theme from 'theme'
