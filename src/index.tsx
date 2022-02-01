38bafa45efdbadf354999880c8851e485d739e5d
Zachary Parrish <zachary@parrish.io>
2022-02-01 09:16:43 -0500

    removing hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import App from 'App'
 import theme from 'theme'
