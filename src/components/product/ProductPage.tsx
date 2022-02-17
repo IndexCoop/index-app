d27f3d0dc07ad9da297049d4d0f60dcdee74180b
JD <jann.driessen@gmx.de>
2022-02-17 10:12:18 +0100

    Add token supply

--- src/components/product/ProductPage.tsx
+++ src/components/product/ProductPage.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react'
+
 import { Box, Flex } from '@chakra-ui/react'
+import { BigNumber } from '@ethersproject/bignumber'
+import { useEthers } from '@usedapp/core'
 
 import Page from 'components/Page'
 import { getPriceChartData } from 'components/product/PriceChartData'
@@ -9,6 +13,7 @@ import {
 } from 'providers/MarketData/MarketDataProvider'
 import { SetComponent } from 'providers/SetComponents/SetComponentsProvider'
 import { getPricesChanges } from 'utils/priceChange'
+import { getTokenSupply } from 'utils/setjsApi'
 
 import MarketChart from './MarketChart'
 import ProductComponentsTable from './ProductComponentsTable'
@@ -18,12 +23,13 @@ import ProductStats, { ProductStat } from './ProductStats'
 
 function getStatsForToken(
   tokenData: ProductToken,
-  marketData: TokenMarketDataValues
+  marketData: TokenMarketDataValues,
+  currentSupply: string
 ): ProductStat[] {
   return [
     { title: 'Market Cap', value: '' },
     { title: 'Volume', value: '' },
-    { title: 'Current Supply', value: '' },
+    { title: 'Current Supply', value: currentSupply },
     { title: 'Streaming Fee', value: tokenData.fees?.streamingFee ?? 'n/a' },
   ]
 }
@@ -34,7 +40,33 @@ const ProductPage = (props: {
   components: SetComponent[]
 }) => {
   const { marketData, tokenData } = props
+
+  const { chainId, library } = useEthers()
   const { selectLatestMarketData } = useMarketData()
+
+  const [currentTokenSupply, setCurrentTokenSupply] = useState('...')
+
+  useEffect(() => {
+    const tokenAddress = tokenData.address
+
+    if (
+      tokenAddress === undefined ||
+      library === undefined ||
+      chainId === undefined
+    ) {
+      setCurrentTokenSupply('n/a')
+      return
+    }
+
+    const fetchSupply = async () => {
+      const setDetails = await getTokenSupply(library, [tokenAddress], chainId)
+      const e18 = BigNumber.from('1000000000000000000')
+      const supply = setDetails[0].totalSupply.div(e18).toLocaleString()
+      setCurrentTokenSupply(supply)
+    }
+
+    fetchSupply()
+  }, [tokenData])
 
   const priceChartData = getPriceChartData([marketData])
 
@@ -49,7 +81,7 @@ const ProductPage = (props: {
     )} ( ${plusOrMinus} ${change.rel.toFixed(2)}% )`
   })
 
-  const stats = getStatsForToken(tokenData, marketData)
+  const stats = getStatsForToken(tokenData, marketData, currentTokenSupply)
 
   // TODO: find a way to dynamically capture the page's width so it can be passed
   // to the chart (which does not take dynamic values) - same on dashboard
