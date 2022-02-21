844b9bcbbd8648694de1975f0aea8bc1303241a4
JD <jann.driessen@gmx.de>
2022-02-21 09:29:10 +0100

    Update background to gradient

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,7 +2,8 @@ import { colors } from 'styles/colors'
 
 export const global = {
   body: {
-    bg: colors.background,
+    bg: '#16141E',
+    bgGradient: 'linear(to-b, #16141E, #050233)',
     color: colors.icWhite,
   },
 }
