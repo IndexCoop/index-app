626bee2ea4b9183ec76e8e1a8b13b4038397c8b2
JD <jann.driessen@gmx.de>
2022-02-21 11:32:49 +0100

    Animation navigation links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,41 @@
-import { Flex, Link, Text } from '@chakra-ui/layout'
+import { Box, Flex, Link, Text } from '@chakra-ui/layout'
 
 const NavLink = (props: { href: string; linkText: string }) => {
   return (
-    <Link pr='48px' href={props.href}>
-      <Text fontSize='xl' fontWeight='700'>
-        {props.linkText}
-      </Text>
-    </Link>
+    <Box pr='48px'>
+      <Link
+        display='block'
+        position='relative'
+        href={props.href}
+        _after={{
+          content: '""',
+          position: 'absolute',
+          bottom: '-4px',
+          left: '0',
+          width: ' 100%',
+          height: '0.1em',
+          backgroundColor: '#fff',
+          opacity: 0,
+          transition: 'opacity 300ms, transform 300ms',
+        }}
+        _focus={{
+          _after: {
+            opacity: 1,
+            transform: 'translate3d(0, 0.2em, 0)',
+          },
+        }}
+        _hover={{
+          _after: {
+            opacity: 1,
+            transform: 'translate3d(0, 0.2em, 0)',
+          },
+        }}
+      >
+        <Text color='#fff' fontSize='xl' fontWeight='700'>
+          {props.linkText}
+        </Text>
+      </Link>
+    </Box>
   )
 }
 
