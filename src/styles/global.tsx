9f1667fcc5ec72b03bd110d1f33247dc32309eae
JD <jann.driessen@gmx.de>
2022-02-21 10:31:25 +0100

    Update connect button styling

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,8 +2,8 @@ import { colors } from 'styles/colors'
 
 export const global = {
   body: {
-    bg: '#16141E',
-    bgGradient: 'linear(to-b, #16141E, #050233)',
+    bg: colors.background,
+    bgGradient: `linear(to-b, ${colors.background}, #050233)`,
     color: colors.icWhite,
   },
 }
