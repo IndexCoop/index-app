c43dcc7b616d66424810c271ae87e175fbdc28d4
Zachary Parrish <zachary@parrish.io>
2022-03-07 13:25:01 -0500

    method parity with contract

--- src/utils/zeroExExchangeIssuance.ts
+++ src/utils/zeroExExchangeIssuance.ts
@@ -8,36 +8,241 @@ import { getERC20Contract, preciseMul, toWei } from 'utils'
 import { EI0X_ABI } from './abi/EI0X'
 
 /**
+ * Returns transaction for the following:
+ * Issues an exact amount of SetTokens for given amount of ETH.
+ * The excess amount of tokens is returned in an equivalent amount of ether.
+ *
+ * @param library                library from logged in user
+ * @param setToken               Address of the SetToken to be issued
+ * @param amountSetToken         Amount of SetTokens to issue
+ * @param componentQuotes        The encoded 0x transactions to execute
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
+ *
+ * @return amountEthReturn       Amount of ether returned to the caller
+ */
+export const issueExactSetFromETH = async (
+  library: any,
+  setToken: string,
+  amountSetToken: BigNumber,
+  componentQuotes: any[],
+  issuanceModule: string,
+  isDebtIssuance: boolean
+): Promise<any> => {
+  console.log('issueExactSetFromETH')
+  try {
+    const eiContract = await getExchangeIssuanceContract(library.getSigner())
+    const issueSetTx = await eiContract.issueExactSetFromETH(
+      setToken,
+      amountSetToken,
+      componentQuotes,
+      issuanceModule,
+      isDebtIssuance
+    )
+    return issueSetTx
+  } catch (err) {
+    console.log('error', err)
+    return err
+  }
+}
+
+/**
+ * Returns transaction for the following:
+ * Redeems an exact amount of SetTokens for ETH.
+ * The SetToken must be approved by the sender to this contract.
+ *
+ * @param library                library from logged in user
+ * @param setToken               Address of the SetToken to be issued
+ * @param minEthReceive          Minimum amount of Eth to receive
+ * @param componentQuotes        The encoded 0x transactions to execute
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
+ *
+ * @return outputAmount          Amount of output tokens sent to the caller
+ */
+export const redeemExactSetForETH = async (
+  library: any,
+  setToken: string,
+  minEthReceive: BigNumber,
+  componentQuotes: any[],
+  issuanceModule: string,
+  isDebtIssuance: boolean
+): Promise<any> => {
+  console.log('redeemExactSetForETH')
+  try {
+    const eiContract = await getExchangeIssuanceContract(library.getSigner())
+    const redeemSetTx = await eiContract.issueExactSetFromETH(
+      setToken,
+      minEthReceive,
+      componentQuotes,
+      issuanceModule,
+      isDebtIssuance
+    )
+    return redeemSetTx
+  } catch (err) {
+    console.log('error', err)
+    return err
+  }
+}
+
+/**
+ * Returns transaction to get component & position quotes for token issuance
+ *
+ * @param library                library from logged in user
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
+ * @param setToken               Address of the SetToken to be issued
+ * @param amountSetToken         Amount of SetTokens to issue
+ *
+ * @return componenets           Array of component addresses
+ * @return positions             Array of component positions
+ */
+export const getRequiredIssuanceComponents = async (
+  library: any,
+  issuanceModule: string,
+  isDebtIssuance: boolean,
+  setToken: string,
+  amountSetToken: BigNumber
+): Promise<any> => {
+  console.log('issueExactSetFromToken')
+  // TODO: Make match 0x methods from chirstians contracts
+  try {
+    const eiContract = await getExchangeIssuanceContract(library.getSigner())
+    const issueQuoteTx = await eiContract.getRequiredIssuanceComponents(
+      issuanceModule,
+      isDebtIssuance,
+      setToken,
+      amountSetToken
+    )
+    return issueQuoteTx
+  } catch (err) {
+    console.log('error', err)
+    return err
+  }
+}
+
+/**
+ * Returns transaction for the following:
  * Issues an exact amount of SetTokens for given amount of input ERC20 tokens.
  * The excess amount of tokens is returned in an equivalent amount of ether.
  *
  * @param library                library from logged in user
- * @param buyTokenAddress        Address of the ERC20 token to buy
- * @param sellTokenAddress       Address of the ERC20 token to sell
- * @param maxAmountInput         Maximum amount of input tokens to be used to issue SetTokens.
+ * @param setToken               Address of the SetToken to be issued
+ * @param inputToken             Address of the input token
+ * @param amountSetToken         Amount of SetTokens to issue
+ * @param maxAmountInputToken    Maximum amount of input tokens to be used to issue SetTokens.
  * @param componentQuotes        The encoded 0x transactions to execute
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
  *
  * @return totalInputTokenSold   Amount of input token spent for issuance
  */
 export const issueExactSetFromToken = async (
   library: any,
-  buyTokenAddress: string,
-  sellTokenAddress: string,
-  tokenAmount: BigNumber,
-  maxAmountInput: BigNumber,
-  componentQuotes: any[]
+  setToken: string,
+  inputToken: string,
+  amountSetToken: BigNumber,
+  maxAmountInputToken: BigNumber,
+  componentQuotes: any[],
+  issuanceModule: string,
+  isDebtIssuance: boolean
 ): Promise<any> => {
   console.log('issueExactSetFromToken')
   // TODO: Make match 0x methods from chirstians contracts
   try {
     const eiContract = await getExchangeIssuanceContract(library.getSigner())
     const issueSetTx = await eiContract.issueExactSetFromToken(
-      buyTokenAddress,
-      sellTokenAddress,
-      tokenAmount,
-      maxAmountInput
+      setToken,
+      inputToken,
+      amountSetToken,
+      maxAmountInputToken,
+      componentQuotes,
+      issuanceModule,
+      isDebtIssuance
     )
     return issueSetTx
+  } catch (err) {
+    console.log('error', err)
+    return err
+  }
+}
+
+/**
+ * Returns transaction for the following:
+ * Issues an exact amount of SetTokens for given amount of input ERC20 tokens.
+ * The excess amount of tokens is returned in an equivalent amount of ether.
+ *
+ * @param library                library from logged in user
+ * @param setToken               Address of the SetToken to be issued
+ * @param outputToken            Address of the input token
+ * @param amountSetToken         Amount of SetTokens to issue
+ * @param minOutputReceive       Minimum amount of output token to receive
+ * @param componentQuotes        The encoded 0x transactions to execute
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
+ *
+ * @return outputAmount          Amount of output tokens sent to the caller
+ */
+export const redeemExactSetForToken = async (
+  library: any,
+  setToken: string,
+  outputToken: string,
+  amountSetToken: BigNumber,
+  minOutputReceive: BigNumber,
+  componentQuotes: any[],
+  issuanceModule: string,
+  isDebtIssuance: boolean
+): Promise<any> => {
+  console.log('issueExactSetFromToken')
+  // TODO: Make match 0x methods from chirstians contracts
+  try {
+    const eiContract = await getExchangeIssuanceContract(library.getSigner())
+    const redeemSetTx = await eiContract.issueExactSetFromToken(
+      setToken,
+      outputToken,
+      amountSetToken,
+      minOutputReceive,
+      componentQuotes,
+      issuanceModule,
+      isDebtIssuance
+    )
+    return redeemSetTx
+  } catch (err) {
+    console.log('error', err)
+    return err
+  }
+}
+
+/**
+ * Returns transaction to get component & position quotes for token redemption
+ *
+ * @param library                library from logged in user
+ * @param issuanceModule         Address of issuance Module to use
+ * @param isDebtIssuance         Flag indicating wether given issuance module is a debt issuance module
+ * @param setToken               Address of the SetToken to be issued
+ * @param amountSetToken         Amount of SetTokens to issue
+ *
+ * @return componenets           Array of component addresses
+ * @return positions             Array of component positions
+ */
+export const getRequiredRedemptionComponents = async (
+  library: any,
+  issuanceModule: string,
+  isDebtIssuance: boolean,
+  setToken: string,
+  amountSetToken: BigNumber
+): Promise<any> => {
+  console.log('issueExactSetFromToken')
+  // TODO: Make match 0x methods from chirstians contracts
+  try {
+    const eiContract = await getExchangeIssuanceContract(library.getSigner())
+    const redeemQuoteTx = await eiContract.getRequiredRedemptionComponents(
+      issuanceModule,
+      isDebtIssuance,
+      setToken,
+      amountSetToken
+    )
+    return redeemQuoteTx
   } catch (err) {
     console.log('error', err)
     return err
