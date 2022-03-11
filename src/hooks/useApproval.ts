3026acd2c3b63ff36d86201a20795841fe752e32
JD <jann.driessen@gmx.de>
2022-03-11 10:58:15 +0100

    Update use approval

--- src/hooks/useApproval.ts
+++ src/hooks/useApproval.ts
@@ -14,26 +14,33 @@ const ERC20Interface = new utils.Interface(ERC20_ABI)
 /**
  * Approve the spending of an ERC20
  */
-export const useApproval = (tokenAddress: string, spenderAddress: string) => {
+export const useApproval = (tokenAddress?: string, spenderAddress?: string) => {
   const { account, library } = useEthers()
   const allowance = useAllowance(tokenAddress, spenderAddress)
 
   const [isApproving, setIsApproving] = useState(false)
   const [isApproved, setIsApproved] = useState(false)
 
-  const tokenContract = new Contract(tokenAddress, ERC20Interface)
+  // TODO: needs betters solution
+  const contractAddress =
+    tokenAddress && tokenAddress.length > 0
+      ? tokenAddress
+      : '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+  const tokenContract = new Contract(contractAddress, ERC20Interface)
   const { state, send: approveSpend } = useContractFunction(
     tokenContract,
     'approve'
   )
 
-  const handleApprove = useCallback(() => {
+  const handleApprove = useCallback(async () => {
     if (!library || !account || !tokenAddress || !spenderAddress) {
       return
     }
     try {
       setIsApproving(true)
-      approveSpend(spenderAddress, ethers.constants.MaxUint256)
+      await approveSpend(spenderAddress, ethers.constants.MaxUint256)
+      setIsApproved(state.status === 'Success')
+      setIsApproving(false)
     } catch (e) {
       setIsApproving(false)
       return false
@@ -48,14 +55,9 @@ export const useApproval = (tokenAddress: string, spenderAddress: string) => {
   ])
 
   useEffect(() => {
-    if (state.status !== 'Success') {
-      setIsApproving(false)
-    }
-
-    if (allowance?.gte(minimumRequiredApprovalQuantity)) {
-      setIsApproved(true)
-    }
-  }, [state, allowance])
+    const isApproved = allowance?.gte(minimumRequiredApprovalQuantity) ?? false
+    setIsApproved(isApproved)
+  }, [allowance])
 
   return {
     isApproved,
