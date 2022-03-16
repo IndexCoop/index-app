5a8eff7e78a037eb5f34cbb6b7248425b4150622
JD <jann.driessen@gmx.de>
2022-03-16 15:12:28 +0100

    Hide chart for dashboard

--- src/components/views/Homepage.tsx
+++ src/components/views/Homepage.tsx
@@ -77,53 +77,53 @@ const Dashboard = () => {
   }))
   const pieChartPositions = getPieChartPositions(balancesPieChart)
 
-  const top4Positions = pieChartPositions
-    .filter((pos) => pos.title !== 'OTHERS')
-    .flatMap((pos) => pos.title)
-    .slice(0, 4)
+  // const top4Positions = pieChartPositions
+  //   .filter((pos) => pos.title !== 'OTHERS')
+  //   .flatMap((pos) => pos.title)
+  //   .slice(0, 4)
 
-  const allocationsChartData: TokenMarketDataValues[] = top4Positions
-    .map((positionTitle) => {
-      switch (positionTitle) {
-        case 'DPI':
-          return dpi
-        case 'MVI':
-          return mvi
-        case 'DATA':
-          return data
-        case 'BED':
-          return bed
-        case 'GMI':
-          return gmi
-        case 'ETH2x-FLI':
-          return ethfli
-        case 'ETH2x-FLI-P':
-          return ethflip
-        case 'BTC2x-FLI':
-          return btcfli
-        default:
-          return undefined
-      }
-    })
-    // Remove undefined
-    .filter((tokenData): tokenData is TokenMarketDataValues => !!tokenData)
+  // const allocationsChartData: TokenMarketDataValues[] = top4Positions
+  //   .map((positionTitle) => {
+  //     switch (positionTitle) {
+  //       case 'DPI':
+  //         return dpi
+  //       case 'MVI':
+  //         return mvi
+  //       case 'DATA':
+  //         return data
+  //       case 'BED':
+  //         return bed
+  //       case 'GMI':
+  //         return gmi
+  //       case 'ETH2x-FLI':
+  //         return ethfli
+  //       case 'ETH2x-FLI-P':
+  //         return ethflip
+  //       case 'BTC2x-FLI':
+  //         return btcfli
+  //       default:
+  //         return undefined
+  //     }
+  //   })
+  //   // Remove undefined
+  //   .filter((tokenData): tokenData is TokenMarketDataValues => !!tokenData)
 
-  const onChangeChartType = (type: number) => {
-    switch (type) {
-      case 0: {
-        const balanceData = getPriceChartData([
-          { hourlyPrices: totalHourlyPrices },
-        ])
-        setPriceChartData(balanceData)
-        break
-      }
-      case 1: {
-        const allocationsData = getPriceChartData(allocationsChartData)
-        setPriceChartData(allocationsData)
-        break
-      }
-    }
-  }
+  // const onChangeChartType = (type: number) => {
+  //   switch (type) {
+  //     case 0: {
+  //       const balanceData = getPriceChartData([
+  //         { hourlyPrices: totalHourlyPrices },
+  //       ])
+  //       setPriceChartData(balanceData)
+  //       break
+  //     }
+  //     case 1: {
+  //       const allocationsData = getPriceChartData(allocationsChartData)
+  //       setPriceChartData(allocationsData)
+  //       break
+  //     }
+  //   }
+  // }
 
   const onClickDownloadCsv = () => {
     const csv = exportCsv(historyItems, 'index')
@@ -141,29 +141,18 @@ const Dashboard = () => {
       />
     ) : undefined
 
-  const formattedPrice = `$${totalBalanceInUSD.toFixed(2).toString()}`
-  const prices = [formattedPrice]
-  const priceChangesFormatted = getFormattedChartPriceChanges(priceChanges)
+  // const formattedPrice = `$${totalBalanceInUSD.toFixed(2).toString()}`
+  // const prices = [formattedPrice]
+  // const priceChangesFormatted = getFormattedChartPriceChanges(priceChanges)
 
   const width = isWeb ? 1024 : 340
-  const chartHeight = isWeb ? 400 : 300
+  // const chartHeight = isWeb ? 400 : 300
 
   return (
     <Page>
       <Box w={width} mx='auto'>
         <PageTitle title='My Dashboard' subtitle='' />
         <Box my={12}>
-          <MarketChart
-            marketData={priceChartData}
-            prices={prices}
-            priceChanges={priceChangesFormatted}
-            options={{
-              width,
-              height: chartHeight,
-              hideYAxis: false,
-            }}
-            customSelector={<ChartTypeSelector onChange={onChangeChartType} />}
-          />
           <Flex
             direction={['column', 'row']}
             mt='64px'
@@ -194,5 +183,17 @@ const Dashboard = () => {
     </Page>
   )
 }
+
+// <MarketChart
+//   marketData={priceChartData}
+//   prices={prices}
+//   priceChanges={priceChangesFormatted}
+//   options={{
+//     width,
+//     height: chartHeight,
+//     hideYAxis: false,
+//   }}
+//   customSelector={<ChartTypeSelector onChange={onChangeChartType} />}
+// />
 
 export default Dashboard
