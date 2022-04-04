2c6f517231c0c9c10961cf339b9fd68177848ced
Zachary Parrish <zachary@parrish.io>
2022-04-04 13:49:26 -0400

    updated

--- src/hooks/useBestTradeOption.ts
+++ src/hooks/useBestTradeOption.ts
@@ -67,37 +67,6 @@ export const useBestTradeOption = () => {
   const [isFetching, setIsFetching] = useState<boolean>(false)
   const [result, setResult] = useState<Result<ZeroExData, Error> | null>(null)
 
-  /* Determines if the token is eligible for Leveraged Exchange Issuance */
-  const isEligibleLeveragedToken = (token: Token) =>
-    eligibleLeveragedExchangeIssuanceTokens.includes(token)
-
-  /* Determines if the token pair is eligible for Leveraged Exchange Issuance */
-  const isEligibleTradePair = (
-    inputToken: Token,
-    outputToken: Token,
-    isIssuance: boolean
-  ) => {
-    const tokenEligible = isIssuance
-      ? isEligibleLeveragedToken(outputToken)
-      : isEligibleLeveragedToken(inputToken)
-
-    const isIcEth =
-      inputToken.symbol === icETHIndex.symbol ||
-      outputToken.symbol === icETHIndex.symbol
-
-    if (tokenEligible && isIcEth && isIssuance) {
-      // Only ETH is allowed as input for icETH issuance at the moment
-      return inputToken.symbol === ETH.symbol
-    }
-
-    if (tokenEligible && isIcEth && !isIssuance) {
-      // Only ETH is allowed as output for icETH redeeming at the moment
-      return outputToken.symbol === ETH.symbol
-    }
-
-    return tokenEligible
-  }
-
   const fetchAndCompareOptions = async (
     sellToken: Token,
     sellTokenAmount: string,
