f2f92ec1c8151ec52ed66d3a127c9ee5d709a682
JD <jann.driessen@gmx.de>
2022-05-13 15:22:06 +0100

    Add tests for getNativeToken

--- src/utils/tokens.test.ts
+++ src/utils/tokens.test.ts
@@ -1,6 +1,6 @@
-import { getAddressForToken } from './tokens'
+import { ETH, MATIC, WETH } from 'constants/tokens'
 
-import { WETH } from 'constants/tokens'
+import { getAddressForToken, getNativeToken } from './tokens'
 
 describe('getAddressForToken()', () => {
   test('should return undefined for undefined chain', async () => {
@@ -24,5 +24,30 @@ describe('getAddressForToken()', () => {
     const address = getAddressForToken(WETH, 137)
     expect(address).toBeDefined()
     expect(address).toEqual(WETH.polygonAddress)
+  })
+})
+
+describe('getNativeToken()', () => {
+  test('should return undefined for undefined chain', async () => {
+    const nativeToken = getNativeToken(undefined)
+    expect(nativeToken).toBeNull()
+  })
+
+  test('should return correct token address for WETH on Ethereum', async () => {
+    const nativeToken = getNativeToken(1)
+    expect(nativeToken).toBeDefined()
+    expect(nativeToken).toEqual(ETH)
+  })
+
+  test('should return correct token address for WETH on Optimism', async () => {
+    const nativeToken = getNativeToken(10)
+    expect(nativeToken).toBeDefined()
+    expect(nativeToken).toEqual(ETH)
+  })
+
+  test('should return correct token address for WETH on Polygon', async () => {
+    const nativeToken = getNativeToken(137)
+    expect(nativeToken).toBeDefined()
+    expect(nativeToken).toEqual(MATIC)
   })
 })
