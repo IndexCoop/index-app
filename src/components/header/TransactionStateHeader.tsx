19864f66c94e5695ffd4e4304ccb6998fe61eaf3
JD <jann.driessen@gmx.de>
2022-05-24 13:55:51 +0200

    Add states and styling

--- src/components/header/TransactionStateHeader.tsx
+++ src/components/header/TransactionStateHeader.tsx
@@ -1,26 +1,35 @@
+import { colors } from 'styles/colors'
+
+import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons'
 import { Button, Flex, Spinner, Text } from '@chakra-ui/react'
 
-const backgroundColor = '#000'
-const textColor = '#fff'
-const fontSize = 'md'
-const fontWeight = 700
-
-// TODO: add states (pending, success, failed)
-
-type TransactionStateHeaderProps = {
-  onClick: () => void
+export enum TransactionStateHeaderState {
+  none,
+  failed,
+  pending,
+  success,
 }
 
-const TransactionStateHeader = ({ onClick }: TransactionStateHeaderProps) => (
+type TransactionStateHeaderProps = {
+  isDarkMode: boolean
+  onClick: () => void
+  state: TransactionStateHeaderState
+}
+
+const TransactionStateHeader = ({
+  isDarkMode,
+  onClick,
+  state,
+}: TransactionStateHeaderProps) => (
   <Flex>
     <Button
       onClick={onClick}
-      background={textColor}
-      borderColor={backgroundColor}
+      background={isDarkMode ? colors.background : colors.white}
+      borderColor={getBorderColor(state, isDarkMode)}
       borderRadius='32'
-      color={backgroundColor}
-      fontSize={fontSize}
-      fontWeight={fontWeight}
+      color={isDarkMode ? colors.background : colors.white}
+      fontSize='md'
+      fontWeight='700'
       padding='6px 16px'
       _hover={{
         transform:
@@ -28,9 +37,60 @@ const TransactionStateHeader = ({ onClick }: TransactionStateHeaderProps) => (
         transformStyle: 'preserve-3d',
       }}
     >
-      <Spinner size='sm' mr='16px' /> 1 Pending
+      <TransactionStateView isDarkMode={isDarkMode} state={state} />
     </Button>
   </Flex>
 )
+
+type TransactionStateViewProps = {
+  isDarkMode: boolean
+  state: TransactionStateHeaderState
+}
+
+const TransactionStateView = (props: TransactionStateViewProps) => {
+  switch (props.state) {
+    case TransactionStateHeaderState.failed:
+      return (
+        <>
+          <WarningIcon w={4} h={4} mr='1' color={colors.icRed} />
+          <Text color={colors.icRed}> Failed</Text>
+        </>
+      )
+    case TransactionStateHeaderState.pending:
+      return (
+        <>
+          <Spinner
+            size='sm'
+            mr='16px'
+            color={props.isDarkMode ? colors.white : colors.background}
+          />
+          <Text>1 Pending</Text>
+        </>
+      )
+    case TransactionStateHeaderState.success:
+      return (
+        <>
+          <CheckCircleIcon w={4} h={4} mr='1' color={colors.icMalachite} />
+          <Text color={colors.icMalachite}>Success</Text>
+        </>
+      )
+    default:
+      return <></>
+  }
+}
+
+const getBorderColor = (
+  state: TransactionStateHeaderState,
+  darkMode: boolean
+) => {
+  switch (state) {
+    case TransactionStateHeaderState.failed:
+      return colors.icRed
+    case TransactionStateHeaderState.success:
+      return colors.icMalachite
+    default:
+      return darkMode ? colors.white : colors.black
+  }
+}
 
 export default TransactionStateHeader
