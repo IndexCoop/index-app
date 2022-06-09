4714a3b0a5040bb9f0c86e6fc11024fcf5ae4bdc
JD <jann.driessen@gmx.de>
2022-06-09 08:42:31 +0200

    Extend hook account to return provider

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,9 +1,13 @@
+import { Web3Provider } from '@ethersproject/providers'
 import { useEthers } from '@usedapp/core'
 
-type Account = string | null | undefined
+type Account = {
+  account: string | null | undefined
+  provider: Web3Provider | undefined
+}
 
 // A wrapper to be able to easily exchange how we retrieve the account
-export const useAccount = (): { account: Account } => {
-  const { account } = useEthers()
-  return { account }
+export const useAccount = (): Account => {
+  const { account, library } = useEthers()
+  return { account, provider: library }
 }
