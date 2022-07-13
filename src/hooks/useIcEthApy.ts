5f967e0ef679831d4e6328c735046fcd682ce925
JD <jann.driessen@gmx.de>
2022-07-13 15:17:38 +0200

    Fix base url for fetching icETH apy

--- src/hooks/useIcEthApy.ts
+++ src/hooks/useIcEthApy.ts
@@ -2,12 +2,14 @@ import { useCallback, useEffect, useState } from 'react'
 
 import { BigNumber } from '@ethersproject/bignumber'
 
+import { IndexApiBaseUrl } from 'constants/server'
+
 export const useIcEthApy = (): { apy: BigNumber } => {
   const [apy, setApy] = useState(BigNumber.from(0))
 
   const fetchApy = useCallback(async () => {
     try {
-      const resp = await fetch('https://api.indexcoop.com/iceth/apy', {
+      const resp = await fetch(`${IndexApiBaseUrl}/iceth/apy`, {
         headers: {
           Origin: 'https://app.indexcoop.com',
         },
