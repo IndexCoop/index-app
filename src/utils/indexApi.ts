dcf9e4ad5137e792af006a424db9ae6838824836
JD <jann.driessen@gmx.de>
2022-07-25 15:00:09 +0200

    Add index api

---
+++ src/utils/indexApi.ts
@@ -0,0 +1,26 @@
+import { getApiKey, IndexApiBaseUrl } from 'constants/server'
+
+export class IndexApi {
+  constructor() {}
+
+  /**
+   * Fetches path and returns a json.
+   * @returns JSON or throws error.
+   */
+  async get(path: string) {
+    console.log('GET', path)
+    try {
+      const key = getApiKey()
+      const resp = await fetch(`${IndexApiBaseUrl}${path}`, {
+        headers: {
+          'X-INDEXCOOP-API-KEY': key,
+        },
+      })
+      return resp.json()
+    } catch (error) {
+      console.log('Error fetching Index API for path', path)
+      console.log(error)
+      throw error
+    }
+  }
+}
