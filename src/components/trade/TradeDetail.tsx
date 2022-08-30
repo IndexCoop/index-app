ffca36411ecdf0942127b5d5d1c7935c5eb4ac5a
JD <jann.driessen@gmx.de>
2022-08-30 12:02:28 -0700

    Add gas fees view to header

--- src/components/trade/TradeDetail.tsx
+++ src/components/trade/TradeDetail.tsx
@@ -6,9 +6,10 @@ import {
   AccordionItem,
   AccordionPanel,
   Flex,
+  Text,
 } from '@chakra-ui/react'
 
-import { useColorStyles } from 'styles/colors'
+import { colors, useColorStyles } from 'styles/colors'
 
 export const TradeDetail = () => {
   const { styles } = useColorStyles()
@@ -17,25 +18,41 @@ export const TradeDetail = () => {
     <Flex>
       <Accordion allowToggle border={0} borderColor='transparent' w='100%'>
         <AccordionItem>
-          <h4>
-            <AccordionButton
-              borderRadius={16}
-              _expanded={{
-                bg: styles.background,
-                border: '1px solid',
-                borderColor: styles.border,
-                color: styles.text,
-              }}
-            >
-              <Flex align='center' flex='1' justify='flex-start'>
-                <InfoOutlineIcon color={styles.text} />
-              </Flex>
-              <AccordionIcon />
-            </AccordionButton>
-          </h4>
-          <AccordionPanel pb={4}></AccordionPanel>
+          {({ isExpanded }) => (
+            <>
+              <h4>
+                <AccordionButton
+                  borderRadius={16}
+                  _expanded={{
+                    bg: styles.background,
+                    border: '1px solid',
+                    borderColor: styles.border,
+                    color: styles.text,
+                  }}
+                >
+                  <Flex
+                    align='center'
+                    flex='1'
+                    justify='space-between'
+                    pr='8px'
+                  >
+                    <InfoOutlineIcon color={styles.text} />
+                    {!isExpanded && <GasFees label='0.0035 ETH' />}
+                  </Flex>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h4>
+              <AccordionPanel pb={4}></AccordionPanel>
+            </>
+          )}
         </AccordionItem>
       </Accordion>
     </Flex>
   )
 }
+
+const GasFees = ({ label }: { label: string }) => (
+  <Flex bg={colors.icGray1} borderRadius={16} fontSize='12px' px='2' py='1'>
+    <Text color={colors.icGray4}>Gas {label}</Text>
+  </Flex>
+)
