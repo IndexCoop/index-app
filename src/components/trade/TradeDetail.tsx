c3c0805377c331c3e7725fee01f5996b30d602bd
JD <jann.driessen@gmx.de>
2022-08-30 11:36:33 -0700

    Add header info icon and style expanded state

--- src/components/trade/TradeDetail.tsx
+++ src/components/trade/TradeDetail.tsx
@@ -1,30 +1,39 @@
+import { InfoOutlineIcon } from '@chakra-ui/icons'
 import {
   Accordion,
   AccordionButton,
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
-  Box,
   Flex,
 } from '@chakra-ui/react'
 
+import { useColorStyles } from 'styles/colors'
+
 export const TradeDetail = () => {
+  const { styles } = useColorStyles()
+
   return (
     <Flex>
-      <Accordion allowToggle w='100%'>
+      <Accordion allowToggle border={0} borderColor='transparent' w='100%'>
         <AccordionItem>
           <h4>
-            <AccordionButton>
-              <Box flex='1'>Section 1 title</Box>
+            <AccordionButton
+              borderRadius={16}
+              _expanded={{
+                bg: styles.background,
+                border: '1px solid',
+                borderColor: styles.border,
+                color: styles.text,
+              }}
+            >
+              <Flex align='center' flex='1' justify='flex-start'>
+                <InfoOutlineIcon color={styles.text} />
+              </Flex>
               <AccordionIcon />
             </AccordionButton>
           </h4>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
+          <AccordionPanel pb={4}></AccordionPanel>
         </AccordionItem>
       </Accordion>
     </Flex>
