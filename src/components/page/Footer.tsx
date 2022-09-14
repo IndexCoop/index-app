a60ea7d3c165fe8e63dc1e5b477bec220b6e7568
Zachary Parrish <zachary@parrish.io>
2022-09-14 15:09:20 -0400

    correcting footer text

--- src/components/page/Footer.tsx
+++ src/components/page/Footer.tsx
@@ -47,17 +47,60 @@ const Footer = () => {
         pt='30px'
         fontSize={'2xs'}
       >
-        Information is for educational and illustrative purposes only. The Index
-        Cooperative is not engaged in the business of the offer, sale or trading
-        of securities and does not provide legal, tax, or investment advice.
-        Cryptocurrencies and other digital assets are speculative and involve a
-        substantial degree of risk, including the risk of complete loss. There
-        can be no assurance that any cryptocurrency, token, coin, or other
-        crypto asset will be viable, liquid, or solvent.No Index Cooperative
-        communication is intended to imply that any digital assets are low-risk
-        or risk-free. The Index Cooperative works hard to provide accurate
-        information on this website, but cannot guarantee all content is
-        correct, complete, or updated.
+        Disclaimer: This content is for informational purposes only and is not
+        legal, tax, investment, financial, or other advice. You should not take,
+        or refrain from taking, any action based on any information contained
+        herein, or any other information that we make available at any time,
+        including blog posts, data, articles, links to third-party content,
+        discord content, news feeds, tutorials, tweets, and videos. Before you
+        make any financial, legal, technical, or other decisions, you should
+        seek independent professional advice from a licensed and qualified
+        individual in the area for which such advice would be appropriate. This
+        information is not intended to be comprehensive or address all aspects
+        of Index or its products. There is additional documentation on Index’s
+        website about the functioning of Index Coop, and its ecosystem and
+        community.
+      </Text>
+      <Text
+        w={['390px', '500px', '820px', '1024px']}
+        pt='30px'
+        fontSize={'2xs'}
+      >
+        You shall not purchase or otherwise acquire any of our token products if
+        you are: a citizen, resident (tax or otherwise), green card holder,
+        incorporated in, owned or controlled by a person or entity in, located
+        in, or have a registered office or principal place of business in the
+        U.S. (a “U.S. Person”), or if you are a person in any jurisdiction in
+        which such offer, sale, and/or purchase of any of our token products is
+        unlawful, prohibited, or unauthorized (together with U.S. Person, a
+        “Restricted Person”). The term “Restricted Person” includes, but is not
+        limited to, any natural person residing in, or any firm, company,
+        partnership, trust, corporation, entity, government, state or agency of
+        a state, or any other incorporated or unincorporated body or
+        association, association or partnership (whether or not having separate
+        legal personality) that is established and/or lawfully existing under
+        the laws of, a jurisdiction in which such offer, sale, and/or purchase
+        of any of our token products is unlawful, prohibited, or unauthorized).
+        You shall not resell or otherwise transfer any of our token products to
+        any Restricted Person. The transfer or resale of any of our token
+        products to any Restricted Person is not permitted. Click{' '}
+        <Link
+          target={'_blank'}
+          href='https://indexcoop.com/legal/tokens-restricted-for-us-persons'
+          textDecoration={'underline'}
+        >
+          here
+        </Link>{' '}
+        to view the list of Tokens Restricted for Restricted Persons. You shall
+        read the{' '}
+        <Link
+          target={'_blank'}
+          href='https://indexcoop.com/legal/terms-of-service'
+          textDecoration={'underline'}
+        >
+          Terms of Service
+        </Link>{' '}
+        and use our Website in compliance with the Terms of Service.
       </Text>
     </Flex>
   )
