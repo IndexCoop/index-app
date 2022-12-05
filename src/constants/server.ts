ccfb6975e1ef3f2970a204d51fdae4413c323a3a
JD <jann.driessen@gmx.de>
2022-12-05 12:23:20 +0100

    Update index api

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -3,3 +3,10 @@ export const IndexApiBaseUrl = 'https://api.indexcoop.com'
 export function getApiKey(): string {
   return process.env.REACT_APP_INDEX_COOP_API ?? ''
 }
+
+export function getIndexApiHeaders() {
+  const key = getApiKey()
+  return {
+    'X-INDEXCOOP-API-KEY': key,
+  }
+}
