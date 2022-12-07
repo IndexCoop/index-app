bf002679c6fcac04dbc0a4f4ab17562ecaa609e4
JD <jann.driessen@gmx.de>
2022-12-07 14:50:35 +0100

    Simplify retrieving index api key

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,14 +1,10 @@
 export const AlchemyMainnetUrl = `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_ID}`
 export const AlchemyPolygonUrl = `https://polygon-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_ID}`
 export const IndexApiBaseUrl = 'https://api.indexcoop.com'
-
-export function getApiKey(): string {
-  return process.env.REACT_APP_INDEX_COOP_API ?? ''
-}
+export const IndexApiKey = process.env.REACT_APP_INDEX_COOP_API
 
 export function getIndexApiHeaders() {
-  const key = getApiKey()
   return {
-    'X-INDEXCOOP-API-KEY': key,
+    'X-INDEXCOOP-API-KEY': IndexApiKey ?? '',
   }
 }
