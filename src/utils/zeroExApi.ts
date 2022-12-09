8906964ac6e98ae42e7bc053a372fd069684a36f
JD <jann.driessen@gmx.de>
2022-12-09 20:19:26 +0100

    Fix typo in 0x affiliate address parameter

--- src/utils/zeroExApi.ts
+++ src/utils/zeroExApi.ts
@@ -1,14 +1,16 @@
 import { ZeroExApi } from '@indexcoop/flash-mint-sdk'
 
-import { getIndexApiHeaders, IndexApiBaseUrl } from 'constants/server'
-
-const affilliateAddress = '0x37e6365d4f6aE378467b0e24c9065Ce5f06D70bF'
+import {
+  getIndexApiHeaders,
+  IndexApiBaseUrl,
+  ZeroExAffiliateAddress,
+} from 'constants/server'
 
 export function getConfiguredZeroExApi(swapPathOverride: string): ZeroExApi {
   const headers = getIndexApiHeaders()
   return new ZeroExApi(
     `${IndexApiBaseUrl}/0x`,
-    affilliateAddress,
+    ZeroExAffiliateAddress,
     headers,
     swapPathOverride
   )
