0a38736996a787d25d491e69ef38289590e66ec0
JD <jann.driessen@gmx.de>
2022-12-16 18:09:49 +0100

    Add use gas estimatooor for notional

--- src/hooks/useBestQuote/flashMintNotional.ts
+++ src/hooks/useBestQuote/flashMintNotional.ts
@@ -1,11 +1,12 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import { JsonRpcProvider } from '@ethersproject/providers'
 
+import { DefaultGasLimitFlashMintNotional } from 'constants/gas'
 import { DAI, FIXED_DAI, FIXED_USDC, Token, USDC } from 'constants/tokens'
 import { getFullCostsInUsd, getGasCostsInUsd } from 'utils/costs'
-import { getFlashMintNotionalGasEstimate } from 'utils/flashMintNotional/fmNotionalGasEstimate'
 import { getFlashMintNotionalQuote } from 'utils/flashMintNotional/fmNotionalQuote'
 import { getFlashMintNotionalTransaction } from 'utils/flashMintNotional/fmNotionalTransaction'
+import { GasEstimatooor } from 'utils/gasEstimatooor'
 import { TxSimulator } from 'utils/simulator'
 
 import { FlashMintNotionalQuote, QuoteType } from './'
@@ -41,18 +42,20 @@ export async function getEnhancedFlashMintNotionalQuote(
       provider
     )
     if (quote) {
-      // const req = await getFlashMintNotionalTransaction(
-      //   isMinting,
-      //   inputToken,
-      //   outputToken,
-      //   quote.indexTokenAmount,
-      //   quote.inputOutputTokenAmount,
-      //   quote.swapData,
-      //   slippage,
-      //   provider,
-      //   signer,
-      //   chainId
-      // )
+      const tx = await getFlashMintNotionalTransaction(
+        isMinting,
+        inputToken,
+        outputToken,
+        quote.indexTokenAmount,
+        quote.inputOutputTokenAmount,
+        quote.swapData,
+        slippage,
+        provider,
+        signer,
+        chainId
+      )
+
+      if (!tx) throw new Error('No transaction object')
 
       // if (req) {
       //   const accessKey = process.env.REACT_APP_TENDERLY_ACCESS_KEY ?? ''
@@ -60,18 +63,14 @@ export async function getEnhancedFlashMintNotionalQuote(
       //   await simulator.simulate(req)
       // }
 
-      const gasEstimate = await getFlashMintNotionalGasEstimate(
-        isMinting,
-        inputToken,
-        outputToken,
-        indexTokenAmount,
-        quote.inputOutputTokenAmount,
-        quote.swapData,
-        slippage,
-        chainId,
-        provider,
-        false // shouldn't fail for just estimating a quote
+      const defaultGasEstimate = BigNumber.from(
+        DefaultGasLimitFlashMintNotional
       )
+      const gasEstimatooor = new GasEstimatooor(signer, defaultGasEstimate)
+      // We don't want this function to fail for estimates here.
+      // A default will be returned if the tx would fail.
+      const canFail = false
+      const gasEstimate = await gasEstimatooor.estimate(tx, canFail)
       const gasCosts = gasEstimate.mul(gasPrice)
       const gasCostsInUsd = getGasCostsInUsd(gasCosts, nativeTokenPrice)
       return {
