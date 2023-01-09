0092778e7018a38dd2851dee33b7b4a8fb865d8c
JD <jann.driessen@gmx.de>
2023-01-09 14:30:33 +0100

    Add select slippage

--- src/utils/slippage.test.ts
+++ src/utils/slippage.test.ts
@@ -1,7 +1,7 @@
 import { slippageMap } from 'constants/slippage'
 import { icETHIndex, WSETH2 } from 'constants/tokens'
 
-import { getSlippageModification } from './slippage'
+import { getSlippageModification, selectSlippage } from './slippage'
 
 describe('getSlippageModification()', () => {
   it('returns null for unaltered slippage', () => {
@@ -20,5 +20,40 @@ describe('getSlippageModification()', () => {
     const symbol = WSETH2.symbol
     const slippage = getSlippageModification(symbol)
     expect(slippage).toBe(slippageMap.get(symbol))
+  })
+})
+
+describe('selectSlippage()', () => {
+  it('returns given slippage for undefined token', () => {
+    const symbol = 'MVI'
+    const slippageModified = getSlippageModification(symbol)
+    const result = selectSlippage(1, symbol)
+    expect(slippageModified).toBeNull()
+    expect(result).toBe(1)
+  })
+
+  it('returns correct slippage for token in mapping (bigger value)', () => {
+    const symbol = icETHIndex.symbol
+    const slippageModified = getSlippageModification(symbol)
+    const result = selectSlippage(1, symbol)
+    expect(slippageModified).toBe(slippageMap.get(symbol))
+    expect(result).toBe(slippageModified)
+  })
+
+  it('returns correct slippage for token in mapping (smaller value)', () => {
+    const symbol = WSETH2.symbol
+    const slippageModified = getSlippageModification(symbol)
+    const result = selectSlippage(1, symbol)
+    expect(slippageModified).toBe(slippageMap.get(symbol))
+    expect(result).toBe(slippageModified)
+  })
+
+  it('returns correct slippage when default was modified by user', () => {
+    const expectedSlippaged = 2
+    const symbol = WSETH2.symbol
+    const slippageModified = getSlippageModification(symbol)
+    const result = selectSlippage(expectedSlippaged, symbol)
+    expect(slippageModified).toBe(slippageMap.get(symbol))
+    expect(result).toBe(expectedSlippaged)
   })
 })
--- src/utils/slippage.ts
+++ src/utils/slippage.ts
@@ -1,5 +1,11 @@
-import { slippageMap } from 'constants/slippage'
+import { slippageDefault, slippageMap } from 'constants/slippage'
 
 export function getSlippageModification(tokenSymbol: string): number | null {
   return slippageMap.get(tokenSymbol) ?? null
+}
+
+export function selectSlippage(slippage: number, tokenSymbol: string): number {
+  if (slippage !== slippageDefault) return slippage
+  const slippageModified = getSlippageModification(tokenSymbol)
+  return slippageModified ?? slippage
 }
