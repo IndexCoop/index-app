46dccf15c06e5e5de88293a921f9b1ac6763d0ab
JD <jann.driessen@gmx.de>
2023-01-19 10:03:58 +0100

    Add apy hook to dseth page

--- src/views/productpages/DSETH.tsx
+++ src/views/productpages/DSETH.tsx
@@ -1,8 +1,12 @@
 import TokenPage from 'components/token-page/TokenPage'
 import { DiversifiedStakedETHIndex } from 'constants/tokens'
+import { useApy } from 'hooks/useApy'
+import { displayFromWei } from 'utils'
 
 const DSETH = () => {
-  return <TokenPage token={DiversifiedStakedETHIndex} />
+  const { apy } = useApy(DiversifiedStakedETHIndex.symbol)
+  const formattedApy = displayFromWei(apy, 2) ?? undefined
+  return <TokenPage token={DiversifiedStakedETHIndex} apy={formattedApy} />
 }
 
 export default DSETH
