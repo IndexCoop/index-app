ccb23b2ff3c34d7640176a2ae768be117d5f25d5
JD <jann.driessen@gmx.de>
2023-02-01 10:07:24 +0100

    Remove balances and transaction history from trade page

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -3,22 +3,13 @@ import { Box, Flex } from '@chakra-ui/react'
 import Page from 'components/page/Page'
 import QuickTradeContainer from 'components/trade'
 
-import BalanceHistoryTabs from './BalanceHistoryTabs'
-
 const Homepage = () => {
   return (
     <Page>
-      <Flex
-        align={'center'}
-        direction={'column'}
-        justify={'center'}
-        mx='auto'
-        w={['340px', '500px', '820px', '1024px']}
-      >
+      <Flex mx='auto'>
         <Box mb={12} w={['inherit', '500px']}>
           <QuickTradeContainer />
         </Box>
-        <BalanceHistoryTabs />
       </Flex>
     </Page>
   )
