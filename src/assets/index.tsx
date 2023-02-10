bda106a5a71b47fac947050eaaf237e167db0fe4
JD <jann.driessen@gmx.de>
2023-02-10 09:47:18 +0100

    Update logo assets and set widths and heights

---
+++ src/assets/index.tsx
@@ -0,0 +1,11 @@
+import IndexLogoBlack from 'assets/index-logo-black.svg'
+import IndexLogoFullBlack from 'assets/index-logo-full-black.svg'
+import IndexLogoFullWhite from 'assets/index-logo-full-white.svg'
+import IndexLogoWhite from 'assets/index-logo-white.svg'
+
+export {
+  IndexLogoBlack,
+  IndexLogoFullBlack,
+  IndexLogoFullWhite,
+  IndexLogoWhite,
+}
