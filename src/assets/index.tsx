7d179d01bc4adcf1725fe7b55cc7c12157113f23
JD <jann.driessen@gmx.de>
2023-02-18 12:32:49 +0100

    Update trade token lists

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -5,11 +5,11 @@ import fixedUsdcLogo from 'assets/fixed_usdc_logo.png'
 import gmiLogo from 'assets/gmilogo.png'
 import gtcEthLogo from 'assets/gtceth_logo.svg'
 import icethLogo from 'assets/icethlogo.png'
-import indexLogo from 'assets/index-token.png'
 import IndexLogoBlack from 'assets/index-logo-black.svg'
 import IndexLogoFullBlack from 'assets/index-logo-full-black.svg'
 import IndexLogoFullWhite from 'assets/index-logo-full-white.svg'
 import IndexLogoWhite from 'assets/index-logo-white.svg'
+import indexLogo from 'assets/index-token.png'
 import wseth2Logo from 'assets/wseth2_logo.svg'
 
 // Index logo
