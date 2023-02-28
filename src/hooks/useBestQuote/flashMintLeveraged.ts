cb06b58e10336b645856421bb08902500d37b0fd
JD <jann.driessen@gmx.de>
2023-02-28 08:39:45 +0100

    Update adjusted amount for iceth redeem quotes

--- src/hooks/useBestQuote/flashMintLeveraged.ts
+++ src/hooks/useBestQuote/flashMintLeveraged.ts
@@ -105,7 +105,7 @@ export async function getEnhancedFlashMintLeveragedQuote(
       if (inputToken.symbol === 'icETH' || outputToken.symbol === 'icETH') {
         adjustedQuoteAmount = isMinting
           ? inputOutputTokenAmount.mul(10001).div(10000)
-          : inputOutputTokenAmount.mul(100).div(101)
+          : inputOutputTokenAmount.mul(1000).div(1005)
       }
       const tx = await getFlashMintLeveragedTransaction(
         isMinting,
