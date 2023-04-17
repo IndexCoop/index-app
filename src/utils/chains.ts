5befaa70318df300635ba6636705239da258af04
JD <jann.driessen@gmx.de>
2023-04-17 15:42:10 +0200

    Add utility function for adding the mev blocker rpc

---
+++ src/utils/chains.ts
@@ -0,0 +1,35 @@
+interface AddEthereumChainParameter {
+  chainId: string // A 0x-prefixed hexadecimal string
+  chainName: string
+  nativeCurrency: {
+    name: string
+    symbol: string // 2-6 characters long
+    decimals: 18
+  }
+  rpcUrls: string[]
+  blockExplorerUrls?: string[]
+  iconUrls?: string[] // Currently ignored.
+}
+
+const MevBlockerParams: AddEthereumChainParameter = {
+  chainId: '0x1',
+  chainName: 'MEV Blocker (Ethereum Mainnet)',
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: ['https://rpc.mevblocker.io'],
+  blockExplorerUrls: ['https://etherscan.io'],
+}
+
+export async function addMEVProtectionChain(ethereum: any): Promise<void> {
+  try {
+    await ethereum.request({
+      method: 'wallet_addEthereumChain',
+      params: [MevBlockerParams],
+    })
+  } catch (addError: any) {
+    console.log('Error adding MEV Blocker', addError)
+  }
+}
