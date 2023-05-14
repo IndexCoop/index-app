4ff99fa24872ae73003739984a46b6609bdc7c57
test <test@gmail.com>
2023-05-14 17:25:17 +0800

    Adjust manifest

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,5 @@
+// Added as per these suggestion to fix an import error when using the ledger wagmi connector:
+// https://stackoverflow.com/questions/68316320/module-not-found-did-you-mean-js
 module.exports = function override(config, env) {
     // New config, e.g. config.plugins.push...
 
