0449b0d0257ae22feea5d7aafb7c94a3f33adcff
test <test@gmail.com>
2023-05-14 16:12:13 +0800

    Add ledger wagmi connector and react-override to fix compilation error: https://stackoverflow.com/questions/68316320/module-not-found-did-you-mean-js

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+module.exports = function override(config, env) {
+    // New config, e.g. config.plugins.push...
+
+    config.module.rules = [...config.module.rules, 
+        {
+            test: /\.m?js/,
+            resolve: {
+              fullySpecified: false
+            }
+        }
+      ]
+
+    return config
+}
