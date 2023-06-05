942b7a37fcb432159201db106c8f81415995153e
JD <jann.driessen@gmx.de>
2023-06-05 12:20:05 +0100

    remove obsolete addresses

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -1,21 +1,6 @@
-// Index Coop Mainnet Contract Addresses
-export const dpi2020StakingRewardsAddress =
-  '0x8f06FBA4684B5E0988F215a47775Bb611Af0F986'
-export const dpi2021StakingRewardsAddress =
-  '0xB93b505Ed567982E2b6756177ddD23ab5745f309'
-export const mviStakingRewardsAddress =
-  '0x5bc4249641b4bf4e37ef513f3fa5c63ecab34881'
-export const gmiStakingRewardsAddress =
-  '0xc3cC57579138f33A987E0333DF3d59B0Bbc59Fed'
 export const zeroExRouterAddress = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF'
-
-// Index Coop Optimism Addresses
-export const FlashMintPerp = '0x2b44C227d95B8FDa1c8750986d3aDfF0E67627F7'
 export const zeroExRouterOptimismAddress =
   '0xdef1abe32c034e558cdd535791643c58a13acc10'
 
-// Uniswap V2 Addresses
-export const uniswapEthDpiLpTokenAddress =
-  '0x4d5ef58aac27d99935e5b6b4a6778ff292059991'
-export const uniswapEthMviLpTokenAddress =
-  '0x4d3C5dB2C68f6859e0Cd05D080979f597DD64bff'
+// Index Coop Optimism Addresses
+export const FlashMintPerp = '0x2b44C227d95B8FDa1c8750986d3aDfF0E67627F7'
