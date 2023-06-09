731c2d43fb378f56e30db262bdf0d02b857779a9
JD <jann.driessen@gmx.de>
2023-06-09 16:56:12 +0100

    fix paths resolution for tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
 }
