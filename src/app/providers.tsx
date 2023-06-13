2d582bec8a9e18e5a2c54af5f6240ac701ffcc5b
JD <jann.driessen@gmx.de>
2023-06-13 15:37:44 +0200

    refactor providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,44 @@
 'use client'
 
+import { WagmiConfig } from 'wagmi'
+
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { GTMProvider } from '@elgorditosalsero/react-gtm-hook'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 
 import { GlobalFonts } from '@/lib/styles/fonts'
-import theme from '@/lib/styles/theme'
+import theme, { rainbowkitTheme } from '@/lib/styles/theme'
+import { initSentryEventTracking } from '@/lib/utils/api/sentry'
+import { chains, wagmiClient } from '@/lib/utils/wagmi'
 
 import '@rainbow-me/rainbowkit/styles.css'
 
+initSentryEventTracking()
+
+const rainbowKitAppInfo = {
+  appName: 'Index Coop',
+  learnMoreUrl: 'https://indexcoop.com',
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
+  const gtmParams = {
+    id: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID ?? '',
+  }
   return (
     <CacheProvider>
       <ChakraProvider theme={theme}>
         <GlobalFonts />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        {children}
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider
+            chains={chains}
+            theme={rainbowkitTheme}
+            appInfo={rainbowKitAppInfo}
+          >
+            <GTMProvider state={gtmParams}>{children}</GTMProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
       </ChakraProvider>
     </CacheProvider>
   )
--- src/app/swap/[[...path]]/providers.tsx
+++ src/app/swap/[[...path]]/providers.tsx
@@ -1,40 +1,13 @@
-import { rainbowkitTheme } from '@/lib/styles/theme'
-import { WagmiConfig } from 'wagmi'
-
-import { GTMProvider } from '@elgorditosalsero/react-gtm-hook'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-
 import { BalanceProvider } from '@/lib/providers/Balances'
 import { MarketDataProvider } from '@/lib/providers/MarketData'
 import { ProtectionProvider } from '@/lib/providers/Protection'
-import { initSentryEventTracking } from '@/lib/utils/api/sentry'
-import { chains, wagmiClient } from '@/lib/utils/wagmi'
-
-initSentryEventTracking()
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const gtmParams = {
-    id: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID ?? '',
-  }
-
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={rainbowkitTheme}
-        appInfo={{
-          appName: 'Index Coop',
-          learnMoreUrl: 'https://indexcoop.com',
-        }}
-      >
-        <MarketDataProvider>
-          <BalanceProvider>
-            <ProtectionProvider>
-              <GTMProvider state={gtmParams}>{children}</GTMProvider>
-            </ProtectionProvider>
-          </BalanceProvider>
-        </MarketDataProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <MarketDataProvider>
+      <BalanceProvider>
+        <ProtectionProvider>{children}</ProtectionProvider>
+      </BalanceProvider>
+    </MarketDataProvider>
   )
 }
