a44c135652efb14968a59f335ed0d1d63f20fb4e
JD <jann.driessen@gmx.de>
2023-06-13 10:39:49 +0200

    reactivate ledger status hook

--- src/lib/hooks/useLedgerStatus.ts
+++ src/lib/hooks/useLedgerStatus.ts
@@ -1,6 +1,5 @@
 export function useLedgerStatus() {
-  // TODO:
-  // const searchParams = new URLSearchParams(window.location.search)
-  // const isRunningInLedgerLive = searchParams.get('embedded') === 'true'
-  return { isRunningInLedgerLive: false }
+  const searchParams = new URLSearchParams(window.location.search)
+  const isRunningInLedgerLive = searchParams.get('embedded') === 'true'
+  return { isRunningInLedgerLive }
 }
