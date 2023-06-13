1cd6cb0b7bee8c899d5b281dacad62029834d1cf
JD <jann.driessen@gmx.de>
2023-06-13 15:45:54 +0200

    update ledger status hook to handle missing window on load

--- src/lib/hooks/useLedgerStatus.ts
+++ src/lib/hooks/useLedgerStatus.ts
@@ -1,4 +1,5 @@
 export function useLedgerStatus() {
+  if (!window) return { isRunningInLedgerLive: false }
   const searchParams = new URLSearchParams(window.location.search)
   const isRunningInLedgerLive = searchParams.get('embedded') === 'true'
   return { isRunningInLedgerLive }
