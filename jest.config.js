cfa36bab3a16cdb5d93a95933a17bcd131b0eae0
JD <jann.driessen@gmx.de>
2023-06-27 12:24:35 +0200

    remove obsolete imports in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig')
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
