4cac53df79e855557ea447b5913ccda90c79faf6
JD <jann.driessen@gmx.de>
2023-08-24 17:22:54 +0200

    add rewrites for previous paths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/DPI',
+        destination: '/swap',
+      },
+      {
+        source: '/icETH ',
+        destination: '/swap',
+      },
+      {
+        source: '/MVI ',
+        destination: '/swap',
+      },
+      {
+        source: '/dsETH ',
+        destination: '/swap',
+      },
+      {
+        source: '/BED ',
+        destination: '/swap',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
