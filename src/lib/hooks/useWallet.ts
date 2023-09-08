c21c76d5815ef57c79003b6d721a4c7db57403e9
JD <jann.driessen@gmx.de>
2023-09-08 15:43:58 +0200

    upgrade rainbowkit. add ethers adapter

--- src/lib/hooks/useWallet.ts
+++ src/lib/hooks/useWallet.ts
@@ -1,4 +1,8 @@
-import { useAccount, useProvider, useSigner } from 'wagmi'
+import { providers } from 'ethers'
+import { useEffect, useState } from 'react'
+import { useAccount, useNetwork } from 'wagmi'
+
+import { getEthersProvider, getEthersSigner } from '../utils/ethers-adapters'
 
 type Account = {
   address: string | null | undefined
@@ -10,8 +14,18 @@ type Account = {
 // A wrapper to be able to easily exchange how we retrieve the account
 export const useWallet = (): Account => {
   const { address } = useAccount()
-  const provider = useProvider()
-  const { data: signer } = useSigner()
+  const { chain } = useNetwork()
+  const provider = getEthersProvider({ chainId: chain?.id })
   const isConnected = !!address
+  const [signer, setSigner] = useState<providers.JsonRpcSigner | undefined>(
+    undefined
+  )
+  useEffect(() => {
+    const fetchSigner = async () => {
+      const signer = await getEthersSigner({ chainId: chain?.id })
+      setSigner(signer)
+    }
+    fetchSigner()
+  }, [chain])
   return { address, provider, signer, isConnected }
 }
