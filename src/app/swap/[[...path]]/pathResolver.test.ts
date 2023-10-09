77c62c6dea42f216e75e49762e3df8df3da3827b
JD <jann.driessen@gmx.de>
2023-10-09 11:26:06 +0100

    update path resolver to return tokens

--- src/app/swap/[[...path]]/pathResolver.test.ts
+++ src/app/swap/[[...path]]/pathResolver.test.ts
@@ -1,51 +1,60 @@
+import indexNames, { ETH, MetaverseIndex, USDC } from '@/constants/tokens'
 import { PathResolver } from './pathResolver'
 
 describe('PathResolver', () => {
-  it('returns null for path: /swap', async () => {
+  it('returns default for path: /swap', async () => {
     const pathComponents: string[] = []
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBeNull()
-    expect(resolvedPath.outputToken).toBeNull()
+    expect(resolvedPath.inputToken.symbol).toBe(ETH.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(indexNames[0].symbol)
   })
 
   it('returns an input token for path: /swap/usdc', async () => {
     const pathComponents = ['usdc']
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBe('usdc')
-    expect(resolvedPath.outputToken).toBeNull()
+    expect(resolvedPath.inputToken.symbol).toBe(USDC.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(indexNames[0].symbol)
   })
 
   it('returns an input and output token for path: /swap/usdc/mvi', async () => {
     const pathComponents = ['usdc', 'mvi']
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBe('usdc')
-    expect(resolvedPath.outputToken).toBe('mvi')
+    expect(resolvedPath.inputToken.symbol).toBe(USDC.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(MetaverseIndex.symbol)
   })
 
-  it('returns input token as null and output token for path: /swap/_/mvi', async () => {
+  it('returns default for input token for path: /swap/_/mvi', async () => {
     const pathComponents = ['_', 'mvi']
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBeNull()
-    expect(resolvedPath.outputToken).toBe('mvi')
+    expect(resolvedPath.inputToken.symbol).toBe(ETH.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(MetaverseIndex.symbol)
   })
 
-  it('returns input token and output token as null for path: /swap/eth/_', async () => {
+  it('returns default index for path: /swap/eth/_', async () => {
     const pathComponents = ['eth', '_']
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBe('eth')
-    expect(resolvedPath.outputToken).toBeNull()
+    expect(resolvedPath.inputToken.symbol).toBe(ETH.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(indexNames[0].symbol)
   })
 
-  it('returns null for longer wrong path: /swap/usdc/mvi/eth', async () => {
+  it('returns default for longer wrong path: /swap/usdc/mvi/eth', async () => {
     const pathComponents = ['usdc', 'mvi', 'eth']
     const resolver = new PathResolver()
     const resolvedPath = resolver.resolve(pathComponents)
-    expect(resolvedPath.inputToken).toBeNull()
-    expect(resolvedPath.outputToken).toBeNull()
+    expect(resolvedPath.inputToken.symbol).toBe(ETH.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(indexNames[0].symbol)
+  })
+
+  it('returns default for wrong path with two indices: /swap/ic21/mvi', async () => {
+    const pathComponents = ['ic21', 'mvi']
+    const resolver = new PathResolver()
+    const resolvedPath = resolver.resolve(pathComponents)
+    expect(resolvedPath.inputToken.symbol).toBe(ETH.symbol)
+    expect(resolvedPath.outputToken.symbol).toBe(MetaverseIndex.symbol)
   })
 })
