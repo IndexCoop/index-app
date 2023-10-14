b45cc30f1978724ad80e5e53a48ce6fee2844dd8
0xonramp <0xonramp@gmail.com>
2023-10-14 17:29:56 -0400

    Add vault cards

--- src/app/vaults/layout.tsx
+++ src/app/vaults/layout.tsx
@@ -17,13 +17,13 @@ export default function SwapLayout({
       <HeaderV2 />
       <Flex direction='column' mb='50px'>
         <Flex
-          w={['390px', '500px', '820px', '1024px']}
-          m={['0', '0 auto']}
+          maxWidth='1024px'
+          m='0 auto'
           p={[
             '100px 16px 0px 16px',
             '100px 16px 0px 16px',
-            '128px 0 0 0',
-            '128px 0 0 0',
+            '128px 16px 0px 16px',
+            '128px 16px 0px 16px',
           ]}
         >
           {children}
