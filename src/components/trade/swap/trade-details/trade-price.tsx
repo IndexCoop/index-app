aebee45a398dee25cb7d578fb1b78d9a2e5b9cdf
JD <jann.driessen@gmx.de>
2023-10-20 11:33:13 -0400

    update trade price styling

--- src/components/trade/swap/trade-details/trade-price.tsx
+++ src/components/trade/swap/trade-details/trade-price.tsx
@@ -1,20 +1,24 @@
-import { useColorStyles } from '@/lib/styles/colors'
-
 import { Flex, Text } from '@chakra-ui/react'
 
-type TradePriceProps = {
+import { colors } from '@/lib/styles/colors'
+
+interface TradePriceProps {
   comparisonLabel: string
   usdLabel: string
 }
 
 export const TradePrice = ({ comparisonLabel, usdLabel }: TradePriceProps) => {
-  const { styles } = useColorStyles()
   return (
     <Flex align='flex-start' direction={['column', 'row']}>
-      <Text color={styles.text} fontSize='12px'>
+      <Text fontSize='12px' fontWeight={500} textColor={colors.icGray3}>
         {comparisonLabel}
       </Text>
-      <Text color={styles.text2} fontSize='12px' ml={[0, '4px']}>
+      <Text
+        fontSize='12px'
+        fontWeight={500}
+        textColor={colors.icGray2}
+        ml={[0, '4px']}
+      >
         {usdLabel}
       </Text>
     </Flex>
