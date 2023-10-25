fbf96a0d31cb0093de0f538b9b44538a94dc8a2f
JD <jann.driessen@gmx.de>
2023-10-25 14:27:33 -0400

    add tests for determining index token amount

---
+++ src/lib/hooks/use-best-quote/index-token-amount.ts
@@ -0,0 +1,37 @@
+import { BigNumber } from '@ethersproject/bignumber'
+
+import { toWei } from '@/lib/utils'
+import { ZeroExData } from '@/lib/utils/api/zeroex-utils'
+
+// To determine if price impact for DEX is smaller 5%
+export const maxPriceImpact = 5
+
+export const getIndexTokenAmount = (
+  isIssuance: boolean,
+  sellTokenAmount: string,
+  sellTokenDecimals: number,
+  sellTokenPrice: number,
+  buyTokenPrice: number,
+  dexSwapOption: ZeroExData | null
+): BigNumber => {
+  if (!isIssuance) {
+    return toWei(sellTokenAmount, sellTokenDecimals)
+  }
+
+  let setTokenAmount = BigNumber.from(dexSwapOption?.buyAmount ?? '0')
+
+  const priceImpact =
+    dexSwapOption && dexSwapOption.estimatedPriceImpact
+      ? parseFloat(dexSwapOption.estimatedPriceImpact)
+      : 0
+
+  if (!dexSwapOption || priceImpact >= maxPriceImpact) {
+    // Recalculate the exchange issue/redeem quotes if not enough DEX liquidity
+    const sellTokenTotal = parseFloat(sellTokenAmount) * sellTokenPrice
+    const approxOutputAmount =
+      buyTokenPrice === 0 ? 0 : Math.floor(sellTokenTotal / buyTokenPrice)
+    setTokenAmount = toWei(approxOutputAmount, sellTokenDecimals)
+  }
+
+  return setTokenAmount
+}
