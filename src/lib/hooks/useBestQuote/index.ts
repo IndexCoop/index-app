217e10f42674dc0336a5c1ec567862e68fc999e4
JD <jann.driessen@gmx.de>
2023-11-02 08:01:55 -0400

    update rfq quoting

--- src/lib/hooks/useBestQuote/index.ts
+++ src/lib/hooks/useBestQuote/index.ts
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import { PopulatedTransaction } from 'ethers'
 import { Hex } from 'viem'
 
 import { BigNumber } from '@ethersproject/bignumber'
 
-import { Token } from '@/constants/tokens'
+import { ic21, Token } from '@/constants/tokens'
 import { useNetwork } from '@/lib/hooks/useNetwork'
 import { useBalanceData } from '@/lib/providers/Balances'
 import { toWei } from '@/lib/utils'
@@ -139,188 +139,191 @@ export const useBestQuote = () => {
    *
    * @param slippage   The max acceptable slippage, e.g. 3 for 3 %
    */
-  const fetchAndCompareOptions = async (
-    sellToken: Token,
-    sellTokenAmount: string,
-    sellTokenPrice: number,
-    buyToken: Token,
-    // buyTokenAmount: string,
-    buyTokenPrice: number,
-    nativeTokenPrice: number,
-    isMinting: boolean,
-    slippage: number
-  ) => {
-    const inputTokenAddress = getAddressForToken(sellToken, chainId)
-    const outputTokenAddress = getAddressForToken(buyToken, chainId)
+  const fetchAndCompareOptions = useCallback(
+    async (
+      sellToken: Token,
+      sellTokenAmount: string,
+      sellTokenPrice: number,
+      buyToken: Token,
+      // buyTokenAmount: string,
+      buyTokenPrice: number,
+      nativeTokenPrice: number,
+      isMinting: boolean,
+      slippage: number
+    ) => {
+      const inputTokenAddress = getAddressForToken(sellToken, chainId)
+      const outputTokenAddress = getAddressForToken(buyToken, chainId)
 
-    if (!provider || !chainId) {
-      console.error('Error fetching quotes - no provider or chain id present')
-      return
-    }
+      if (!provider || !chainId) {
+        console.error('Error fetching quotes - no provider or chain id present')
+        return
+      }
 
-    if (!inputTokenAddress || !outputTokenAddress) {
-      console.log(inputTokenAddress, outputTokenAddress)
-      console.error('Error can not determine input/ouput token address')
-      return
-    }
+      if (!inputTokenAddress || !outputTokenAddress) {
+        console.log(inputTokenAddress, outputTokenAddress)
+        console.error('Error can not determine input/ouput token address')
+        return
+      }
 
-    setIsFetching(true)
+      setIsFetching(true)
 
-    let rfq: RequestForQuote | null = null
+      let rfq: RequestForQuote | null = null
 
-    // if (buyToken.symbol === ic21.symbol || sellToken.symbol === ic21.symbol) {
-    //   rfq = {
-    //     takerAddress: address!,
-    //   }
-    // }
-
-    const slippagePercentage = slippage / 100
-    /* Check 0x for DEX Swap option*/
-    const zeroExResult = await getZeroExTradeData(
-      // for now we only allow selling
-      true,
-      sellToken,
-      buyToken,
-      // for now we only allow specifing selling amount,
-      // so sell token amount will always be correct
-      sellTokenAmount,
-      slippagePercentage,
-      chainId,
-      false,
-      rfq
-    )
-    const dexSwapOption = zeroExResult.success ? zeroExResult.value : null
-    const dexSwapError = zeroExResult.success
-      ? null
-      : new Error('Not enough liqiuidity available for trade.')
-    const gasLimit0x = BigNumber.from(dexSwapOption?.gas ?? '0')
-    const gasPrice0x = BigNumber.from(dexSwapOption?.gasPrice ?? '0')
-    const gas0x = gasPrice0x.mul(gasLimit0x)
-    const sellTokenAmountInWei = toWei(sellTokenAmount, sellToken.decimals)
-    const gasCostsInUsd = getGasCostsInUsd(gas0x, nativeTokenPrice)
-    const zeroExQuote: ZeroExQuote | null = dexSwapOption
-      ? {
-          type: QuoteType.zeroEx,
-          isMinting,
-          inputToken: sellToken,
-          outputToken: buyToken,
-          gas: gasLimit0x,
-          gasPrice: gasPrice0x,
-          gasCosts: gas0x,
-          gasCostsInUsd,
-          fullCostsInUsd: getFullCostsInUsd(
-            toWei(sellTokenAmount, sellToken.decimals),
-            gas0x,
-            sellToken.decimals,
-            sellTokenPrice,
-            nativeTokenPrice
-          ),
-          priceImpact: parseFloat(dexSwapOption.estimatedPriceImpact ?? '5'),
-          indexTokenAmount: isMinting
-            ? BigNumber.from(dexSwapOption.buyAmount)
-            : sellTokenAmountInWei,
-          inputOutputTokenAmount: isMinting
-            ? sellTokenAmountInWei
-            : BigNumber.from(dexSwapOption.buyAmount),
-          // type specific properties
-          chainId: dexSwapOption.chainId,
-          data: dexSwapOption.data,
-          minOutput: dexSwapOption.minOutput,
-          sources: dexSwapOption.sources,
-          to: dexSwapOption.to,
-          value: dexSwapOption.value,
+      if (buyToken.symbol === ic21.symbol || sellToken.symbol === ic21.symbol) {
+        rfq = {
+          takerAddress: signer._address,
         }
-      : null
+      }
 
-    const fetchAndCompareMoreOptions = async () => {
-      setIsFetchingMoreOptions(true)
-
-      /* Determine Set token amount based on different factors */
-      let indexTokenAmount = getSetTokenAmount(
-        isMinting,
-        sellTokenAmount,
-        sellToken.decimals,
-        sellTokenPrice,
-        buyTokenPrice,
-        dexSwapOption
-      )
-
-      const gasStation = new GasStation(provider)
-      const gasPrice = await gasStation.getGasPrice()
-
-      const flashMintQuote = await getEnhancedFlashMintQuote(
-        isMinting,
-        inputTokenAddress,
-        outputTokenAddress,
+      const slippagePercentage = slippage / 100
+      /* Check 0x for DEX Swap option*/
+      const zeroExResult = await getZeroExTradeData(
+        // for now we only allow selling
+        true,
         sellToken,
         buyToken,
-        indexTokenAmount,
-        sellTokenPrice,
-        nativeTokenPrice,
-        gasPrice,
-        slippage,
+        // for now we only allow specifing selling amount,
+        // so sell token amount will always be correct
+        sellTokenAmount,
+        slippagePercentage,
         chainId,
-        provider,
-        signer
+        false,
+        rfq
       )
+      const dexSwapOption = zeroExResult.success ? zeroExResult.value : null
+      const dexSwapError = zeroExResult.success
+        ? null
+        : new Error('Not enough liqiuidity available for trade.')
+      const gasLimit0x = BigNumber.from(dexSwapOption?.gas ?? '0')
+      const gasPrice0x = BigNumber.from(dexSwapOption?.gasPrice ?? '0')
+      const gas0x = gasPrice0x.mul(gasLimit0x)
+      const sellTokenAmountInWei = toWei(sellTokenAmount, sellToken.decimals)
+      const gasCostsInUsd = getGasCostsInUsd(gas0x, nativeTokenPrice)
+      const zeroExQuote: ZeroExQuote | null = dexSwapOption
+        ? {
+            type: QuoteType.zeroEx,
+            isMinting,
+            inputToken: sellToken,
+            outputToken: buyToken,
+            gas: gasLimit0x,
+            gasPrice: gasPrice0x,
+            gasCosts: gas0x,
+            gasCostsInUsd,
+            fullCostsInUsd: getFullCostsInUsd(
+              toWei(sellTokenAmount, sellToken.decimals),
+              gas0x,
+              sellToken.decimals,
+              sellTokenPrice,
+              nativeTokenPrice
+            ),
+            priceImpact: parseFloat(dexSwapOption.estimatedPriceImpact ?? '5'),
+            indexTokenAmount: isMinting
+              ? BigNumber.from(dexSwapOption.buyAmount)
+              : sellTokenAmountInWei,
+            inputOutputTokenAmount: isMinting
+              ? sellTokenAmountInWei
+              : BigNumber.from(dexSwapOption.buyAmount),
+            // type specific properties
+            chainId: dexSwapOption.chainId,
+            data: dexSwapOption.data,
+            minOutput: dexSwapOption.minOutput,
+            sources: dexSwapOption.sources,
+            to: dexSwapOption.to,
+            value: dexSwapOption.value,
+          }
+        : null
 
-      console.log('////////')
-      console.log('exchangeIssuanceZeroExQuote', flashMintQuote)
+      const fetchAndCompareMoreOptions = async () => {
+        setIsFetchingMoreOptions(true)
 
-      const bestQuote = getBestQuote(
-        zeroExQuote?.fullCostsInUsd ?? null,
-        flashMintQuote?.fullCostsInUsd ?? null,
-        zeroExQuote?.priceImpact ?? 5
-      )
+        /* Determine Set token amount based on different factors */
+        let indexTokenAmount = getSetTokenAmount(
+          isMinting,
+          sellTokenAmount,
+          sellToken.decimals,
+          sellTokenPrice,
+          buyTokenPrice,
+          dexSwapOption
+        )
 
-      const getSavings = (): number => {
-        if (!zeroExQuote) return 0
-        if (bestQuote.type === QuoteType.flashMint && flashMintQuote) {
-          return (
-            (zeroExQuote.fullCostsInUsd ?? 0) -
-            (flashMintQuote.fullCostsInUsd ?? 0)
-          )
+        const gasStation = new GasStation(provider)
+        const gasPrice = await gasStation.getGasPrice()
+
+        const flashMintQuote = await getEnhancedFlashMintQuote(
+          isMinting,
+          inputTokenAddress,
+          outputTokenAddress,
+          sellToken,
+          buyToken,
+          indexTokenAmount,
+          sellTokenPrice,
+          nativeTokenPrice,
+          gasPrice,
+          slippage,
+          chainId,
+          provider,
+          signer
+        )
+
+        console.log('////////')
+        console.log('exchangeIssuanceZeroExQuote', flashMintQuote)
+
+        const bestQuote = getBestQuote(
+          zeroExQuote?.fullCostsInUsd ?? null,
+          flashMintQuote?.fullCostsInUsd ?? null,
+          zeroExQuote?.priceImpact ?? 5
+        )
+
+        const getSavings = (): number => {
+          if (!zeroExQuote) return 0
+          if (bestQuote.type === QuoteType.flashMint && flashMintQuote) {
+            return (
+              (zeroExQuote.fullCostsInUsd ?? 0) -
+              (flashMintQuote.fullCostsInUsd ?? 0)
+            )
+          }
+          return 0
         }
-        return 0
+
+        const flashMintIsBestQuote = bestQuote.type === QuoteType.flashMint
+        const savingsUsd = getSavings()
+        console.log(
+          flashMintIsBestQuote,
+          savingsUsd,
+          flashMintQuote?.fullCostsInUsd,
+          zeroExQuote?.fullCostsInUsd,
+          'isBestQuote/savings'
+        )
+
+        const quoteResult: MoreQuotesResult = {
+          hasBetterQuote: flashMintIsBestQuote,
+          isReasonPriceImpact: bestQuote.priceImpact,
+          quotes: {
+            flashMint: flashMintIsBestQuote ? flashMintQuote : null,
+          },
+          savingsUsd,
+        }
+
+        setQuoteResultOptions(quoteResult)
+        setIsFetchingMoreOptions(false)
       }
 
-      const flashMintIsBestQuote = bestQuote.type === QuoteType.flashMint
-      const savingsUsd = getSavings()
-      console.log(
-        flashMintIsBestQuote,
-        savingsUsd,
-        flashMintQuote?.fullCostsInUsd,
-        zeroExQuote?.fullCostsInUsd,
-        'isBestQuote/savings'
-      )
+      console.log('FETCH MORE...')
+      // The individual Flash Mint functions will check if the the token pair is eligible
+      fetchAndCompareMoreOptions()
 
-      const quoteResult: MoreQuotesResult = {
-        hasBetterQuote: flashMintIsBestQuote,
-        isReasonPriceImpact: bestQuote.priceImpact,
+      const quoteResult: QuoteResult = {
+        error: dexSwapError,
         quotes: {
-          flashMint: flashMintIsBestQuote ? flashMintQuote : null,
+          zeroEx: zeroExQuote,
         },
-        savingsUsd,
       }
 
-      setQuoteResultOptions(quoteResult)
-      setIsFetchingMoreOptions(false)
-    }
-
-    console.log('FETCH MORE...')
-    // The individual Flash Mint functions will check if the the token pair is eligible
-    fetchAndCompareMoreOptions()
-
-    const quoteResult: QuoteResult = {
-      error: dexSwapError,
-      quotes: {
-        zeroEx: zeroExQuote,
-      },
-    }
-
-    setQuoteResult(quoteResult)
-    setIsFetching(false)
-  }
+      setQuoteResult(quoteResult)
+      setIsFetching(false)
+    },
+    [chainId, provider, signer]
+  )
 
   return {
     fetchAndCompareOptions,
