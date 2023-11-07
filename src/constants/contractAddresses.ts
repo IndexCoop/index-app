c048800263bebdb9ec6fc4788ee75f8525e43c42
JD <jann.driessen@gmx.de>
2023-11-07 03:44:38 -0500

    remove obsolete address

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -1,6 +1,3 @@
 export const zeroExRouterAddress = '0xDef1C0ded9bec7F1a1670819833240f027b25EfF'
 export const zeroExRouterOptimismAddress =
   '0xdef1abe32c034e558cdd535791643c58a13acc10'
-
-// Index Coop Optimism Addresses
-export const FlashMintPerp = '0x2b44C227d95B8FDa1c8750986d3aDfF0E67627F7'
