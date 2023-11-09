3e6ae301260f9d30c2738c2f4e471f210ec148df
JD <jann.driessen@gmx.de>
2023-11-09 09:42:31 +0100

    add missing dependency

--- src/lib/hooks/use-token-price.ts
+++ src/lib/hooks/use-token-price.ts
@@ -29,7 +29,7 @@ export function useTokenPrice(token: Token): number {
       setTokenPrice(tokenPrice)
     }
     fetchTokenPrice()
-  }, [chainId, token])
+  }, [chainId, isSupportedNetwork, token])
 
   return tokenPrice
 }
