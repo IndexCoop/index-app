b308c298ff21717c1c210da6ec1b6e6e38867f91
JD <jann.driessen@gmx.de>
2023-11-10 14:48:10 +0100

    delete obsolete issuance interface

--- src/lib/utils/abi/interfaces.ts
+++ src/lib/utils/abi/interfaces.ts
@@ -1,10 +1,6 @@
 import { utils } from 'ethers'
 
 import { ERC20_ABI } from './ERC20'
-import { ISSUANCE_ABI } from './Issuance'
 
 export { ERC20_ABI } from './ERC20'
-
 export const ERC20Interface = new utils.Interface(ERC20_ABI)
-
-export const FlashMintPerpInterface = new utils.Interface(ISSUANCE_ABI)
