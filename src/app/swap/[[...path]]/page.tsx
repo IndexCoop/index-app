61049fa8a5a32196c038f939414cda88589faffc
JD <jann.driessen@gmx.de>
2023-11-15 17:14:32 +0100

    use selected token hook to determine showing icreth supply

--- src/app/swap/[[...path]]/page.tsx
+++ src/app/swap/[[...path]]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import { useConnect } from 'wagmi'
 
 import { Box, Flex } from '@chakra-ui/react'
 
@@ -9,13 +10,35 @@ import {
   RethSupplyCapOverrides,
   SupplyCapState,
 } from '@/components/supply'
-import QuickTradeContainer from '@/components/trade'
+import { Swap } from '@/components/trade/swap'
+import { LeveragedRethStakingYield } from '@/constants/tokens'
+import { useLedgerStatus } from '@/lib/hooks/useLedgerStatus'
+import { useSelectedToken } from '@/lib/providers/selected-token-provider'
+import { ledgerConnector } from '@/lib/utils/wagmi'
 
 export default function SwapPage() {
+  const { connectAsync, isIdle } = useConnect()
+  const { isRunningInLedgerLive } = useLedgerStatus()
+  const { inputToken, outputToken } = useSelectedToken()
+
   const [supplyCapOverrides, setSupplyCapOverrides] = useState<
     RethSupplyCapOverrides | undefined
   >(undefined)
-  const [showSupplyCap, setShowSupplyCap] = useState(false)
+
+  const showRethSupplyCap = useMemo(
+    () =>
+      inputToken.symbol === LeveragedRethStakingYield.symbol ||
+      outputToken.symbol === LeveragedRethStakingYield.symbol,
+    [inputToken, outputToken]
+  )
+
+  useEffect(() => {
+    if (!isIdle || !isRunningInLedgerLive) return
+    console.log('connecting to ledger')
+    connectAsync({ connector: ledgerConnector as any }).catch((error: any) => {
+      alert('error connecting to ledger: ' + error.toString())
+    })
+  }, [])
 
   return (
     <Flex
@@ -24,12 +47,12 @@ export default function SwapPage() {
       height='inherit'
     >
       <Box mb={[4, 4, 4, 12]} mr={4} w={['inherit', '500px']}>
-        <QuickTradeContainer
-          onOverrideSupplyCap={(overrides) => setSupplyCapOverrides(overrides)}
-          onShowSupplyCap={(show) => setShowSupplyCap(show)}
+        <Swap
+        // TODO: https://github.com/IndexCoop/index-app/blob/e7a9b8dd7b16901b92c93532707af1216b21a17b/src/components/trade/flashmint/index.tsx#L164C10-L164C29
+        // onOverrideSupplyCap={(overrides) => setSupplyCapOverrides(overrides)}
         />
       </Box>
-      {showSupplyCap && (
+      {showRethSupplyCap && (
         <Box h='100%' w={['100%', '100%', '500px', '360px']}>
           <RethSupplyCapContainer
             state={SupplyCapState.capWillExceed}
