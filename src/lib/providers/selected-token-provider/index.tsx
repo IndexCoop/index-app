2f933b2dfb07f44603834d0f56ea9a6e76d1c32a
JD <jann.driessen@gmx.de>
2023-11-15 17:25:52 +0100

    add input/output token props for swap

--- src/lib/providers/selected-token-provider/index.tsx
+++ src/lib/providers/selected-token-provider/index.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 
 import { useParams, useRouter } from 'next/navigation'
 
@@ -7,69 +13,79 @@ import { ETH, Token } from '@/constants/tokens'
 import { getDefaultIndex } from '@/lib/utils/tokens'
 
 export interface TokenContext {
+  isMinting: boolean
   inputToken: Token
   outputToken: Token
   selectInputToken: (inputToken: Token) => void
   selectOutputToken: (inputToken: Token) => void
+  toggleIsMinting: () => void
 }
 
 export const SelectedTokenContext = createContext<TokenContext>({
+  isMinting: true,
   inputToken: ETH,
   outputToken: getDefaultIndex(),
   selectInputToken: () => {},
   selectOutputToken: () => {},
+  toggleIsMinting: () => {},
 })
 
 export const useSelectedToken = () => useContext(SelectedTokenContext)
 
 export const SelectedTokenProvider = (props: { children: any }) => {
+  const [isMinting, setMinting] = useState<boolean>(true)
   const [inputToken, setInputToken] = useState<Token>(ETH)
   const [outputToken, setOutputToken] = useState<Token>(getDefaultIndex())
 
   const params = useParams()
   const router = useRouter()
 
-  // TODO: comment out and just use simple setters from outside for now
-  // TODO: delete use trade token lists
-  // // TODO: params.path
-  // useEffect(() => {
-  //   const resolver = new PathResolver()
-  //   const tokens = resolver.resolve(params.path as string[])
-  //   setInputToken(tokens.inputToken)
-  //   setOutputToken(tokens.outputToken)
-  // }, [])
+  useEffect(() => {
+    console.log(params.path, 'params.path')
+    const resolver = new PathResolver()
+    const tokens = resolver.resolve(params.path as string[])
+    setMinting(tokens.isMinting)
+    setInputToken(tokens.inputToken)
+    setOutputToken(tokens.outputToken)
+  }, [params.path])
 
-  const routeSwap = (inputToken: string, outputToken: string) => {
-    router.push(
-      `/swap/${inputToken.toLowerCase()}/${outputToken.toLowerCase()}`
-    )
-  }
+  const routeSwap = useCallback(
+    (inputToken: string, outputToken: string) => {
+      router.push(
+        `/swap/${inputToken.toLowerCase()}/${outputToken.toLowerCase()}`
+      )
+    },
+    [router]
+  )
 
-  const selectInputToken = (inputToken: Token) => {
-    setInputToken(inputToken)
-    // routeSwap(inputToken.symbol, outputToken.symbol)
-  }
+  const selectInputToken = useCallback(
+    (inputToken: Token) => {
+      routeSwap(inputToken.symbol, outputToken.symbol)
+    },
+    [outputToken, routeSwap]
+  )
 
-  const selectOutputToken = (outputToken: Token) => {
-    setOutputToken(outputToken)
-    // routeSwap(inputToken.symbol, outputToken.symbol)
-  }
+  const selectOutputToken = useCallback(
+    (outputToken: Token) => {
+      routeSwap(inputToken.symbol, outputToken.symbol)
+    },
+    [inputToken, routeSwap]
+  )
 
-  // TODO:
-  // const toggleIsMinting = () => {
-  //   // TODO: test
-  //   setIsBuying(!isBuying)
-  //   routeSwap(outputToken.symbol, inputToken.symbol)
-  // }
+  const toggleIsMinting = useCallback(() => {
+    console.log('toggling:', outputToken.symbol, inputToken.symbol)
+    routeSwap(outputToken.symbol, inputToken.symbol)
+  }, [inputToken, outputToken, routeSwap])
 
   return (
     <SelectedTokenContext.Provider
       value={{
+        isMinting,
         inputToken,
         outputToken,
         selectInputToken,
         selectOutputToken,
-        // toggleIsMinting,
+        toggleIsMinting,
       }}
     >
       {props.children}
