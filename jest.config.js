37bd4e86d337ae83715bad0b250be99234a65279
JD <jann.driessen@gmx.de>
2023-11-17 09:21:29 +0100

    collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverage: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
