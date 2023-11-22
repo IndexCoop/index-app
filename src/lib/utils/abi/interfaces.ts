0d6baeb70411df9b4765ae42f845505ae78e715b
JD <jann.driessen@gmx.de>
2023-11-22 08:52:29 +0100

    docs

--- src/lib/utils/abi/interfaces.ts
+++ src/lib/utils/abi/interfaces.ts
@@ -2,5 +2,7 @@ import { utils } from 'ethers'
 
 import { ERC20_ABI } from './ERC20'
 
+// Export for viem/wagmi
 export { ERC20_ABI } from './ERC20'
+// Export for ethers related code
 export const ERC20Interface = new utils.Interface(ERC20_ABI)
