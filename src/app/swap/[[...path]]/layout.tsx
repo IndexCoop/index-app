cb6f85b0a0f042bcd9cb05142053c9db82640379
JD <jann.driessen@gmx.de>
2023-11-23 14:14:49 +0100

    update app layout

--- src/app/swap/[[...path]]/layout.tsx
+++ src/app/swap/[[...path]]/layout.tsx
@@ -1,36 +1,11 @@
 'use client'
 
-import { Flex } from '@chakra-ui/react'
-
-import Footer from '@/components/footer'
-import Header from '@/components/header'
-
 import { Providers } from './providers'
 
-// TODO: potentially move header, footer etc. out of here (to a higher level)
-export default function SwapLayout({
-  children,
-}: {
+type LayoutProps = {
   children: React.ReactNode
-}) {
-  return (
-    <Providers>
-      <Header />
-      <Flex direction='column' mb='50px'>
-        <Flex
-          w={['390px', '500px', '820px', '1024px']}
-          m={['0', '0 auto']}
-          p={[
-            '100px 16px 0px 16px',
-            '100px 16px 0px 16px',
-            '128px 0 0 0',
-            '128px 0 0 0',
-          ]}
-        >
-          {children}
-        </Flex>
-      </Flex>
-      <Footer />
-    </Providers>
-  )
+}
+
+export default function SwapLayout({ children }: LayoutProps) {
+  return <Providers>{children}</Providers>
 }
