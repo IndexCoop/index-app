1e078998f4f5150942eaacfcd7b166e15bb06fb1
JD <jann.driessen@gmx.de>
2023-12-21 14:15:54 +0100

    add warning component

---
+++ src/components/swap/components/warning.tsx
@@ -0,0 +1,57 @@
+import { WarningIcon } from '@chakra-ui/icons'
+import { Flex, Text } from '@chakra-ui/react'
+
+import { colors } from '@/lib/styles/colors'
+
+export enum WarningType {
+  priceImpact,
+  restricted,
+}
+
+export interface Warning {
+  title: string
+  text: string
+}
+
+export const warningsData: Record<WarningType, Warning> = {
+  [WarningType.priceImpact]: {
+    title: 'Price Impact Warning',
+    text: 'A swap of this size may have a high price impact, given the current liquidity in the pool. This means that your trade could alter the price of the token and the quantity of tokens you receive.',
+  },
+  [WarningType.restricted]: {
+    title: 'Restricted Persons',
+    text: 'Some of our contracts are unavailable to persons or entities who: are citizens of, reside in, located in, incorporated in, or operate a registered office in the U.S.A.',
+  },
+}
+
+type WarningProps = {
+  warning: Warning
+}
+
+export const WarningComp = (props: WarningProps) => (
+  <Flex direction={'column'} m='20px 16px 8px'>
+    <Flex align={'center'} direction={'row'}>
+      <WarningIcon color={colors.icGray3} mr={'8px'} />
+      <Text fontSize={'sm'} fontWeight={600} textColor={colors.icGray3}>
+        {props.warning.title}
+      </Text>
+    </Flex>
+    <Text fontSize={'xs'} fontWeight={400} mt='8px' textColor={colors.icGray3}>
+      {props.warning.text}
+    </Text>
+  </Flex>
+)
+
+type WarningsProps = {
+  warnings: Warning[]
+}
+
+export const Warnings = (props: WarningsProps) => {
+  return (
+    <>
+      {props.warnings.map((warning) => (
+        <WarningComp key={warning.title.toLowerCase()} warning={warning} />
+      ))}
+    </>
+  )
+}
