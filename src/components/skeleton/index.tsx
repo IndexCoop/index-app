025edb860e05ea565ae47b938de9d5f33697089e
JD <jann.driessen@gmx.de>
2024-01-11 13:24:27 +0100

    docs

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -1,5 +1,6 @@
 import Skeleton, { SkeletonProps } from 'react-loading-skeleton'
 
+// This import is needed, otherwise the skeleton won't be visible.
 import 'react-loading-skeleton/dist/skeleton.css'
 
 import { colors } from '@/lib/styles/colors'
