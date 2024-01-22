202ece29a899cc1971cee8bd05cb097653b57e8b
JD <jann.driessen@gmx.de>
2024-01-22 12:38:53 +0100

    add range selection

---
+++ src/components/chart/components/range-selection.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import { Flex, Text } from '@chakra-ui/react'
+
+import { colors } from '@/lib/styles/colors'
+
+type RangeSelectionItemProps = {
+  label: string
+  isSelected: boolean
+  onClick: () => void
+}
+
+function RangeSelectionItem({
+  label,
+  onClick,
+  isSelected,
+}: RangeSelectionItemProps) {
+  const color = isSelected ? '#1C2929' : colors.icGray5
+  const backgroundColor = isSelected ? '#F2F2F2' : 'none'
+  return (
+    <Flex
+      bg={backgroundColor}
+      borderRadius='8px'
+      cursor='pointer'
+      onClick={onClick}
+      mr='4px'
+      p='10px'
+    >
+      <Text color={color} fontSize='sm' fontWeight='600'>
+        {label}
+      </Text>
+    </Flex>
+  )
+}
+
+const range = ['1H', '1D', '1W', '1M', '1Y', 'ALL']
+export function RangeSelection() {
+  const [selected, setSelected] = useState(3)
+  return (
+    <Flex>
+      {range.map((range, index) => (
+        <RangeSelectionItem
+          key={index}
+          label={range}
+          isSelected={selected === index}
+          onClick={() => setSelected(index)}
+        />
+      ))}
+    </Flex>
+  )
+}
--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -1,20 +1,22 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 import { colors } from '@/lib/styles/colors'
 
-function Price({ label }: { label: string }) {
-  return (
-    <Text color={colors.icGray9} fontSize='x-large' fontWeight='600'>
-      {label}
-    </Text>
-  )
-}
+import { Price } from './components/price'
+import { RangeSelection } from './components/range-selection'
 
 export function Chart() {
   return (
-    <Flex bg={colors.icWhite} borderRadius='22' shadow={'md'}>
-      <Flex p='24px'>
+    <Flex
+      align='start'
+      bg={colors.icWhite}
+      borderRadius='22'
+      shadow={'md'}
+      w='700px'
+    >
+      <Flex align='center' flex='row' p='24px' justify='space-between' w='100%'>
         <Price label={'$2,379.95'} />
+        <RangeSelection />
       </Flex>
     </Flex>
   )
