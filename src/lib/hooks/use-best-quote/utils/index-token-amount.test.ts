b24ad9bfb38a12ab99428e9f6903e99eae71076e
JD <jann.driessen@gmx.de>
2024-01-23 10:05:49 +0100

    simplify getting index token amount

--- src/lib/hooks/use-best-quote/utils/index-token-amount.test.ts
+++ src/lib/hooks/use-best-quote/utils/index-token-amount.test.ts
@@ -1,94 +1,20 @@
-import { formatUnits } from 'viem'
+import { formatUnits, parseUnits } from 'viem'
 
-import { ZeroExData } from '@/lib/utils/api/zeroex-utils'
-import { toWei } from '@/lib/utils'
-
-import { maxPriceImpact } from '../config'
 import { getIndexTokenAmount } from './index-token-amount'
 
 describe('getIndexTokenAmount - redeeming', () => {
   it('returns input token amount directly', async () => {
     const isMinting = false
-    const expectedIndexTokenAmount = toWei('1', 18)
-    const indexTokenAmount = getIndexTokenAmount(
-      isMinting,
-      '1',
-      18,
-      18,
-      0,
-      0,
-      null
-    )
-    expect(indexTokenAmount.toString()).toEqual(
-      expectedIndexTokenAmount.toString()
-    )
+    const expectedIndexTokenAmount = parseUnits('1', 18)
+    const indexTokenAmount = getIndexTokenAmount(isMinting, '1', 18, 18, 0, 0)
+    expect(indexTokenAmount.toBigInt()).toEqual(expectedIndexTokenAmount)
   })
 })
 
 describe('getIndexTokenAmount - minting', () => {
   const outputAdjust = 0.99
 
-  it('returns buy amount (0x data)', async () => {
-    const isMinting = true
-    const dexData = {
-      buyAmount: '2000000000000000000',
-    } as ZeroExData
-    const indexTokenAmount = getIndexTokenAmount(
-      isMinting,
-      '1',
-      18,
-      18,
-      0,
-      0,
-      dexData
-    )
-    expect(indexTokenAmount.toString()).toEqual(dexData.buyAmount.toString())
-  })
-
-  it('returns buy amount (0x data) if price impact is below max', async () => {
-    const isMinting = true
-    const dexData = {
-      estimatedPriceImpact: '0.1',
-      buyAmount: '2000000000000000000',
-    } as ZeroExData
-    const indexTokenAmount = getIndexTokenAmount(
-      isMinting,
-      '1',
-      18,
-      18,
-      0,
-      0,
-      dexData
-    )
-    expect(indexTokenAmount.toString()).toEqual(dexData.buyAmount.toString())
-  })
-
-  it('returns approx amount if price impact is above max', async () => {
-    const isMinting = true
-    const inputTokenAmount = '1'
-    const inputTokenPrice = 2
-    const outputTokenPrice = 3
-    const dexData = {
-      estimatedPriceImpact: maxPriceImpact.toString(),
-      buyAmount: '2000000000000000000',
-    } as ZeroExData
-    const indexTokenAmount = getIndexTokenAmount(
-      isMinting,
-      inputTokenAmount,
-      18,
-      18,
-      inputTokenPrice,
-      outputTokenPrice,
-      dexData
-    )
-    const inputTokenTotal = parseFloat(inputTokenAmount) * inputTokenPrice
-    const approxOutputAmount =
-      (inputTokenTotal / outputTokenPrice) * outputAdjust
-    const expectedAmount = toWei(approxOutputAmount, 18)
-    expect(indexTokenAmount.toString()).toEqual(expectedAmount.toString())
-  })
-
-  it('returns approx amount if no 0x data', async () => {
+  it('returns approx. index token amount', async () => {
     const isMinting = true
     const inputTokenAmount = '1'
     const inputTokenPrice = 2
@@ -99,18 +25,17 @@ describe('getIndexTokenAmount - minting', () => {
       18,
       18,
       inputTokenPrice,
-      outputTokenPrice,
-      null
+      outputTokenPrice
     )
-    const inputTokenTotal = parseFloat(inputTokenAmount) * inputTokenPrice
+    const inputTokenAmountUsd = parseFloat(inputTokenAmount) * inputTokenPrice
     const approxOutputAmount =
-      (inputTokenTotal / outputTokenPrice) * outputAdjust
-    const expectedAmount = toWei(approxOutputAmount, 18)
+      (inputTokenAmountUsd / outputTokenPrice) * outputAdjust
+    const expectedAmount = parseUnits(approxOutputAmount.toString(), 18)
     const indexTokenPriceTotal =
       Number(formatUnits(BigInt(indexTokenAmount.toString()), 18)) *
       outputTokenPrice
     expect(indexTokenAmount.toString()).toEqual(expectedAmount.toString())
-    expect(indexTokenPriceTotal).toBeCloseTo(inputTokenTotal, 1)
+    expect(indexTokenPriceTotal).toBeCloseTo(inputTokenAmountUsd, 1)
   })
 
   it('returns index token amount with correct decimals for usdc input', async () => {
@@ -124,13 +49,12 @@ describe('getIndexTokenAmount - minting', () => {
       6,
       18,
       inputTokenPrice,
-      outputTokenPrice,
-      null
+      outputTokenPrice
     )
     const inputTokenTotal = parseFloat(inputTokenAmount) * inputTokenPrice
     const approxOutputAmount =
       (inputTokenTotal / outputTokenPrice) * outputAdjust
-    const expectedAmount = toWei(approxOutputAmount, 18)
+    const expectedAmount = parseUnits(approxOutputAmount.toString(), 18)
     const indexTokenPriceTotal =
       Number(formatUnits(BigInt(indexTokenAmount.toString()), 18)) *
       outputTokenPrice
--- src/lib/hooks/use-best-quote/utils/index-token-amount.ts
+++ src/lib/hooks/use-best-quote/utils/index-token-amount.ts
@@ -2,37 +2,21 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 import { toWei } from '@/lib/utils'
 
-import { maxPriceImpact } from '../config'
-
 export const getIndexTokenAmount = (
   isMinting: boolean,
   inputTokenAmount: string,
   inputTokenDecimals: number,
   outputTokenDecimals: number,
   inputTokenPrice: number,
-  outputTokenPrice: number,
-  dexSwapOption: { buyAmount: string; estimatedPriceImpact: string } | null
+  outputTokenPrice: number
 ): BigNumber => {
   if (!isMinting) {
     return toWei(inputTokenAmount, inputTokenDecimals)
   }
-
-  let indexTokenAmount = dexSwapOption
-    ? BigNumber.from(dexSwapOption?.buyAmount ?? '0')
-    : BigNumber.from('0')
-
-  const priceImpact =
-    dexSwapOption && dexSwapOption.estimatedPriceImpact
-      ? Math.abs(parseFloat(dexSwapOption.estimatedPriceImpact))
-      : 0
-
-  if (!dexSwapOption || priceImpact >= maxPriceImpact) {
-    // Recalculate the exchange issue/redeem quotes if not enough DEX liquidity
-    const sellTokenTotal = parseFloat(inputTokenAmount) * inputTokenPrice
-    const approxOutputAmount =
-      outputTokenPrice === 0 ? 0 : (sellTokenTotal / outputTokenPrice) * 0.99
-    indexTokenAmount = toWei(approxOutputAmount, outputTokenDecimals)
-  }
-
+  // Recalculate the exchange issue/redeem quotes if not enough DEX liquidity
+  const inputTokenAmountUsd = parseFloat(inputTokenAmount) * inputTokenPrice
+  const approxOutputAmount =
+    outputTokenPrice === 0 ? 0 : (inputTokenAmountUsd / outputTokenPrice) * 0.99
+  const indexTokenAmount = toWei(approxOutputAmount, outputTokenDecimals)
   return indexTokenAmount
 }
