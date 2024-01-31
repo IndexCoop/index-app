7e93c4d758c79a7613df7050879773f6f44994f2
JD <jann.driessen@gmx.de>
2024-01-31 15:32:47 +0100

    add try/catch

--- src/lib/hooks/use-best-quote/utils/issuance/index.ts
+++ src/lib/hooks/use-best-quote/utils/issuance/index.ts
@@ -30,29 +30,34 @@ export async function getEnhancedRedemptionQuote(
   //     currencies.filter((curr) => curr.symbol === inputOutputToken.symbol)
   //       .length > 0
   //   if (!isAllowedCurrency) return null
-  return {
-    type: QuoteType.redemption,
-    chainId: 1,
-    contract: '0x04b59F9F09750C044D7CfbC177561E409085f0f3',
-    isMinting: false,
-    inputToken: Ethereum2xFlexibleLeverageIndex,
-    outputToken: Ethereum2xFlexibleLeverageIndex, // FIXME: use new 2x token
-    gas: BigNumber.from(0), // TODO:
-    gasPrice: BigNumber.from(gasPrice.toString()),
-    // TODO:
-    gasCosts: BigNumber.from(0),
-    gasCostsInUsd: 0,
-    fullCostsInUsd: 0,
-    priceImpact: 0,
-    indexTokenAmount: BigNumber.from(0),
-    inputOutputTokenAmount: BigNumber.from(0),
-    inputTokenAmount: BigNumber.from(0),
-    inputTokenAmountUsd: 0,
-    outputTokenAmount: BigNumber.from(0),
-    outputTokenAmountUsd: 0,
-    inputTokenPrice: 0,
-    outputTokenPrice: 0,
-    slippage: 0,
-    tx: {},
+  try {
+    return {
+      type: QuoteType.redemption,
+      chainId: 1,
+      contract: '0x04b59F9F09750C044D7CfbC177561E409085f0f3',
+      isMinting: false,
+      inputToken: Ethereum2xFlexibleLeverageIndex,
+      outputToken: Ethereum2xFlexibleLeverageIndex, // FIXME: use new 2x token
+      gas: BigNumber.from(0), // TODO:
+      gasPrice: BigNumber.from(gasPrice.toString()),
+      // TODO:
+      gasCosts: BigNumber.from(0),
+      gasCostsInUsd: 0,
+      fullCostsInUsd: 0,
+      priceImpact: 0,
+      indexTokenAmount: BigNumber.from(0),
+      inputOutputTokenAmount: BigNumber.from(0),
+      inputTokenAmount: BigNumber.from(0),
+      inputTokenAmountUsd: 0,
+      outputTokenAmount: BigNumber.from(0),
+      outputTokenAmountUsd: 0,
+      inputTokenPrice: 0,
+      outputTokenPrice: 0,
+      slippage: 0,
+      tx: {},
+    }
+  } catch (e) {
+    console.warn('Error fetching redemption quote', e)
+    return null
   }
 }
