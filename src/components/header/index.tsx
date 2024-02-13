d7b4000dd3438b4f8715cc45421fdeadb0dac3b4
JD <jann.driessen@gmx.de>
2024-02-13 11:17:35 +0100

    hide links for now

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Connect } from './connect'
-import { HeaderLink } from './link'
 import { Logo } from './logo'
 
 export enum Path {
@@ -12,11 +11,11 @@ export enum Path {
 
 export function Header() {
   return (
-    <header className='bg-ic-white sticky top-0 flex justify-between px-9 py-4 shadow-md backdrop-blur shadow-ic-black/15 z-50 opacity-[.97]'>
+    <header className='bg-ic-white sticky top-0 flex justify-between px-9 py-4 shadow-md backdrop-blur shadow-ic-black/15 z-50 opacity-[.96]'>
       <div className='flex flex-row items-center space-x-6'>
         <Logo />
-        <HeaderLink href={Path.TRADE} label='Trade' />
-        <HeaderLink href={Path.PRODUCTS} label='Products' />
+        {/* <HeaderLink href={Path.TRADE} label='Trade' /> */}
+        {/* <HeaderLink href={Path.PRODUCTS} label='Products' /> */}
       </div>
       <div className='hidden sm:flex flex-row h-10 max-h-10'>
         <Connect />
