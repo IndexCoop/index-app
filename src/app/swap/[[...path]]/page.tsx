04e8390eaa786eab50e6b3012d3c8424446fe714
JD <jann.driessen@gmx.de>
2024-02-13 16:27:40 +0100

    add migration banner

--- src/app/swap/[[...path]]/page.tsx
+++ src/app/swap/[[...path]]/page.tsx
@@ -12,6 +12,7 @@ import {
 import { Swap } from '@/components/swap'
 import { LeveragedRethStakingYield } from '@/constants/tokens'
 import { useSelectedToken } from '@/lib/providers/selected-token-provider'
+import { FliMigrationBanner } from '@/components/banners/fli-migration-banner'
 
 export default function SwapPage() {
   const { inputToken, isMinting, outputToken } = useSelectedToken()
@@ -33,7 +34,13 @@ export default function SwapPage() {
       mx='auto'
       height='inherit'
     >
-      <Box mb={[4, 4, 4, 12]} mr={4} w={['inherit', '500px']}>
+      <Flex
+        className='flex-col gap-8'
+        mb={[4, 4, 4, 12]}
+        mr={4}
+        w={['inherit', '500px']}
+      >
+        <FliMigrationBanner />
         <Swap
           isBuying={isMinting}
           inputToken={inputToken}
@@ -41,7 +48,7 @@ export default function SwapPage() {
           // TODO: https://github.com/IndexCoop/index-app/blob/e7a9b8dd7b16901b92c93532707af1216b21a17b/src/components/trade/flashmint/index.tsx#L164C10-L164C29
           // onOverrideSupplyCap={(overrides) => setSupplyCapOverrides(overrides)}
         />
-      </Box>
+      </Flex>
       {showRethSupplyCap && (
         <Box h='100%' w={['100%', '100%', '500px', '360px']}>
           <RethSupplyCapContainer
