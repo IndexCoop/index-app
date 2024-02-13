9c537424736512cf92b2f90d0ae4903f41ae23e9
JD <jann.driessen@gmx.de>
2024-02-13 16:49:06 +0100

    add tailwind prettier plugin

--- src/components/banners/fli-migration-banner.tsx
+++ src/components/banners/fli-migration-banner.tsx
@@ -2,11 +2,11 @@ import './fli-migration-banner.css'
 
 export function FliMigrationBanner() {
   return (
-    <div className='banner-custom flex flex-col gap-1 justify-center p-6 rounded-3xl'>
-      <h2 className='text-ic-black text-base font-bold'>
+    <div className='banner-custom flex flex-col justify-center gap-1 rounded-3xl p-6'>
+      <h2 className='text-base font-bold text-ic-black'>
         Holding FLI? Upgrade for better cost of carry.
       </h2>
-      <p className='text-ic-gray-600 text-xs font-medium'>
+      <p className='text-xs font-medium text-ic-gray-600'>
         We are migrating our Leverage suite to improved contracts. During this
         time, the FLI tokens are only tradable for the new 2x tokens.
       </p>
