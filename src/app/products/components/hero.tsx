6d4500d4facfcc992fdc8b847c9510a74c7a185d
0xonramp <0xonramp@gmail.com>
2024-02-23 21:38:41 -0500

    Improve styling

--- src/app/products/components/hero.tsx
+++ src/app/products/components/hero.tsx
@@ -4,8 +4,8 @@ import { Path } from '@/constants/paths'
 export function Hero() {
   return (
     <div className='flex flex-col items-center space-y-10 md:space-y-12'>
-      <h2 className='text-ic-gray-800 text-3xl md:text-4xl lg:text-5xl font-bold text-center max-w-3xl'>
-        Crypto is complex. Our products make it simple.
+      <h2 className='text-ic-gray-800 text-3xl md:text-4xl lg:text-5xl font-bold text-center max-w-3xl lg:leading-tight'>
+        Crypto is complex.<br />Our products make it simple.
       </h2>
       <h3 className='text-ic-gray-500 font-medium text-sm md:text-base text-center max-w-lg'>
         Unlock powerful sector, leverage and yield strategies with our simple
