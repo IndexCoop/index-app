ed68bee407ac4249b1e2fa963d072d084b8e9bf1
0xonramp <0xonramp@gmail.com>
2024-02-23 21:11:17 -0500

    Remove blur

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className='h-full'>
-      <body className="h-fit flex flex-col bg-[url('/gradient-splash.jpg')] bg-top backdrop-blur-3xl">
+      <body className="h-fit flex flex-col bg-[url('/gradient-splash.jpg')] bg-top">
         <ProvidersLite>
           <Header />
           <main>{children}</main>
