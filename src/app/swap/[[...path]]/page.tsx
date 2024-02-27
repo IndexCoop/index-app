d361ccbbfe8270de3a5e1758f8c6632f18a8c3a9
JD <jann.driessen@gmx.de>
2024-02-27 14:49:47 +0100

    temp add to swap page

--- src/app/swap/[[...path]]/page.tsx
+++ src/app/swap/[[...path]]/page.tsx
@@ -12,6 +12,7 @@ import {
 import { Swap } from '@/components/swap'
 import { LeveragedRethStakingYield } from '@/constants/tokens'
 import { useSelectedToken } from '@/lib/providers/selected-token-provider'
+import { Chart } from '@/components/chart'
 
 export default function SwapPage() {
   const { inputToken, isMinting, outputToken } = useSelectedToken()
@@ -28,12 +29,11 @@ export default function SwapPage() {
   )
 
   return (
-    <Flex
-      direction={['column', 'column', 'column', 'row']}
-      mx='auto'
-      height='inherit'
-    >
-      <Box mb={[4, 4, 4, 12]} mr={4} w={['inherit', '500px']}>
+    <Flex direction={['column', 'column', 'column', 'row']} mx='auto'>
+      <Box mb='auto' minW={['100%', '700px']} >
+        <Chart />
+      </Box>
+      <Flex mb={[4, 4, 4, 12]} ml={[0, 16]} mt={[8, 0]} w={['inherit', '500px']} h={'100%'}>
         <Swap
           isBuying={isMinting}
           inputToken={inputToken}
@@ -41,7 +41,7 @@ export default function SwapPage() {
           // TODO: https://github.com/IndexCoop/index-app/blob/e7a9b8dd7b16901b92c93532707af1216b21a17b/src/components/trade/flashmint/index.tsx#L164C10-L164C29
           // onOverrideSupplyCap={(overrides) => setSupplyCapOverrides(overrides)}
         />
-      </Box>
+      </Flex>
       {showRethSupplyCap && (
         <Box h='100%' w={['100%', '100%', '500px', '360px']}>
           <RethSupplyCapContainer
