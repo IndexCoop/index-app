d1bc3fbcb1160e1a07747144c0b94b9c24ead8cf
JD <jann.driessen@gmx.de>
2024-03-04 09:41:05 +0000

    show navigation links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Connect } from './connect'
+import { HeaderLink } from './link'
 import { Logo } from './logo'
 
 export enum Path {
@@ -11,13 +12,13 @@ export enum Path {
 
 export function Header() {
   return (
-    <header className='bg-ic-white sticky top-0 flex justify-between px-9 py-4 shadow-md backdrop-blur shadow-ic-black/15 z-50 opacity-[.96]'>
+    <header className='bg-ic-white shadow-ic-black/15 sticky top-0 z-50 flex justify-between px-9 py-4 opacity-[.96] shadow-md backdrop-blur'>
       <div className='flex flex-row items-center space-x-6'>
         <Logo />
-        {/* <HeaderLink href={Path.TRADE} label='Trade' /> */}
-        {/* <HeaderLink href={Path.PRODUCTS} label='Products' /> */}
+        <HeaderLink href={Path.TRADE} label='Trade' />
+        <HeaderLink href={Path.PRODUCTS} label='Products' />
       </div>
-      <div className='hidden sm:flex flex-row h-10 max-h-10'>
+      <div className='hidden h-10 max-h-10 flex-row sm:flex'>
         <Connect />
       </div>
     </header>
