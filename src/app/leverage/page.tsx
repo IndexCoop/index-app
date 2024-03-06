7f12678ce637a50d22700594514006a98bf9bad9
JD <jann.driessen@gmx.de>
2024-03-06 14:43:35 +0000

    center main content

--- src/app/leverage/page.tsx
+++ src/app/leverage/page.tsx
@@ -5,14 +5,16 @@ import { Title } from './components/title'
 
 export default function Page() {
   return (
-    <div className='mx-auto flex flex-col gap-6 p-12'>
-      <div className='flex flex-row gap-36'>
-        <Title />
-        <Stats />
-      </div>
-      <div className='flex flex-row gap-6'>
-        <LeverageChart />
-        <LeverageWidget />
+    <div className='flex'>
+      <div className='mx-auto flex flex-col gap-6 p-12'>
+        <div className='flex flex-row gap-36'>
+          <Title />
+          <Stats />
+        </div>
+        <div className='flex flex-row gap-6'>
+          <LeverageChart />
+          <LeverageWidget />
+        </div>
       </div>
     </div>
   )
