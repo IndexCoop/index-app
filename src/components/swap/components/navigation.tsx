2a2dd434ca967cd50de78ff6661c8e9ac382abbb
JD <jann.driessen@gmx.de>
2024-03-06 12:09:13 +0000

    add buy button

--- src/components/swap/components/navigation.tsx
+++ src/components/swap/components/navigation.tsx
@@ -1,8 +1,5 @@
-import { Text } from '@chakra-ui/react'
-
 import { Settings } from '@/components/settings'
 import { useSlippage } from '@/lib/providers/slippage'
-import { colors } from '@/lib/styles/colors'
 
 export function SwapNavigation() {
   const {
@@ -13,15 +10,14 @@ export function SwapNavigation() {
   } = useSlippage()
   return (
     <div className='flex flex-row justify-between'>
-      <Text
-        color={colors.ic.gray[900]}
-        fontSize={'md'}
-        fontWeight={500}
-        ml={'12px'}
-        my={'16px'}
-      >
-        Swap
-      </Text>
+      <div className='my-4 ml-3 flex  flex-row gap-10'>
+        <div className='text-ic-gray-900 cursor-pointer text-base font-medium'>
+          Swap
+        </div>
+        <div className='text-ic-gray-900 cursor-pointer text-base font-medium'>
+          Buy
+        </div>
+      </div>
       <Settings
         isAuto={isAutoSlippage}
         isDarkMode={false}
