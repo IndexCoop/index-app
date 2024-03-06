ec0dc235b2c6df94acb8d2a65ff113515feca1cc
JD <jann.driessen@gmx.de>
2024-03-06 15:54:14 +0000

    update footer colors from static's design

--- src/components/footer/footer-link.tsx
+++ src/components/footer/footer-link.tsx
@@ -11,7 +11,7 @@ export function FooterLink({ children, href }: Props) {
       <Link
         target='_blank'
         href={href}
-        className='text-ic-gray-600 hover:text-ic-gray-900 dark:hover:text-ic-gray-600 dark:text-ic-gray-400 text-sm leading-6'
+        className='text-ic-gray-600 hover:text-ic-gray-900 dark:hover:text-ic-gray-400 dark:text-ic-gray-200 text-sm leading-6'
       >
         {children}
       </Link>
