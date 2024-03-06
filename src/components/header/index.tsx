e87fed60988c9f350b78f6bfe05a7aaa31c21a8e
JD <jann.driessen@gmx.de>
2024-03-06 15:47:40 +0000

    for now show leverage link only for dev env

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,8 @@ import { Connect } from './connect'
 import { HeaderLink } from './link'
 import { Logo } from './logo'
 
+const isDevEnv = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production'
+
 export function Header() {
   return (
     <header className='bg-ic-white dark:bg-ic-blue-950 shadow-ic-black/15 sticky top-0 z-50 flex justify-between px-9 py-4 opacity-[.96] shadow-md backdrop-blur'>
@@ -10,7 +12,7 @@ export function Header() {
         <Logo />
         <HeaderLink href={Path.TRADE} label='Trade' />
         <HeaderLink href={Path.PRODUCTS} label='Products' />
-        <HeaderLink href={Path.LEVERAGE} label='Leverage' />
+        {isDevEnv && <HeaderLink href={Path.LEVERAGE} label='Leverage' />}
       </div>
       <div className='hidden h-10 max-h-10 flex-row sm:flex'>
         <Connect />
