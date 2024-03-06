c3d1651d8a4a8065cb8f35e1a8cc0f0aa607bb6b
0xonramp <0xonramp@gmail.com>
2024-03-06 17:39:56 -0500

    Remove unused vaults

--- src/app/vaults/providers.tsx
+++
@@ -1,5 +0,0 @@
-import { ProtectionProvider } from '@/lib/providers/protection'
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ProtectionProvider>{children}</ProtectionProvider>
-}
