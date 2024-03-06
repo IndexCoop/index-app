7b4bb1f497004dd5658dfaa4534f53873b90d25a
JD <jann.driessen@gmx.de>
2024-03-06 15:24:31 +0000

    update header removing props

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,11 @@ import { Connect } from './connect'
 import { HeaderLink } from './link'
 import { Logo } from './logo'
 
-export function Header({ isDarkMode }: { isDarkMode?: boolean }) {
+export function Header() {
   return (
     <header className='bg-ic-white dark:bg-ic-blue-950 shadow-ic-black/15 sticky top-0 z-50 flex justify-between px-9 py-4 opacity-[.96] shadow-md backdrop-blur'>
       <div className='flex flex-row items-center space-x-6'>
-        <Logo isDarkMode={isDarkMode === true} />
+        <Logo />
         <HeaderLink href={Path.TRADE} label='Trade' />
         <HeaderLink href={Path.PRODUCTS} label='Products' />
         <HeaderLink href={Path.LEVERAGE} label='Leverage' />
