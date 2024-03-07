81544c167def33571a3c74354b7c851c677eca63
JD <jann.driessen@gmx.de>
2024-03-07 09:40:55 +0000

    add leverage selector

--- src/app/leverage/components/leverage-widget/components/leverage-selector.tsx
+++ src/app/leverage/components/leverage-widget/components/leverage-selector.tsx
@@ -1,3 +1,20 @@
 export function LeverageSelector() {
-  return <div></div>
+  return (
+    <div className='flex flex-row items-center justify-between rounded-xl border border-[#3A6060] p-4'>
+      <div className='text-ic-gray-300 text-xs font-medium'>Leverage</div>
+      <div className='flex flex-row gap-2'>
+        <LeverageSelectorButton label='-1x' />
+        <LeverageSelectorButton label='2x' />
+        <LeverageSelectorButton label='3x' />
+      </div>
+    </div>
+  )
+}
+
+export function LeverageSelectorButton({ label }: { label: string }) {
+  return (
+    <div className='text-ic-gray-300 bg-ic-blue-950 w-14 cursor-pointer rounded-full px-3 py-2 text-center text-sm font-bold'>
+      {label}
+    </div>
+  )
 }
