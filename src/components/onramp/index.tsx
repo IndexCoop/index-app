70f8cf46e1d43c931e6f9c3525aa031a1af56f23
JD <jann.driessen@gmx.de>
2024-03-11 15:42:56 +0000

    test iframe

--- src/components/onramp/index.tsx
+++ src/components/onramp/index.tsx
@@ -18,13 +18,19 @@ export const OnrampModal = (props: OnrampModalProps) => {
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered scrollBehavior='inside'>
       <ModalOverlay className='bg-ic-black bg-opacity-60 backdrop-blur' />
-      <ModalContent
-        className='border-ic-gray-100 bg-ic-white mx-4 my-0 rounded-xl border-2'
-        h={['60vh', '50vh']}
-      >
+      <ModalContent className='border-ic-gray-100 bg-ic-white mx-4 my-0 rounded-xl border-2'>
         <ModalHeader>Buy</ModalHeader>
         <ModalCloseButton />
-        <ModalBody></ModalBody>
+        <ModalBody>
+          <iframe
+            // style='border-radius: 4px; border: 1px solid #58585f; margin: auto;max-width: 420px'
+            src='https://buy.onramper.com?apiKey=pk_prod_01HETEQF46GSK6BS5JWKDF31BT'
+            height='630px'
+            width='420px'
+            title='Onramper widget'
+            allow='accelerometer; autoplay; camera; gyroscope; payment'
+          ></iframe>
+        </ModalBody>
         <div className='text-ic-gray-500 mb-4 text-center text-xs'>
           Powered by Onramper
         </div>
