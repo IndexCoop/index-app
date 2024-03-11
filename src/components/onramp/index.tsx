b9a207a28a1f95d2dc74f803d24c5cb78f6a9eb9
JD <jann.driessen@gmx.de>
2024-03-11 13:57:51 +0000

    update modal header/footer and backdrop

--- src/components/onramp/index.tsx
+++ src/components/onramp/index.tsx
@@ -1,15 +1,11 @@
 import {
-  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  Text,
 } from '@chakra-ui/react'
-
-import { colors, useColorStyles } from '@/lib/styles/colors'
 
 type OnrampModalProps = {
   address?: string
@@ -19,33 +15,19 @@ type OnrampModalProps = {
 
 export const OnrampModal = (props: OnrampModalProps) => {
   const { isOpen, onClose } = props
-  const { styles } = useColorStyles()
-  const backgroundColor = styles.background
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered scrollBehavior='inside'>
-      <ModalOverlay
-        bg='rgba(0, 0, 0, 0.6)'
-        backdropFilter='auto'
-        backdropBlur='8px'
-      />
+      <ModalOverlay className='bg-ic-black bg-opacity-60 backdrop-blur' />
       <ModalContent
-        backgroundColor={backgroundColor}
-        borderColor={colors.ic.gray[100]}
-        borderRadius='10'
-        borderStyle='solid'
-        borderWidth='2px'
+        className='border-ic-gray-100 bg-ic-white mx-4 my-0 rounded-xl border-2'
         h={['60vh', '50vh']}
-        m={['16px', 0]}
       >
-        <ModalHeader>Select a token</ModalHeader>
+        <ModalHeader>Buy</ModalHeader>
         <ModalCloseButton />
-        <ModalBody p='16px 0'>
-          <Flex justify='flex-end' pr='16px' w='100%'>
-            <Text fontSize='sm' fontWeight='500'>
-              Quantity Owned
-            </Text>
-          </Flex>
-        </ModalBody>
+        <ModalBody></ModalBody>
+        <div className='text-ic-gray-500 mb-4 text-center text-xs'>
+          Powered by Onramper
+        </div>
       </ModalContent>
     </Modal>
   )
