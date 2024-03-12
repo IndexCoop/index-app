49d315c2201f045f7490155d864c10e3ddd62e23
JD <jann.driessen@gmx.de>
2024-03-12 13:19:18 +0000

    update layout

--- src/components/onramp/index.tsx
+++ src/components/onramp/index.tsx
@@ -18,17 +18,16 @@ export const OnrampModal = (props: OnrampModalProps) => {
   return (
     <Modal onClose={onClose} isOpen={isOpen} isCentered scrollBehavior='inside'>
       <ModalOverlay className='bg-ic-black bg-opacity-60 backdrop-blur' />
-      <ModalContent className='border-ic-gray-100 bg-ic-white mx-4 my-0 rounded-xl border-2'>
+      <ModalContent className='border-ic-gray-100 bg-ic-white mx-0 my-0 rounded-xl border-2 p-0'>
         <ModalHeader>Buy</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
+        <ModalBody className='m-0 p-0'>
           <iframe
-            // style='border-radius: 4px; border: 1px solid #58585f; margin: auto;max-width: 420px'
-            src='https://buy.onramper.com?apiKey=pk_prod_01HETEQF46GSK6BS5JWKDF31BT'
-            height='630px'
-            width='420px'
-            title='Onramper widget'
-            allow='accelerometer; autoplay; camera; gyroscope; payment'
+            src='https://buy.onramper.com?apiKey=pk_prod_01HREVCX1YJDHAHQ6BE777J41B'
+            title='Onramper Widget'
+            height='600px'
+            width='100%'
+            // allow='accelerometer; autoplay; camera; gyroscope; payment'
           ></iframe>
         </ModalBody>
       </ModalContent>
