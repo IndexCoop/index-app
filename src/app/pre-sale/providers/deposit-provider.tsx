39542b68fc220d57b31397a526d553fde44cce2b
JD <jann.driessen@gmx.de>
2024-03-22 15:43:25 +0000

    add auto fetching quote

--- src/app/pre-sale/providers/deposit-provider.tsx
+++ src/app/pre-sale/providers/deposit-provider.tsx
@@ -2,12 +2,19 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
   useState,
 } from 'react'
 
+import { usePublicClient } from 'wagmi'
+
 import { HighYieldETHIndex, Token, WSTETH } from '@/constants/tokens'
-import { isValidTokenInput } from '@/lib/utils'
+import { getEnhancedIssuanceQuote } from '@/lib/hooks/use-best-quote/utils/issuance'
+import { getTokenPrice, useNativeTokenPrice } from '@/lib/hooks/use-token-price'
+import { useWallet } from '@/lib/hooks/use-wallet'
+import { isValidTokenInput, toWei } from '@/lib/utils'
+import { GasStation } from '@/lib/utils/api/gas-station'
 
 interface DepositContextProps {
   inputValue: string
@@ -30,6 +37,10 @@ const DepositContext = createContext<DepositContextProps>({
 export const useDeposit = () => useContext(DepositContext)
 
 export function DepositProvider(props: { children: any; preSaleToken: Token }) {
+  const nativeTokenPrice = useNativeTokenPrice(1)
+  const publicClient = usePublicClient()
+  const { provider, signer } = useWallet()
+
   const { preSaleToken } = props
   const preSaleCurrencyToken = WSTETH
 
@@ -39,6 +50,14 @@ export function DepositProvider(props: { children: any; preSaleToken: Token }) {
   const inputToken = useMemo(
     () => (isDepositing ? preSaleCurrencyToken : preSaleToken),
     [isDepositing, preSaleCurrencyToken, preSaleToken],
+  )
+
+  const inputTokenAmount = useMemo(
+    () =>
+      inputValue === ''
+        ? BigInt(0)
+        : toWei(inputValue, inputToken.decimals).toBigInt(),
+    [inputToken, inputValue],
   )
 
   const onChangeInputTokenAmount = useCallback(
@@ -57,6 +76,48 @@ export function DepositProvider(props: { children: any; preSaleToken: Token }) {
   const toggleIsDepositing = useCallback(() => {
     setDepositing(!isDepositing)
   }, [isDepositing])
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const outputToken = isDepositing ? preSaleToken : preSaleCurrencyToken
+      const inputTokenPrice = await getTokenPrice(inputToken, 1)
+      const outputTokenPrice = await getTokenPrice(outputToken, 1)
+      const gasStation = new GasStation(provider)
+      const gasPrice = await gasStation.getGasPrice()
+      const quoteIssuance = await getEnhancedIssuanceQuote(
+        {
+          isIssuance: isDepositing,
+          gasPrice: gasPrice.toBigInt(),
+          indexTokenAmount: inputTokenAmount,
+          inputToken,
+          inputTokenPrice,
+          outputToken,
+          outputTokenPrice,
+          nativeTokenPrice,
+          slippage: 0,
+        },
+        publicClient,
+        signer,
+      )
+      console.log(
+        inputTokenAmount.toString(),
+        quoteIssuance?.indexTokenAmount.toString(),
+        quoteIssuance?.inputOutputTokenAmount.toString(),
+        'issuance-quote',
+      )
+    }
+    fetchQuote()
+  }, [
+    inputToken,
+    inputTokenAmount,
+    isDepositing,
+    nativeTokenPrice,
+    preSaleCurrencyToken,
+    preSaleToken,
+    provider,
+    publicClient,
+    signer,
+  ])
 
   return (
     <DepositContext.Provider
