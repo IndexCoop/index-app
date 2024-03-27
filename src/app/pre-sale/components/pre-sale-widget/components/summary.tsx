a8ba1661d50076685ca0abe37ad26e64b268ad6c
JD <jann.driessen@gmx.de>
2024-03-27 13:42:21 +0100

    add gas fees component

--- src/app/pre-sale/components/pre-sale-widget/components/summary.tsx
+++ src/app/pre-sale/components/pre-sale-widget/components/summary.tsx
@@ -1,6 +1,8 @@
 import { Disclosure } from '@headlessui/react'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
+import { GasFees } from '@/components/gas-fees'
+
 type SummaryQuoteProps = {
   label: string
   value: string
@@ -12,7 +14,7 @@ function SummaryQuote(props: SummaryQuoteProps) {
     <div className='text-ic-gray-300 flex flex-row items-center justify-between text-xs'>
       <div className='font-medium'>{props.label}</div>
       <div className='flex flex-row gap-1'>
-        <div className='font-bold'>{props.value}</div>
+        <div className='text-ic-white font-bold'>{props.value}</div>
         <div className='font-normal'>{props.valueUsd}</div>
       </div>
     </div>
@@ -29,13 +31,21 @@ export function Summary() {
               <span className='text-xs font-medium'>
                 {open ? 'Summary' : ''}
               </span>
-              <span className='ml-6 flex h-7 items-center'>
-                {open ? (
-                  <ChevronUpIcon className='h-6 w-6' aria-hidden='true' />
-                ) : (
-                  <ChevronDownIcon className='h-6 w-6' aria-hidden='true' />
-                )}
-              </span>
+              <div className='flex flex-row items-center gap-1'>
+                {!open ? (
+                  <GasFees
+                    valueUsd={'$8.79'}
+                    styles={{ valueUsdTextColor: 'text-ic-gray-300' }}
+                  />
+                ) : null}
+                <span className='flex items-center'>
+                  {open ? (
+                    <ChevronUpIcon className='h-6 w-6' aria-hidden='true' />
+                  ) : (
+                    <ChevronDownIcon className='h-6 w-6' aria-hidden='true' />
+                  )}
+                </span>
+              </div>
             </Disclosure.Button>
           </dt>
           <Disclosure.Panel as='dd' className='mt-2 flex flex-col gap-2'>
@@ -49,6 +59,12 @@ export function Summary() {
               value={'10 ETH2x'}
               valueUsd={'($23,593.52)'}
             />
+            <div className='text-ic-gray-300 flex flex-row items-center justify-between text-xs'>
+              <div className='font-normal'>Network Fee</div>
+              <div>
+                <GasFees valueUsd={'$8.79'} value={'(0.04 ETH)'} />
+              </div>
+            </div>
           </Disclosure.Panel>
         </div>
       )}
