8a899c8d4918122b7c99cc13f1c9881326124d27
JD <jann.driessen@gmx.de>
2024-03-27 11:47:55 +0100

    add summary quote component

--- src/app/pre-sale/components/pre-sale-widget/components/summary.tsx
+++ src/app/pre-sale/components/pre-sale-widget/components/summary.tsx
@@ -1,6 +1,24 @@
 import { Disclosure } from '@headlessui/react'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
+type SummaryQuoteProps = {
+  label: string
+  value: string
+  valueUsd: string
+}
+
+function SummaryQuote(props: SummaryQuoteProps) {
+  return (
+    <div className='text-ic-gray-300 flex flex-row items-center justify-between text-xs'>
+      <div className='font-medium'>{props.label}</div>
+      <div className='flex flex-row gap-1'>
+        <div className='font-bold'>{props.value}</div>
+        <div className='font-normal'>{props.valueUsd}</div>
+      </div>
+    </div>
+  )
+}
+
 export function Summary() {
   return (
     <Disclosure as='div' className='rounded-xl border border-[#3A6060]'>
@@ -20,10 +38,17 @@ export function Summary() {
               </span>
             </Disclosure.Button>
           </dt>
-          <Disclosure.Panel as='dd' className='mt-2 pr-12'>
-            <p className='text-ic-gray-600 text-sm font-medium leading-6'>
-              {''}
-            </p>
+          <Disclosure.Panel as='dd' className='mt-2 flex flex-col gap-2'>
+            <SummaryQuote
+              label='Pay'
+              value={'9.94 ETH'}
+              valueUsd={'($23,593.52)'}
+            />
+            <SummaryQuote
+              label='Receive'
+              value={'10 ETH2x'}
+              valueUsd={'($23,593.52)'}
+            />
           </Disclosure.Panel>
         </div>
       )}
