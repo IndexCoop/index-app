b601794e367e2d822a0ef9521a7edb07724be0fc
JD <jann.driessen@gmx.de>
2024-04-02 10:29:34 +0200

    add bottom message comp

---
+++ src/components/swap/components/transaction-review/components/bottom-message.tsx
@@ -0,0 +1,8 @@
+export function BottomMessage() {
+  return (
+    <div className='text-ic-gray-600 mb-4 mt-2 text-center text-sm font-medium'>
+      Submitting will open your currently connected wallet to confirm and sign
+      the transaction.
+    </div>
+  )
+}
