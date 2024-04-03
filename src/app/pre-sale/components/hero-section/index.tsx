76108ef14ac8f5c02551ff27ed78835775911d0c
JD <jann.driessen@gmx.de>
2024-04-03 16:34:02 +0200

    update hero section

--- src/app/pre-sale/components/hero-section/index.tsx
+++ src/app/pre-sale/components/hero-section/index.tsx
@@ -2,16 +2,13 @@ export function HeroSection() {
   return (
     <div className='flex flex-col space-y-4'>
       <h2 className='text-ic-black text-xl font-semibold tracking-tight'>
-        Open Pre-sales (Beta)
+        Product Pre-Sales
       </h2>
       <p className='text-ic-gray-800 text-sm font-medium leading-6'>
-        Join one of our pre-sales and earn product revenue tokens (PRTs) in
-        return. Simply deposit funds into the your chosen product vault - if the
-        pre-sale threshold is met, you will get your product revenue tokens via
-        airdrop. PRTs can be staked to distribute an Index products’ revenue
-        amongst all PRT holders*. If the pre-sale threshold is not met within
-        the timeframe, your deposits will be returned alongside an $INDEX
-        reward.
+        Participate in pre-sales to demonstrate demand for potential products
+        and receive Product Revenue Tokens (PRTs) in return! Deposit to a
+        pre-sale and earn PRTs if the target threshold is met. For successful
+        products, PRTs can be staked* to receive a share of product revenue.
       </p>
     </div>
   )
