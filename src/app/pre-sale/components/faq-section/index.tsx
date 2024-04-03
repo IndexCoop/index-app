8088f9656e681ef82af2be49a0cdb021601f2228
JD <jann.driessen@gmx.de>
2024-04-03 16:47:53 +0200

    update faq section texts and add new lines

--- src/app/pre-sale/components/faq-section/index.tsx
+++ src/app/pre-sale/components/faq-section/index.tsx
@@ -17,12 +17,12 @@ const faqs = [
   {
     question: 'How high are my rewards?',
     answer:
-      'Depending on whether the deposit threshold has been met, you will either receive product revenue tokens or $INDEX rewards. The $INDEX rewards are specified in each presale on the list above. The amount of PRTs distributed will be set per day and PRT rewards will be calculated on a pro rate basis amongst all depositors. The amount of PRT tokens given out for each pre-sale can vary by product. For each product there will be a total of 10,000 PRTs which will share the respective token’s revenue.',
+      'There is a fixed supply of 10,000 PRTs for all products with PRTs. The presale will show how many PRTs will be distributed in the event of a success, meeting the threshold by the deadline. For example, if 3,000 PRTs are distributed in the presale, 30% are allocated for presale depositors.\nEach presale will have a distribution curve for PRTs, and the distribution curve determines your rewards. You can track your amount of PRTs earned in the staking widget. Typically, the more you deposit and the earlier you deposit, the higher your PRT allocation will be. Still, other factors exist, such as the total amount of capital deposited during the presale.',
   },
   {
     question: 'Will my funds be locked up?',
     answer:
-      'No. You can withdraw your deposit at any time. If you choose to keep your funds deposited after a successful pre-sale is completed, they will automatically convert into the new token, which will be tradable via our app.',
+      'No, you may withdraw at any time during and after a pre-sale. However, if you withdraw before a pre-sale is completed, you forfeit any accrued PRT rewards. Pre-sales may require participants to maintain deposits for a specified amount of time to be eligible for PRT staking also.\nIf a pre-sale is successful, your original deposit will be transformed into the ultimate product token, so no action will be required. At that point, the product token will be tradeable via the Index Coop app.',
   },
 ]
 
@@ -33,36 +33,46 @@ export function FaqSection() {
         FAQ
       </h2>
       <dl className='divide-ic-gray-900/10 mt-6 space-y-6 divide-y'>
-        {faqs.map((faq) => (
-          <Disclosure as='div' key={faq.question} className='pt-6'>
-            {({ open }) => (
-              <>
-                <dt>
-                  <Disclosure.Button className='text-ic-gray-800 flex w-full items-start justify-between text-left'>
-                    <span className='text-base font-semibold leading-7'>
-                      {faq.question}
-                    </span>
-                    <span className='ml-6 flex h-7 items-center'>
-                      {open ? (
-                        <ChevronUpIcon className='h-6 w-6' aria-hidden='true' />
-                      ) : (
-                        <ChevronDownIcon
-                          className='h-6 w-6'
-                          aria-hidden='true'
-                        />
-                      )}
-                    </span>
-                  </Disclosure.Button>
-                </dt>
-                <Disclosure.Panel as='dd' className='mt-2 pr-12'>
-                  <p className='text-ic-gray-600 text-sm font-medium leading-6'>
-                    {faq.answer}
-                  </p>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-        ))}
+        {faqs.map((faq) => {
+          const answerParagraphs = faq.answer.split('\n')
+          return (
+            <Disclosure as='div' key={faq.question} className='pt-6'>
+              {({ open }) => (
+                <>
+                  <dt>
+                    <Disclosure.Button className='text-ic-gray-800 flex w-full items-start justify-between text-left'>
+                      <span className='text-base font-semibold leading-7'>
+                        {faq.question}
+                      </span>
+                      <span className='ml-6 flex h-7 items-center'>
+                        {open ? (
+                          <ChevronUpIcon
+                            className='h-6 w-6'
+                            aria-hidden='true'
+                          />
+                        ) : (
+                          <ChevronDownIcon
+                            className='h-6 w-6'
+                            aria-hidden='true'
+                          />
+                        )}
+                      </span>
+                    </Disclosure.Button>
+                  </dt>
+                  <Disclosure.Panel as='dd' className='mt-2 pr-12'>
+                    <p className='text-ic-gray-600 text-sm font-medium leading-6'>
+                      {answerParagraphs.map((paragraph, index) => (
+                        <p className='mb-2' key={index}>
+                          {paragraph}
+                        </p>
+                      ))}
+                    </p>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          )
+        })}
       </dl>
     </div>
   )
