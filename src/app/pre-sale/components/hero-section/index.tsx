4ffa3960be13c3f695634bcd16cf298f16721eea
JD <jann.driessen@gmx.de>
2024-04-08 10:16:07 +0200

    update presale description

--- src/app/pre-sale/components/hero-section/index.tsx
+++ src/app/pre-sale/components/hero-section/index.tsx
@@ -5,8 +5,8 @@ export function HeroSection() {
         Product Pre-Sales
       </h2>
       <p className='text-ic-gray-800 text-sm font-medium leading-6'>
-        Participate in pre-sales to demonstrate demand for potential products
-        and receive Product Revenue Tokens (PRTs) in return! Deposit to a
+        Participate in pre-sales for a chance to gain upside in future product
+        success in the form of Product Revenue Tokens (PRTs)! Deposit to a
         pre-sale and earn PRTs if the target threshold is met. For successful
         products, PRTs can be staked* to receive a share of product revenue.
       </p>
