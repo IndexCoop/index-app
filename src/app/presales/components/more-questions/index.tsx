6089371cff9de13dad19ba005f6471ee318b020f
JD <jann.driessen@gmx.de>
2024-04-10 09:49:20 +0100

    add blog links

--- src/app/presales/components/more-questions/index.tsx
+++ src/app/presales/components/more-questions/index.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 
-// FIXME: add blog link
 export function MoreQuestions() {
   return (
     <div className='text-ic-gray-600 mt-16 text-sm font-medium'>
       Have more questions? More presale info{' '}
-      <Link className='underline' href='' target='_blank'>
+      <Link
+        className='underline'
+        href='https://indexcoop.com/blog/hyeth-pre-sale-faqs'
+        target='_blank'
+      >
         here
       </Link>
     </div>
