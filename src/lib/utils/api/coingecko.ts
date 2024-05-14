20dd103c60ddca3d007eb93d5515c9c9d0b05622
JD <jann.driessen@gmx.de>
2024-05-14 15:13:10 +0100

    add coingecko asset platform for arbitrum

--- src/lib/utils/api/coingecko.ts
+++ src/lib/utils/api/coingecko.ts
@@ -1,16 +1,23 @@
 import { indicesTokenList } from '@/constants/tokenlists'
 import { IndexApi } from '@/lib/utils/api/index-api'
 
-import { OPTIMISM, POLYGON } from '../../../constants/chains'
+import { ARBITRUM, OPTIMISM, POLYGON } from '../../../constants/chains'
 import { ETH } from '../../../constants/tokens'
 
 const baseURL = '/coingecko'
 const indexApi = new IndexApi()
 
 const getAssetPlatform = (chainId: number) => {
-  if (chainId === POLYGON.chainId) return 'polygon-pos'
-  if (chainId === OPTIMISM.chainId) return 'optimistic-ethereum'
-  return 'ethereum'
+  switch (chainId) {
+    case ARBITRUM.chainId:
+      return 'arbitrum-one'
+    case POLYGON.chainId:
+      return 'polygon-pos'
+    case OPTIMISM.chainId:
+      return 'optimistic-ethereum'
+    default:
+      return 'ethereum'
+  }
 }
 
 export const fetchCoingeckoTokenPrice = async (
