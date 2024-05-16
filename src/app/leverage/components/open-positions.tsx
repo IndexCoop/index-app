3cec525d8b6f6a49f132645e8092a43f729e47fc
0xonramp <0xonramp@gmail.com>
2024-05-16 07:03:50 -0400

    Add balances

--- src/app/leverage/components/open-positions.tsx
+++ src/app/leverage/components/open-positions.tsx
@@ -1,9 +1,40 @@
+import { useAccount } from 'wagmi'
+
+import {
+  IndexCoopBitcoin2xIndex,
+  IndexCoopBitcoin3xIndex,
+  IndexCoopEthereum2xIndex,
+  IndexCoopEthereum3xIndex,
+  IndexCoopInverseBitcoinIndex,
+  IndexCoopInverseEthereumIndex,
+} from '@/constants/tokens'
+import { useBalances } from '@/lib/hooks/use-balance'
+
+const leverageTokenAddresses = [
+  IndexCoopBitcoin2xIndex,
+  IndexCoopBitcoin3xIndex,
+  IndexCoopEthereum2xIndex,
+  IndexCoopEthereum3xIndex,
+  IndexCoopInverseBitcoinIndex,
+  IndexCoopInverseEthereumIndex,
+].map((token) => token.arbitrumAddress ?? '')
+
 export function OpenPositions() {
+  const { address } = useAccount()
+  const balances = useBalances(address, leverageTokenAddresses)
+
   return (
     <div className='border-ic-gray-600 w-full rounded-3xl border bg-[#1C2C2E]'>
       <h3 className='text-ic-white p-6 text-[14px] font-bold md:p-8'>
         Open Positions
       </h3>
+      {balances.length > 0 && (
+        <div className='p-6'>
+          {balances.map((balance) => (
+            <div key={balance.token}>{balance.toString()}</div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
