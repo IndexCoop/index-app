24cc6ecdde9d0fca147d89563ddf2c9000000ce9
0xonramp <0xonramp@gmail.com>
2024-05-15 22:50:03 -0400

    feat: Open positions

---
+++ src/app/leverage/components/open-positions.tsx
@@ -0,0 +1,9 @@
+export function OpenPositions() {
+  return (
+    <div className='border-ic-gray-600 w-full rounded-3xl border bg-[#1C2C2E]'>
+      <h3 className='text-ic-white p-6 text-[14px] font-bold md:p-8'>
+        Open Positions
+      </h3>
+    </div>
+  )
+}
