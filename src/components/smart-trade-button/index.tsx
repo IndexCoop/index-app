5e32f4970f5751370f5a4a43cfe24d984a934cb2
JD <jann.driessen@gmx.de>
2024-05-17 12:21:26 +0100

    add props

--- src/components/smart-trade-button/index.tsx
+++ src/components/smart-trade-button/index.tsx
@@ -3,15 +3,43 @@ import { useMemo } from 'react'
 import { useTradeButton } from '@/components/swap/hooks/use-trade-button'
 import { useTradeButtonState } from '@/components/swap/hooks/use-trade-button-state'
 import { TradeButton } from '@/components/trade-button'
+import { Token } from '@/constants/tokens'
 import { useApproval } from '@/lib/hooks/use-approval'
+import { useNetwork } from '@/lib/hooks/use-network'
 import { getNativeToken } from '@/lib/utils/tokens'
 
-export function SmartTradeButton() {
+type SmartTradeButtonProps = {
+  inputToken: Token
+  outputToken: Token
+  inputTokenAmount: bigint
+  inputValue: string
+  contract: string
+  hasFetchingError: boolean
+  hasInsufficientFunds: boolean
+  isFetchingQuote: boolean
+  isSupportedNetwork: boolean
+}
+
+export function SmartTradeButton(props: SmartTradeButtonProps) {
+  const {
+    contract,
+    hasFetchingError,
+    hasInsufficientFunds,
+    inputTokenAmount,
+    inputToken,
+    inputValue,
+    isFetchingQuote,
+    isSupportedNetwork,
+    outputToken,
+  } = props
+
+  const { chainId } = useNetwork()
+
   const {
     isApproved: isApprovedForSwap,
     isApproving: isApprovingForSwap,
     approve: onApproveForSwap,
-  } = useApproval(inputToken, contract, inputTokenAmountWei.toBigInt())
+  } = useApproval(inputToken, contract, inputTokenAmount)
 
   const shouldApprove = useMemo(() => {
     const nativeToken = getNativeToken(chainId)
@@ -27,16 +55,20 @@ export function SmartTradeButton() {
     isApprovedForSwap,
     isApprovingForSwap,
     outputToken,
-    sellTokenAmount,
+    inputValue,
   )
   const { buttonLabel, isDisabled } = useTradeButton(buttonState)
+
+  const onClick = () => {
+    // TODO:
+  }
 
   return (
     <TradeButton
       label={buttonLabel}
       isDisabled={isDisabled}
-      isLoading={isApprovingForSwap || isFetchingAnyQuote}
-      onClick={onClickTradeButton}
+      isLoading={isApprovingForSwap || isFetchingQuote}
+      onClick={onClick}
     />
   )
 }
