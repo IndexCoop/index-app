4e22b9a38d8598d33ae1ef85b2b2dfa7e4d8c0a5
0xonramp <0xonramp@gmail.com>
2024-05-19 22:45:42 -0400

    Token formatting

--- src/app/leverage/components/open-positions.tsx
+++ src/app/leverage/components/open-positions.tsx
@@ -1,3 +1,6 @@
+import { BigNumber } from 'ethers'
+import Image from 'next/image'
+import { useMemo } from 'react'
 import { useAccount } from 'wagmi'
 
 import {
@@ -7,34 +10,82 @@ import {
   IndexCoopEthereum3xIndex,
   IndexCoopInverseBitcoinIndex,
   IndexCoopInverseEthereumIndex,
+  Token,
 } from '@/constants/tokens'
 import { useBalances } from '@/lib/hooks/use-balance'
+import { displayFromWei } from '@/lib/utils'
 
-const leverageTokenAddresses = [
+const leverageTokens = [
   IndexCoopBitcoin2xIndex,
   IndexCoopBitcoin3xIndex,
   IndexCoopEthereum2xIndex,
   IndexCoopEthereum3xIndex,
   IndexCoopInverseBitcoinIndex,
   IndexCoopInverseEthereumIndex,
-].map((token) => token.arbitrumAddress ?? '')
+]
+
+type TokenBalance = Token & { balance: string | null }
+
+const leverageTokenAddresses = leverageTokens
+  .map((token) => token.arbitrumAddress ?? '')
+  .filter((address) => address.length > 0)
 
 export function OpenPositions() {
   const { address } = useAccount()
   const balances = useBalances(address, leverageTokenAddresses)
+  const tokens = useMemo(() => {
+    if (balances.length === 0) return []
+
+    return balances.reduce((acc, current) => {
+      const token = leverageTokens.find(
+        (leverageToken) => current.token === leverageToken.arbitrumAddress,
+      )
+      return token
+        ? [
+            ...acc,
+            {
+              ...token,
+              balance: displayFromWei(
+                BigNumber.from(current.value.toString()),
+                3,
+                token.decimals,
+              ),
+            },
+          ]
+        : acc
+    }, [] as TokenBalance[])
+  }, [balances])
+
+  if (tokens.length === 0) return null
 
   return (
-    <div className='border-ic-gray-600 w-full rounded-3xl border bg-[#1C2C2E]'>
-      <h3 className='text-ic-white p-6 text-[14px] font-bold md:p-8'>
-        Open Positions
-      </h3>
-      {balances.length > 0 && (
-        <div className='p-6'>
-          {balances.map((balance) => (
-            <div key={balance.token}>{balance.toString()}</div>
-          ))}
-        </div>
-      )}
+    <div className='border-ic-gray-600 w-full rounded-3xl border bg-[#1C2C2E] lg:max-w-[67%]'>
+      <h3 className='text-ic-white p-6 font-bold'>Open Positions</h3>
+      <div className='border-ic-gray-600 flex w-full border-b px-6 pb-3'>
+        <div className='text-ic-gray-400 w-1/2'>Position</div>
+        <div className='text-ic-gray-400 w-1/2'>Balance</div>
+      </div>
+      <div className='divide-ic-gray-900/20 divide-y-4 py-2'>
+        {tokens.map((token) => (
+          <div
+            key={token.symbol}
+            className='text-ic-white flex h-14 w-full px-6'
+          >
+            <div className='flex w-1/2'>
+              <div className='my-auto mr-2 overflow-hidden rounded-full'>
+                <Image
+                  src={token.image}
+                  alt={`${token.symbol} logo`}
+                  height={30}
+                  width={30}
+                />
+              </div>
+              <div className='my-auto font-medium'>{token.symbol}</div>
+            </div>
+            <div className='flex w-1/2 items-center'>{token.balance}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
