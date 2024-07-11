bb44d8703c55e0e964fed2256a82bdd9e9f14d31
0xonramp <0xonramp@gmail.com>
2024-07-11 08:26:45 -0400

    Update staked token

--- src/app/prt-staking/constants.ts
+++ src/app/prt-staking/constants.ts
@@ -6,11 +6,7 @@ export const prts: ProductRevenueToken[] = [
   {
     rewardTokenData: getIndexTokenData('hyETH')!,
     stakeTokenData: getIndexTokenData('prtHyETH')!,
-    stakedTokenData: {
-      // FIXME: Use staked token once deployed
-      ...getIndexTokenData('prtHyETH')!,
-      address: '0x2881cc01aAD96C70319f308906D49c3784112E0D',
-    },
+    stakedTokenData: getIndexTokenData('sPrtHyETH')!,
     description:
       'Designed to track the performance of the largest high-yield opportunities for ETH on Ethereum mainnet.',
     moreInfoUrl: 'https://indexcoop.com/products/high-yield-eth',
