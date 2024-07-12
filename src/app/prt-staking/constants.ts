41e21e70c2bf424b72d2128241097011be551731
0xonramp <0xonramp@gmail.com>
2024-07-12 14:53:31 -0400

    Revert token changes

--- src/app/prt-staking/constants.ts
+++ src/app/prt-staking/constants.ts
@@ -4,18 +4,9 @@ import { ProductRevenueToken } from '@/app/prt-staking/types'
 
 export const prts: ProductRevenueToken[] = [
   {
-    rewardTokenData: {
-      ...getIndexTokenData('hyETH')!,
-      address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-    },
-    stakeTokenData: {
-      ...getIndexTokenData('prtHyETH')!,
-      address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    },
-    stakedTokenData: {
-      ...getIndexTokenData('sPrtHyETH')!,
-      address: '0xE385E4225008198020316FbBA7Afe34dEd105553',
-    },
+    rewardTokenData: getIndexTokenData('hyETH')!,
+    stakeTokenData: getIndexTokenData('prtHyETH')!,
+    stakedTokenData: getIndexTokenData('sPrtHyETH')!,
     description:
       'Designed to track the performance of the largest high-yield opportunities for ETH on Ethereum mainnet.',
     moreInfoUrl: 'https://indexcoop.com/products/high-yield-eth',
