5bc78b568e92ca9eb668b0ad70ca27a623f0336e
0xonramp <0xonramp@gmail.com>
2024-07-12 13:02:11 -0400

    Update staging tokens

--- src/app/prt-staking/constants.ts
+++ src/app/prt-staking/constants.ts
@@ -4,9 +4,18 @@ import { ProductRevenueToken } from '@/app/prt-staking/types'
 
 export const prts: ProductRevenueToken[] = [
   {
-    rewardTokenData: getIndexTokenData('hyETH')!,
-    stakeTokenData: getIndexTokenData('prtHyETH')!,
-    stakedTokenData: getIndexTokenData('sPrtHyETH')!,
+    rewardTokenData: {
+      ...getIndexTokenData('hyETH')!,
+      address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    },
+    stakeTokenData: {
+      ...getIndexTokenData('prtHyETH')!,
+      address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    },
+    stakedTokenData: {
+      ...getIndexTokenData('sPrtHyETH')!,
+      address: '0x3329efE11A11F63AD2caE877dF640E377024Cd42',
+    },
     description:
       'Designed to track the performance of the largest high-yield opportunities for ETH on Ethereum mainnet.',
     moreInfoUrl: 'https://indexcoop.com/products/high-yield-eth',
