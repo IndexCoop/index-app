74e1dc1e61600c7029be2953cdf7ea0165da86c8
0xonramp <0xonramp@gmail.com>
2024-07-29 08:33:59 -0400

    Swap out nav provider

--- src/lib/utils/api/nav.ts
+++ src/lib/utils/api/nav.ts
@@ -1,11 +1,23 @@
-import { IndexApi } from './index-api'
+import { getIndexTokenData } from '@indexcoop/tokenlists'
+
+import {
+  IndexDataMetric,
+  IndexDataProvider,
+} from '@/lib/utils/api/index-data-provider'
 
 export class NavProvider {
   async getNavPrice(tokenSymbol: string, chainId = 1): Promise<number> {
-    const indexApi = new IndexApi()
-    const symbol = tokenSymbol.replace(/-/g, '').toLowerCase()
-    const path = `/${chainId}/${symbol}/nav`
-    const res = await indexApi.get(path)
-    return res.nav
+    const token = getIndexTokenData(tokenSymbol, chainId)
+    if (!token) throw new Error(`Index token not found: ${tokenSymbol}`)
+
+    const indexDataProvider = new IndexDataProvider()
+    const res = await indexDataProvider.getTokenMetrics({
+      tokenAddress: token.address,
+      chainId,
+      metrics: [IndexDataMetric.Nav],
+    })
+
+    // TODO: Determine if this should be 0 or throw
+    return res?.nav ?? 0
   }
 }
