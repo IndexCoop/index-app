7c6c9ca2770246d6d4631570a49f1afcf042ad7a
JD <jann.driessen@gmx.de>
2024-08-26 08:27:13 +0200

    feat: add get leverage tokens for chainid

--- src/app/leverage/constants.ts
+++ src/app/leverage/constants.ts
@@ -1,3 +1,5 @@
+import { getLeverageBaseToken } from '@/app/leverage/utils/get-leverage-base-token'
+import { getLeverageType } from '@/app/leverage/utils/get-leverage-type'
 import {
   IndexCoopBitcoin2xIndex,
   IndexCoopBitcoin3xIndex,
@@ -7,6 +9,9 @@ import {
   IndexCoopInverseEthereumIndex,
 } from '@/constants/tokens'
 import { isAddress } from '@/lib/utils'
+import { getAddressForToken } from '@/lib/utils/tokens'
+
+import { LeverageToken } from './types'
 
 export const ethLeverageTokens = [
   IndexCoopEthereum2xIndex,
@@ -22,6 +27,25 @@ export const btcLeverageTokens = [
 
 export const leverageTokens = [...ethLeverageTokens, ...btcLeverageTokens]
 
-export const leverageTokenAddresses = leverageTokens
-  .map((token) => token.arbitrumAddress ?? '')
-  .filter((arbitrumAddress) => isAddress(arbitrumAddress))
+export function getLeverageTokens(chainId: number): LeverageToken[] {
+  const tokens = leverageTokens.map((token) => {
+    const baseToken = getLeverageBaseToken(token.symbol)
+    const address = getAddressForToken(token, chainId)
+    const leverageType = getLeverageType(token.symbol)
+    if (
+      !baseToken ||
+      !address ||
+      !isAddress(address) ||
+      leverageType === null
+    ) {
+      return null
+    }
+    return {
+      ...token,
+      address,
+      baseToken: baseToken.symbol,
+      leverageType,
+    }
+  })
+  return tokens.filter((token) => token !== null)
+}
