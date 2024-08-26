8fa537147ced604ec2bc8a53fa95e6965c9912d2
0xonramp <0xonramp@gmail.com>
2024-08-26 12:12:59 -0400

    Revert nav change

--- src/lib/utils/api/nav.ts
+++ src/lib/utils/api/nav.ts
@@ -1,23 +1,11 @@
-import { getIndexTokenData } from '@indexcoop/tokenlists'
-
-import {
-  IndexDataMetric,
-  IndexDataProvider,
-} from '@/lib/utils/api/index-data-provider'
+import { IndexApi } from './index-api'
 
 export class NavProvider {
   async getNavPrice(tokenSymbol: string, chainId = 1): Promise<number> {
-    const token = getIndexTokenData(tokenSymbol, chainId)
-    if (!token) throw new Error(`Index token not found: ${tokenSymbol}`)
-
-    const indexDataProvider = new IndexDataProvider()
-    const res = await indexDataProvider.getTokenMetrics({
-      tokenAddress: token.address,
-      chainId,
-      metrics: [IndexDataMetric.Nav],
-    })
-
-    // TODO: Determine if this should be 0 or throw
-    return res?.nav ?? 0
+    const indexApi = new IndexApi()
+    const symbol = tokenSymbol.replace(/-/g, '').toLowerCase()
+    const path = `/${chainId}/${symbol}/nav`
+    const res = await indexApi.get(path)
+    return res.nav
   }
 }
