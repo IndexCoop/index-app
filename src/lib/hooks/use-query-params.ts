2b920878b8ac41608123c21f81ee285cd382bdc3
tokdaniel <tokdaniel4@gmail.com>
2024-09-18 16:08:56 +0200

    handle if invalid token configurations is sent

--- src/lib/hooks/use-query-params.ts
+++ src/lib/hooks/use-query-params.ts
@@ -1,11 +1,11 @@
 'use client'
 
-import { Token } from '@indexcoop/flash-mint-sdk'
 import { useSearchParams } from 'next/navigation'
 import { useMemo } from 'react'
 
 import { getCurrencyTokens, getLeverageTokens } from '@/app/leverage/constants'
 import { LeverageToken, LeverageType } from '@/app/leverage/types'
+import { Token } from '@/constants/tokens'
 import { chains } from '@/lib/utils/wagmi'
 
 type DefaultParams = {
@@ -32,38 +32,42 @@ export const useQueryParams = <T extends Partial<DefaultParams>>(
     const buy = searchParams.get('buy') || ''
     const sell = searchParams.get('sell') || ''
 
-    const queryNetwork =
-      chains.find((chain) => chain.id === network)?.id ?? defaultParams.network
+    const queryNetwork = chains.find((chain) => chain.id === network)?.id
 
-    const tokens = getCurrencyTokens(queryNetwork ?? 0).concat(
-      getLeverageTokens(network),
+    const currencyTokens = getCurrencyTokens(queryNetwork ?? 0)
+    const leverageTokens = getLeverageTokens(queryNetwork ?? 0)
+
+    let queryOutputToken: Token | undefined = currencyTokens.find(
+      (token) => token.symbol.toLowerCase() === buy.toLowerCase(),
     )
 
-    const queryOutputToken =
-      tokens.find(
+    let queryInputToken: Token | undefined = leverageTokens.find(
+      (token) => token.symbol.toLowerCase() === sell.toLowerCase(),
+    )
+
+    if (!queryInputToken || !queryOutputToken) {
+      queryOutputToken = leverageTokens.find(
         (token) => token.symbol.toLowerCase() === buy.toLowerCase(),
-      ) ?? defaultParams.outputToken
-    const queryInputToken =
-      tokens.find(
+      )
+
+      queryInputToken = currencyTokens.find(
         (token) => token.symbol.toLowerCase() === sell.toLowerCase(),
-      ) ?? defaultParams.inputToken
+      )
+    }
 
-    const queryIsMinting = queryOutputToken
-      ? 'leverageType' in queryOutputToken
-      : defaultParams.isMinting
+    const queryIsMinting =
+      queryOutputToken && 'leverageType' in queryOutputToken
 
-    const queryLeverageType =
-      (queryIsMinting
-        ? (queryOutputToken as LeverageToken)?.leverageType
-        : (queryInputToken as LeverageToken)?.leverageType) ??
-      defaultParams.leverageType
+    const queryLeverageType = queryIsMinting
+      ? (queryOutputToken as LeverageToken)?.leverageType
+      : (queryInputToken as LeverageToken)?.leverageType
 
     return {
-      queryIsMinting,
-      queryLeverageType,
-      queryNetwork,
-      queryInputToken,
-      queryOutputToken,
+      queryIsMinting: queryIsMinting ?? defaultParams.isMinting,
+      queryLeverageType: queryLeverageType ?? defaultParams.leverageType,
+      queryNetwork: queryNetwork ?? defaultParams.network,
+      queryInputToken: queryInputToken ?? defaultParams.inputToken,
+      queryOutputToken: queryOutputToken ?? defaultParams.outputToken,
     } as ReturnType<T>
     // NOTE: defaultQueryPararms should only be read initially.
     // eslint-disable-next-line react-hooks/exhaustive-deps
