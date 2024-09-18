a55585ea9955cf041b12427d967f527c0237da0e
tokdaniel <tokdaniel4@gmail.com>
2024-09-18 16:13:32 +0200

    separate conditions if we dont find any input or outputtoken

--- src/lib/hooks/use-query-params.ts
+++ src/lib/hooks/use-query-params.ts
@@ -45,13 +45,15 @@ export const useQueryParams = <T extends Partial<DefaultParams>>(
       (token) => token.symbol.toLowerCase() === sell.toLowerCase(),
     )
 
-    if (!queryInputToken || !queryOutputToken) {
-      queryOutputToken = leverageTokens.find(
-        (token) => token.symbol.toLowerCase() === buy.toLowerCase(),
-      )
-
+    if (!queryInputToken) {
       queryInputToken = currencyTokens.find(
         (token) => token.symbol.toLowerCase() === sell.toLowerCase(),
+      )
+    }
+
+    if (!queryOutputToken) {
+      queryOutputToken = leverageTokens.find(
+        (token) => token.symbol.toLowerCase() === buy.toLowerCase(),
       )
     }
 
