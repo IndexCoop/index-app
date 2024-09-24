4217230c0d75b7bcb78c1ba080de3d8a9a553960
JD <jann.driessen@gmx.de>
2024-09-24 10:52:07 +0100

    refactor: add output exception for icRETH

--- src/app/legacy/providers/utils.ts
+++ src/app/legacy/providers/utils.ts
@@ -4,12 +4,16 @@ import {
 } from '@indexcoop/tokenlists'
 import { Address, parseAbi, PublicClient } from 'viem'
 
-import { Token } from '@/constants/tokens'
+import { LeveragedRethStakingYield, RETH, Token } from '@/constants/tokens'
+import { isSameAddress } from '@/lib/utils'
 
 export async function getOutputToken(
   indexToken: Address,
   publicClient: PublicClient,
 ): Promise<Token | null> {
+  if (isSameAddress(indexToken, LeveragedRethStakingYield.address!)) {
+    return RETH
+  }
   const components = await publicClient.readContract({
     address: indexToken,
     abi: parseAbi(['function getComponents() view returns (address[])']),
