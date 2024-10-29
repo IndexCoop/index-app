dcbb90ee705daa91d29ce6ad0116600af27d170b
0xonramp <0xonramp@gmail.com>
2024-10-29 11:17:39 -0400

    Add analytics

--- src/components/charts/period-selector.tsx
+++ src/components/charts/period-selector.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import { Dispatch, SetStateAction } from 'react'
 
 import { ChartPeriod } from '@/components/charts/types'
+import { useAnalytics } from '@/lib/hooks/use-analytics'
 
 const periods = [
   ChartPeriod.Hour,
@@ -17,6 +18,13 @@ type Props = {
 }
 
 export function PeriodSelector({ selectedPeriod, setSelectedPeriod }: Props) {
+  const { logEvent } = useAnalytics()
+
+  const handleClick = (period: ChartPeriod) => {
+    setSelectedPeriod(period)
+    logEvent('Chart Period Selected', { period })
+  }
+
   return (
     <div className='ml-auto flex space-x-1 text-sm font-medium md:space-x-2'>
       {periods.map((period) => (
@@ -26,7 +34,7 @@ export function PeriodSelector({ selectedPeriod, setSelectedPeriod }: Props) {
             'text-ic-gray-50 flex size-9 cursor-pointer items-center justify-center rounded-lg text-sm font-semibold',
             selectedPeriod === period && 'bg-ic-gray-200 !text-ic-gray-950',
           )}
-          onClick={() => setSelectedPeriod(period)}
+          onClick={() => handleClick(period)}
         >
           {period}
         </div>
