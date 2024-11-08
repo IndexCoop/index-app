ce089a76ff5c5c4fe89ebc436547fbab7c85836e
0xonramp <0xonramp@gmail.com>
2024-11-08 10:28:03 -0500

    Light mode selector

--- src/components/charts/price-chart.tsx
+++ src/components/charts/price-chart.tsx
@@ -23,13 +23,10 @@ export function PriceChart({ indexToken, nav }: Props) {
     usePriceChartData(tokenAddress)
 
   return (
-    <div className='border-ic-gray-600 flex h-full w-full flex-col rounded-3xl border bg-[#1C2C2E]'>
-      <div className='text-ic-white flex w-full items-stretch px-4 pt-4 md:px-8 md:pt-6'>
+    <div className='border-ic-gray-200 dark:border-ic-gray-600 flex h-full w-full flex-col rounded-3xl border bg-[#F7F8F8] dark:bg-[#1C2C2E]'>
+      <div className='text-ic-gray-800 dark:text-ic-white flex w-full items-stretch px-4 pt-4 md:px-8 md:pt-6'>
         <div className='basis-1/2 self-center text-lg font-semibold md:basis-1/4 md:text-2xl'>
           {formatDollarAmount(nav, true)}
-        </div>
-        <div className='my-auto hidden text-center text-2xl font-medium md:flex'>
-          {indexToken.symbol}
         </div>
         <PeriodSelector
           selectedPeriod={selectedPeriod}
