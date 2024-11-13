4cb15ec515b2c292e0a787c105069c3e5e43d0da
JD <jann.driessen@gmx.de>
2024-11-13 16:03:13 +0100

    feat: add data and gmi to legacy (#1643)

--- src/app/legacy/config.ts
+++ src/app/legacy/config.ts
@@ -1,18 +1,23 @@
 import {
+  DebtIssuanceModuleAddress,
   IndexDebtIssuanceModuleV2Address,
   IndexDebtIssuanceModuleV2Address_v2,
 } from '@indexcoop/flash-mint-sdk'
 
 import {
   BedIndex,
+  DATA,
   GitcoinStakedETHIndex,
+  GmiIndex,
   ic21,
   LeveragedRethStakingYield,
 } from '@/constants/tokens'
 
 export const Issuance = {
   [BedIndex.symbol]: IndexDebtIssuanceModuleV2Address_v2,
+  [DATA.symbol]: DebtIssuanceModuleAddress,
   [GitcoinStakedETHIndex.symbol]: IndexDebtIssuanceModuleV2Address,
+  [GmiIndex.symbol]: DebtIssuanceModuleAddress,
   [ic21.symbol]: IndexDebtIssuanceModuleV2Address_v2,
   [LeveragedRethStakingYield.symbol]: IndexDebtIssuanceModuleV2Address_v2,
 }
@@ -20,6 +25,8 @@ export const Issuance = {
 export const LegacyTokenList = [
   ic21,
   GitcoinStakedETHIndex,
+  GmiIndex,
+  DATA,
   LeveragedRethStakingYield,
   BedIndex,
 ]
