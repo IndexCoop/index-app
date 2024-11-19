721657a7f849671591e172b514755740f64d2ce3
0xonramp <0xonramp@gmail.com>
2024-11-19 08:43:09 -0500

    Revert trade now

--- src/app/products/components/hero.tsx
+++ src/app/products/components/hero.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import Link from 'next/link'
+
+import { Path } from '@/constants/paths'
+import { useAnalytics } from '@/lib/hooks/use-analytics'
+
 export function Hero() {
+  const { logEvent } = useAnalytics()
   return (
-    <div className='flex flex-col items-center space-y-8 md:space-y-10'>
+    <div className='flex flex-col items-center space-y-10 md:space-y-12'>
       <h2 className='text-ic-gray-800 max-w-3xl text-center text-3xl font-bold md:text-4xl lg:text-5xl lg:leading-tight'>
         Crypto is complex.
         <br />
@@ -10,6 +18,15 @@ export function Hero() {
         Unlock powerful sector, leverage and yield strategies with our simple
         tokens.
       </h3>
+      <Link
+        className='bg-ic-blue-500 text-ic-white hover:bg-ic-blue-400 rounded-lg px-11 py-3'
+        href={Path.TRADE}
+        onClick={() =>
+          logEvent('Trade Now Button Clicked', { context: 'Hero' })
+        }
+      >
+        Trade Now
+      </Link>
     </div>
   )
 }
