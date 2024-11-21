92b99dc4960018a1345f4357069fcb481d3cf07f
JD <jann.driessen@gmx.de>
2024-11-21 14:34:23 +0100

    chore: avoid logging console in sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,7 +8,8 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: 'https://56a0c37921d966ca831647a0bb314226@o4507393908211712.ingest.de.sentry.io/4507393908605008',
   enabled: process.env.NODE_ENV === 'production',
-  integrations: [Sentry.captureConsoleIntegration()],
+  // Turn on if you wanna log the console
+  // integrations: [Sentry.captureConsoleIntegration()],
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
