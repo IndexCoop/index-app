d0dadc7991b4fa6a0b170434cd775d8b6279a038
Toki <tokdaniel4@gmail.com>
2024-12-02 11:52:51 +0100

    remove gen from other branch

--- src/gen/clients/axios/userService/userService.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { getApiV2Quote } from "./getApiV2Quote.ts";
-import { getApiV2UserAddress } from "./getApiV2UserAddress.ts";
-import { postApiV2Trade } from "./postApiV2Trade.ts";
-import { postApiV2UserAddress } from "./postApiV2UserAddress.ts";
-
- export function userService() {
-    return { getApiV2Quote, postApiV2Trade, getApiV2UserAddress, postApiV2UserAddress };
-}
