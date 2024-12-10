011256c066b493346d210c7c457cabaa867809d4
JD <jann.driessen@gmx.de>
2024-12-10 15:41:55 -0500

    add polygon and

--- src/app/legacy/config.ts
+++ src/app/legacy/config.ts
@@ -1,4 +1,5 @@
 import {
+  BasicIssuanceModulePolygonAddress,
   DebtIssuanceModuleAddress,
   DebtIssuanceModuleV2Address,
   IndexDebtIssuanceModuleV2Address,
@@ -16,15 +17,26 @@ import {
   LeveragedRethStakingYield,
 } from '@/constants/tokens'
 
+import {
+  Bitcoin2xFlexibleLeverageIndexPolygon,
+  ETH2xFlexibleLeverageIndexPolygon,
+  Matic2xFlexibleLeverageIndexPolygon,
+} from './polygon'
+
 export const Issuance = {
   [BedIndex.symbol]: IndexDebtIssuanceModuleV2Address_v2,
   [Bitcoin2xFlexibleLeverageIndex.symbol]: DebtIssuanceModuleV2Address,
+  [Bitcoin2xFlexibleLeverageIndexPolygon.symbol]:
+    BasicIssuanceModulePolygonAddress,
   [DATA.symbol]: DebtIssuanceModuleAddress,
   [Ethereum2xFlexibleLeverageIndex.symbol]: DebtIssuanceModuleV2Address,
+  [ETH2xFlexibleLeverageIndexPolygon.symbol]: BasicIssuanceModulePolygonAddress,
   [GitcoinStakedETHIndex.symbol]: IndexDebtIssuanceModuleV2Address,
   [GmiIndex.symbol]: DebtIssuanceModuleAddress,
   [ic21.symbol]: IndexDebtIssuanceModuleV2Address_v2,
   [LeveragedRethStakingYield.symbol]: IndexDebtIssuanceModuleV2Address_v2,
+  [Matic2xFlexibleLeverageIndexPolygon.symbol]:
+    BasicIssuanceModulePolygonAddress,
 }
 
 export const LegacyTokenList = [
@@ -36,4 +48,10 @@ export const LegacyTokenList = [
   DATA,
   LeveragedRethStakingYield,
   BedIndex,
+]
+
+export const PolygonLegacyTokenList = [
+  Bitcoin2xFlexibleLeverageIndexPolygon,
+  ETH2xFlexibleLeverageIndexPolygon,
+  Matic2xFlexibleLeverageIndexPolygon,
 ]
---
+++ src/app/legacy/polygon.ts
@@ -0,0 +1,26 @@
+import { Token } from '@/constants/tokens'
+
+export const Bitcoin2xFlexibleLeverageIndexPolygon: Token = {
+  name: 'Bitcoin 2x Flexible Leverage Index',
+  symbol: 'BTC2x-FLI-P',
+  address: '0xd6cA869a4EC9eD2C7E618062Cdc45306d8dBBc14',
+  decimals: 18,
+  image: 'https://set-core.s3.amazonaws.com/img/portfolios/fli_btc.svg',
+}
+
+export const ETH2xFlexibleLeverageIndexPolygon: Token = {
+  name: 'ETH 2x Flexible Leverage Index ',
+  symbol: 'ETH2x-FLI-P',
+  address: '0x3Ad707dA309f3845cd602059901E39C4dcd66473',
+  decimals: 18,
+  image: 'https://set-core.s3.amazonaws.com/img/portfolios/eth2x_fli.svg',
+}
+
+export const Matic2xFlexibleLeverageIndexPolygon: Token = {
+  name: 'MATIC 2x Flexible Leverage Index ',
+  symbol: 'MATIC2x-FLI-P',
+  address: '0xf287D97B6345bad3D88856b26Fb7c0ab3F2C7976',
+  decimals: 18,
+  image:
+    'https://legacyproducts.indexcoop.com/static/media/maticflilogo.c7c422f127140707358a49bb0ae063d7.svg',
+}
