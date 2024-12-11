7d75f0b4bad046c7e66e1d536942439aed536671
JD <jann.driessen@gmx.de>
2024-12-11 15:49:36 -0500

    use tokens from list

--- src/app/legacy/polygon.ts
+++ src/app/legacy/polygon.ts
@@ -1,53 +1,38 @@
-import { Token } from '@/constants/tokens'
+import { getTokenByChainAndSymbol } from '@indexcoop/tokenlists'
+import { polygon } from 'viem/chains'
 
-export const Bitcoin2xFlexibleLeverageIndexPolygon: Token = {
-  name: 'Bitcoin 2x Flexible Leverage Index',
-  symbol: 'BTC2x-FLI-P',
-  address: '0xd6cA869a4EC9eD2C7E618062Cdc45306d8dBBc14',
-  decimals: 18,
-  image: 'https://set-core.s3.amazonaws.com/img/portfolios/fli_btc.svg',
+const btc2xflip = getTokenByChainAndSymbol(polygon.id, 'BTC2x-FLI-P')
+export const Bitcoin2xFlexibleLeverageIndexPolygon = {
+  ...btc2xflip,
+  image: btc2xflip.logoURI,
 }
 
-export const ETH2xFlexibleLeverageIndexPolygon: Token = {
-  name: 'ETH 2x Flexible Leverage Index',
-  symbol: 'ETH2x-FLI-P',
-  address: '0x3Ad707dA309f3845cd602059901E39C4dcd66473',
-  decimals: 18,
-  image: 'https://set-core.s3.amazonaws.com/img/portfolios/eth2x_fli.svg',
+const eth2xflip = getTokenByChainAndSymbol(polygon.id, 'ETH2x-FLI-P')
+export const ETH2xFlexibleLeverageIndexPolygon = {
+  ...eth2xflip,
+  image: eth2xflip.logoURI,
 }
 
-export const InverseBTCFlexibleLeverageIndexPolygon: Token = {
-  name: 'Inverse BTC Flexible Leverage Index',
-  symbol: 'iBTC-FLI-P',
-  address: '0x130cE4E4F76c2265f94a961D70618562de0bb8d2',
-  decimals: 18,
-  image:
-    'https://legacyproducts.indexcoop.com/static/media/ibtcflip.787e30a773c385f8f2c0f1288b4c7ebd.svg',
+const ibtcflip = getTokenByChainAndSymbol(polygon.id, 'iBTC-FLI-P')
+export const InverseBTCFlexibleLeverageIndexPolygon = {
+  ...ibtcflip,
+  image: ibtcflip.logoURI,
 }
 
-export const InverseETHFlexibleLeverageIndexPolygon: Token = {
-  name: 'Inverse ETH Flexible Leverage Index',
-  symbol: 'iETH-FLI-P',
-  address: '0x4f025829C4B13dF652f38Abd2AB901185fF1e609',
-  decimals: 18,
-  image:
-    'https://legacyproducts.indexcoop.com/static/media/iethfliplogo.2948ace0b7e215f7b4f45a3aaa25ac44.svg',
+const iethflip = getTokenByChainAndSymbol(polygon.id, 'iETH-FLI-P')
+export const InverseETHFlexibleLeverageIndexPolygon = {
+  ...iethflip,
+  image: iethflip.logoURI,
 }
 
-export const InverseMATICFlexibleLeverageIndexPolygon: Token = {
-  name: 'Inverse MATIC Flexible Leverage Index',
-  symbol: 'iMATIC-FLI-P',
-  address: '0x340f412860dA7b7823df372a2b59Ff78b7ae6abc',
-  decimals: 18,
-  image:
-    'https://legacyproducts.indexcoop.com/static/media/imaticflilogo.6114063ab4d643f7f7e3abc949196fb8.svg',
+const imaticflip = getTokenByChainAndSymbol(polygon.id, 'iMATIC-FLI-P')
+export const InverseMATICFlexibleLeverageIndexPolygon = {
+  ...imaticflip,
+  image: imaticflip.logoURI,
 }
 
-export const Matic2xFlexibleLeverageIndexPolygon: Token = {
-  name: 'MATIC 2x Flexible Leverage Index ',
-  symbol: 'MATIC2x-FLI-P',
-  address: '0xf287D97B6345bad3D88856b26Fb7c0ab3F2C7976',
-  decimals: 18,
-  image:
-    'https://legacyproducts.indexcoop.com/static/media/maticflilogo.c7c422f127140707358a49bb0ae063d7.svg',
+const matic2xflip = getTokenByChainAndSymbol(polygon.id, 'MATIC2x-FLI-P')
+export const Matic2xFlexibleLeverageIndexPolygon = {
+  ...matic2xflip,
+  image: matic2xflip.logoURI,
 }
