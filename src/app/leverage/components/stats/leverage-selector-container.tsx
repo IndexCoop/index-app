9a171ec04cef053a74d4ce3e0d543ad52db91e09
JD <jann.driessen@gmx.de>
2024-12-12 18:23:02 -0500

    add border and update padding

--- src/app/leverage/components/stats/leverage-selector-container.tsx
+++ src/app/leverage/components/stats/leverage-selector-container.tsx
@@ -3,9 +3,9 @@ import { StatsMetric } from './stats-metric'
 export function LeverageSelectorContainer() {
   const isFetchingStats = false
   return (
-    <div className='flex'>
+    <div className='border-ic-black flex h-full items-center border-l px-16 py-0'>
       <StatsMetric
-        className='w-80'
+        className='w-16'
         isLoading={isFetchingStats}
         label='Net Rate'
         value={'0.03%'}
