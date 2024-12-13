e6c20cc1056618a37d053fa25b6f4afbd5bde6dc
Toki <tokdaniel4@gmail.com>
2024-12-13 10:55:45 +0100

    remove log

--- src/app/leverage/utils/fetch-leverage-token-prices.ts
+++ src/app/leverage/utils/fetch-leverage-token-prices.ts
@@ -14,7 +14,6 @@ export async function fetchLeverageTokenPrices(
   setTokens: Dispatch<SetStateAction<EnrichedToken[]>>,
   chainId: number,
 ) {
-  console.log(balances)
   const tokenBalances = balances.reduce((acc, current) => {
     const token = getTokenByChainAndAddress(chainId, current.token)
 
