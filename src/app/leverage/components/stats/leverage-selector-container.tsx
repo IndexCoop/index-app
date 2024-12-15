14b97585b879f49e5acf45e815691f29769e5aae
JD <jann.driessen@gmx.de>
2024-12-15 15:26:13 -0500

    add current leverage component

--- src/app/leverage/components/stats/leverage-selector-container.tsx
+++ src/app/leverage/components/stats/leverage-selector-container.tsx
@@ -4,7 +4,7 @@ import { StatsMetric } from './stats-metric'
 export function LeverageSelectorContainer() {
   const isFetchingStats = false
   return (
-    <div className='border-ic-black flex h-full items-center gap-8 border-l px-16 py-0'>
+    <div className='border-ic-black flex h-full w-1/3 items-center gap-8 border-l px-16 py-0'>
       <LeverageSelector
         leverage={'2x'}
         leverageType={'Long'}
--- src/app/leverage/components/stats/leverage-selector.tsx
+++ src/app/leverage/components/stats/leverage-selector.tsx
@@ -6,16 +6,34 @@ type LeverageSelectorProps = {
   onClick: () => void
 }
 
+type CurrentLeverageProps = {
+  leverage: string
+}
+
+function CurrentLeverage(props: CurrentLeverageProps) {
+  return (
+    <div className='text-ic-gray-300 flex items-center gap-1 text-xs font-normal'>
+      <span>Current Leverage</span>
+      <span className='bg-ic-blue-300 text-ic-gray-950 rounded px-[6px] py-[1px] text-[9px] font-semibold'>
+        {props.leverage}
+      </span>
+    </div>
+  )
+}
+
 export function LeverageSelector(props: LeverageSelectorProps) {
   const { leverage, leverageType } = props
   return (
-    <div
-      className='flex cursor-pointer flex-row items-center gap-1 py-2 text-sm'
-      onClick={props.onClick}
-    >
-      <span className='text-ic-blue-300 font-bold'>{leverage}</span>
-      <span className='text-ic-gray-100 font-bold'>{leverageType}</span>
-      <ChevronDownIcon className='dark:text-ic-white text-ic-black size-4' />
+    <div className='flex-col gap-1 py-2'>
+      <div
+        className='flex cursor-pointer flex-row items-center gap-1 text-sm'
+        onClick={props.onClick}
+      >
+        <span className='text-ic-blue-300 font-bold'>{leverage}</span>
+        <span className='text-ic-gray-100 font-bold'>{leverageType}</span>
+        <ChevronDownIcon className='dark:text-ic-white text-ic-black size-4' />
+      </div>
+      <CurrentLeverage leverage={'1.94x'} />
     </div>
   )
 }
