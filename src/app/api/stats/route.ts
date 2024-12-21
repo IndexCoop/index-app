a4853d61bec9d80679fa97b479bf6dc737ffeda1
JD <jann.driessen@gmx.de>
2024-12-21 08:53:33 -0500

    update quick stats route to with costs of carry

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -1,11 +1,14 @@
 import { CoingeckoProvider, CoinGeckoService } from '@indexcoop/analytics-sdk'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { fetchCarryCosts } from '@/lib/utils/fetch'
+
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
   const symbol = searchParams.get('symbol')
+  const base = searchParams.get('base')
   const baseCurrency = searchParams.get('baseCurrency')
-  if (!symbol || !baseCurrency) {
+  if (!symbol || !base || !baseCurrency) {
     return NextResponse.json('Bad Request', { status: 400 })
   }
   try {
@@ -13,8 +16,15 @@ export async function GET(req: NextRequest) {
       process.env.COINGECKO_API_KEY!,
     )
     const provider = new CoingeckoProvider(coingeckoService)
-    const data = await provider.getTokenStats(symbol, baseCurrency)
-    return NextResponse.json({ ...data })
+    const data = await provider.getTokenStats(base, baseCurrency)
+    const carryCosts = await fetchCarryCosts()
+    const costOfCarry = carryCosts
+      ? carryCosts[symbol.toLowerCase()] ?? null
+      : null
+    return NextResponse.json({
+      base: { ...data, baseCurrency },
+      token: { symbol, costOfCarry },
+    })
   } catch (error) {
     return NextResponse.json(error, { status: 500 })
   }
