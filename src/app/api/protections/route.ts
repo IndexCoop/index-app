5fd1de0730ac465bb267a4195da1cff9fc75739d
0xonramp <0xonramp@gmail.com>
2025-01-14 13:07:38 -0500

    Update protections api endpoint

--- src/app/api/protections/route.ts
+++ src/app/api/protections/route.ts
@@ -1,26 +1,34 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
   // FIXME: Update hostname
-  const res = await fetch(
-    'https://api-pr-29-80wy.onrender.com/api/v2/protections',
-    {
-      headers: {
-        'cf-ipcountry':
-          req.headers.get('cf-ipcountry') ??
-          req.headers.get('x-vercel-ip-country') ??
-          '',
-        'cf-connecting-ip':
-          req.headers.get('cf-connecting-ip') ??
-          req.headers.get('x-forwarded-for') ??
-          '',
+  try {
+    const res = await fetch(
+      'https://api-pr-29-80wy.onrender.com/api/v2/protections',
+      {
+        headers: {
+          'cf-ipcountry':
+            req.headers.get('cf-ipcountry') ??
+            req.headers.get('x-vercel-ip-country') ??
+            '',
+          'cf-connecting-ip':
+            req.headers.get('cf-connecting-ip') ??
+            req.headers.get('x-forwarded-for') ??
+            '',
+        },
       },
-    },
-  )
-  const { isRestrictedCountry, isUsingVpn } = await res.json()
+    )
+    const { isRestrictedCountry, isUsingVpn } = await res.json()
 
-  return {
-    isRestrictedCountry,
-    isUsingVpn,
+    return NextResponse.json({
+      isRestrictedCountry,
+      isUsingVpn,
+    })
+  } catch (e) {
+    console.error('Caught protections error', e)
+    return NextResponse.json({
+      isRstrictedCountry: false,
+      isUsingVpn: false,
+    })
   }
 }
