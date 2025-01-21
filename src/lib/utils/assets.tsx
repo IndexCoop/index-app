f1a8ce3faddc3c230aa22eab25bfe8ee85da6a12
JD <jann.driessen@gmx.de>
2025-01-21 16:01:36 -0500

    remove obsolete assets

--- src/lib/utils/assets.tsx
+++ src/lib/utils/assets.tsx
@@ -1,8 +1,6 @@
-const bedBorderLogo = '/assets/bed-border.png'
 const dsethLogo = '/assets/dseth_logo.svg'
 const gmiLogo = '/assets/gmi_logo.webp'
 const icethLogo = '/assets/icethlogo.png'
-const icrethLogo = '/assets/icreth-logo.svg'
 const IndexLogoBlack = '/assets/index-logo-black.svg'
 const IndexLogoFullBlack = '/assets/index-logo-full-black.svg'
 const IndexLogoFullWhite = '/assets/index-logo-full-white.svg'
@@ -19,12 +17,4 @@ export {
 }
 
 // Indices logos
-export {
-  bedBorderLogo,
-  dsethLogo,
-  gmiLogo,
-  icethLogo,
-  icrethLogo,
-  indexLogo,
-  wseth2Logo,
-}
+export { dsethLogo, gmiLogo, icethLogo, indexLogo, wseth2Logo }
