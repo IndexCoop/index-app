c7516ec2950dea15a1be47b6a082d1f61b2d340b
0xonramp <0xonramp@gmail.com>
2025-02-08 20:30:43 -0500

    Update styling on -1x

--- src/app/leverage/components/stats/leverage-selector.tsx
+++ src/app/leverage/components/stats/leverage-selector.tsx
@@ -1,3 +1,5 @@
+import { LeverageStrategy } from '@/app/leverage/types'
+import { cn } from '@/lib/utils/tailwind'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 type Props = {
@@ -7,16 +9,33 @@ type Props = {
 }
 
 export function LeverageSelector({ leverage, leverageType, ratio }: Props) {
+  console.log('leverageType', leverageType)
   return (
     <div className='flex flex-col gap-0.5 py-2'>
       <div className='flex cursor-pointer flex-row items-center gap-1 text-sm'>
-        <span className='text-ic-blue-300 font-bold'>{leverage}</span>
+        <span
+          className={cn(
+            'font-bold',
+            leverage === LeverageStrategy.Short1x
+              ? 'text-[#EF7C78]'
+              : 'text-ic-blue-300',
+          )}
+        >
+          {leverage}
+        </span>
         <span className='text-ic-gray-100 font-bold'>{leverageType}</span>
         <ChevronDownIcon className='dark:text-ic-white text-ic-black size-4' />
       </div>
       <div className='text-ic-gray-300 flex items-center gap-1.5 text-xs font-normal'>
         <span className='xs:flex hidden'>Current</span> Leverage
-        <span className='bg-ic-blue-300 text-ic-gray-950 rounded px-1.5 py-px text-[9px] font-semibold'>
+        <span
+          className={cn(
+            'bg-ic-blue-300 text-ic-gray-950 rounded px-1.5 py-px text-[9px] font-semibold',
+            leverage === LeverageStrategy.Short1x
+              ? 'bg-[#EF7C78]'
+              : 'bg-ic-blue-300',
+          )}
+        >
           {ratio ? `${ratio.toFixed(2)}x` : ''}
         </span>
       </div>
