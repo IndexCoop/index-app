e3e01f0bc28ce64f6ffebf9ec6568addcd0ee3e1
JD <jann.driessen@gmx.de>
2025-02-14 09:56:33 +0100

    update feedback button

--- src/app/leverage/components/leverage-feedback-button.tsx
+++ src/app/leverage/components/leverage-feedback-button.tsx
@@ -6,7 +6,7 @@ export function LeverageFeedbackButton() {
   return (
     <PopupButton
       id='Ns10DhMF'
-      className='text-ic-white bg-ic-blue-900 hover:bg-ic-blue-800 h-12 w-full rounded-lg py-2.5 font-bold'
+      className='text-ic-black bg-ic-gray-700 hover:bg-ic-gray-500 h-12 w-full rounded-lg py-2.5 font-bold'
       tracking={surveyTracking}
     >
       Give us your feedback!
