cd438898108b0d4e1774832b2c2eb47101802419
0xonramp <0xonramp@gmail.com>
2025-02-17 16:55:25 -0500

    Tweak env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_ARCX_ANALYTICS_API_KEY: string
+      NEXT_PUBLIC_ARCX_ANALYTICS_API_KEY?: string
       NEXT_PUBLIC_ALCHEMY_ID: string
       NEXT_PUBLIC_INDEX_COOP_API: string
       NEXT_PUBLIC_IP_LOOKUP_KEY: string
-      NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: string
+      NEXT_PUBLIC_GOOGLE_ANALYTICS_ID?: string
       NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID?: string
       NEXT_PUBLIC_TENDERLY_ACCESS_KEY: string
       NEXT_PUBLIC_TENDERLY_USER: string
