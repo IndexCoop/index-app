ae37ac45f3e35d138e46eaa364d2819d18a97e22
0xonramp <0xonramp@gmail.com>
2025-02-21 10:32:55 -0500

    Add open/close styling

--- src/components/selectors/buy-sell-selector-button.tsx
+++ src/components/selectors/buy-sell-selector-button.tsx
@@ -16,7 +16,7 @@ export function BuySellSelectorButton({
   return (
     <div
       className={cn(
-        'dark:bg-ic-gray-975 flex-grow cursor-pointer select-none rounded-md',
+        'dark:bg-ic-gray-975 group flex-grow cursor-pointer select-none rounded-md transition duration-150',
         isSelected && 'bg-[#F0FEFF]',
       )}
       onClick={onClick}
@@ -26,7 +26,7 @@ export function BuySellSelectorButton({
           'py-4 text-center text-sm font-medium',
           isSelected
             ? 'text-ic-gray-700 dark:text-ic-white'
-            : 'text-ic-gray-500',
+            : 'text-ic-gray-500 dark:group-hover:text-ic-gray-300',
         )}
       >
         {label}
@@ -36,7 +36,7 @@ export function BuySellSelectorButton({
           'h-1 w-full',
           isSelected
             ? 'dark:bg-ic-blue-600 bg-[#A5D6D6]'
-            : 'bg-ic-gray-100 dark:bg-ic-gray-800',
+            : 'bg-ic-gray-100 dark:bg-ic-gray-800 dark:group-hover:bg-ic-gray-700',
           roundedLeft ? 'rounded-bl-md' : 'rounded-br-md',
         )}
       />
