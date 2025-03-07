48ffa5ce2d64b94a449731e8869640120cb322a9
Toki <tokdaniel4@gmail.com>
2025-03-07 11:04:28 +0100

    on buy/sell click scroll to widget

--- src/app/leverage/components/leverage-widget/components/buy-sell-selector.tsx
+++ src/app/leverage/components/leverage-widget/components/buy-sell-selector.tsx
@@ -1,4 +1,5 @@
 import { BuySellSelectorButton } from '@/components/selectors/buy-sell-selector-button'
+import { useCallback, useRef } from 'react'
 
 type BuySellSelectorProps = {
   isMinting: boolean
@@ -6,19 +7,31 @@ type BuySellSelectorProps = {
 }
 
 export function BuySellSelector({ isMinting, onClick }: BuySellSelectorProps) {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const onClickBuySell = useCallback(() => {
+    if (wrapperRef.current) {
+      wrapperRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    onClick()
+  }, [onClick])
+
   return (
-    <div className='bg-ic-gray-50 dark:bg-ic-gray-975 flex flex-row rounded-lg'>
+    <div
+      ref={wrapperRef}
+      className='bg-ic-gray-50 dark:bg-ic-gray-975 flex flex-row rounded-lg'
+    >
       <BuySellSelectorButton
         isSelected={isMinting}
         label='Buy'
         roundedLeft={true}
-        onClick={onClick}
+        onClick={onClickBuySell}
       />
       <BuySellSelectorButton
         isSelected={!isMinting}
         label='Sell'
         roundedLeft={false}
-        onClick={onClick}
+        onClick={onClickBuySell}
       />
     </div>
   )
