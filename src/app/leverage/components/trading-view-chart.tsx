1897820c8794cbcdc330c01c3bd723014722f7e6
0xonramp <0xonramp@gmail.com>
2025-03-19 11:53:39 -0400

    Update copy

--- src/app/leverage/components/trading-view-chart.tsx
+++ src/app/leverage/components/trading-view-chart.tsx
@@ -41,7 +41,7 @@ export function TradingViewChart({ indexToken }: Props) {
             className='bg-ic-blue-500 dark:bg-ic-blue-300 dark:hover:bg-ic-blue-200 hover:bg-ic-blue-500/90 text-ic-gray-50 dark:text-ic-black block rounded-md px-8 py-1 text-sm font-medium shadow-sm transition-all duration-300 hover:scale-[1.04]'
             onClick={() => open({ view: 'Connect' })}
           >
-            Connect
+            Connect Wallet
           </Button>
         </div>
       )}
