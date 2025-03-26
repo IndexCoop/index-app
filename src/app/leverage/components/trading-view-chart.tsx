166ba84e1c949a2cc31073751d0b49c8c3acefe8
0xonramp <0xonramp@gmail.com>
2025-03-26 13:24:58 -0400

    fix: safari mobile chart

--- src/app/leverage/components/trading-view-chart.tsx
+++ src/app/leverage/components/trading-view-chart.tsx
@@ -1,35 +1,19 @@
-import { Button } from '@headlessui/react'
-import { useAppKit } from '@reown/appkit/react'
 import Script from 'next/script'
 import { useState } from 'react'
 
 import { TradingViewChartContainer } from '@/app/leverage/components/trading-view-chart-container'
-import { useWallet } from '@/lib/hooks/use-wallet'
 
 export function TradingViewChart() {
   const [isScriptReady, setIsScriptReady] = useState(false)
-  const { isConnected } = useWallet()
-  const { open } = useAppKit()
   return (
     <div className='xs:h-[422px] relative aspect-square w-full lg:aspect-auto'>
       <Script
         src='/tradingview-chart/datafeeds/udf/dist/bundle.js'
-        onLoad={() => {
+        onReady={() => {
           setIsScriptReady(true)
         }}
       />
-      {isScriptReady && isConnected ? (
-        <TradingViewChartContainer />
-      ) : (
-        <div className='bg-ic-black/95 absolute inset-0 z-20 flex items-center justify-center'>
-          <Button
-            className='bg-ic-blue-500 dark:bg-ic-blue-300 dark:hover:bg-ic-blue-200 hover:bg-ic-blue-500/90 text-ic-gray-50 dark:text-ic-black block rounded-md px-8 py-1 text-sm font-medium shadow-sm transition-all duration-300 hover:scale-[1.04]'
-            onClick={() => open({ view: 'Connect' })}
-          >
-            Connect Wallet
-          </Button>
-        </div>
-      )}
+      {isScriptReady ? <TradingViewChartContainer /> : null}
     </div>
   )
 }
