895fded8dc8bd1949fc430237667d3859f626f06
Toki <tokdaniel4@gmail.com>
2025-04-01 10:51:48 +0200

    reset to prod api

--- src/app/earn/components/product-pill.tsx
+++ src/app/earn/components/product-pill.tsx
@@ -7,7 +7,7 @@ export const ProductTitlePill = ({
   text: string
   icon: ReactNode
 }) => (
-  <div className='bg-ic-pill-teal flex items-center rounded-md border-[0.5px] border-black border-opacity-20 px-2 py-1'>
+  <div className='bg-ic-pill-teal flex items-center rounded-md border-[0.5px] border-black border-opacity-20 px-2 py-1 font-["Open_Sauce_Sans"] font-semibold'>
     <div className='rounded-full bg-neutral-50 p-0.5'>{icon}</div>
 
     <p className='ml-2 w-full text-[10px] uppercase tracking-tight text-white'>
