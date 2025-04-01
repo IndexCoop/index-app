076abef792f834312b9e32b88ec7ca3fca6876a8
Toki <tokdaniel4@gmail.com>
2025-04-01 12:53:30 +0200

    da glow

--- src/app/earn/layout.tsx
+++ src/app/earn/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { LightEffect } from '@/app/leverage/components/light-effect'
 import { Providers } from '@/app/providers'
+import BackgroundLight from '@/components/background-light'
 import { Header } from '@/components/header'
 import { getApiV2ProductsEarn } from '@/gen'
 import { SlippageProvider } from '@/lib/providers/slippage'
@@ -24,14 +24,29 @@ export default async function Layout({ children }: LayoutProps) {
       : []
 
   return (
-    <div className='h-full w-full bg-zinc-950'>
+    <div className='relative h-[100dvh] w-full overflow-hidden bg-zinc-950'>
       <Providers>
-        <div className='dark flex flex-col overflow-x-hidden'>
+        <div className='dark flex h-[100dvh] flex-col overflow-hidden'>
+          <BackgroundLight
+            background='radial-gradient(circle at center, rgba(10, 70, 200, 0.4) 40%, transparent 70%)'
+            className='-bottom-[25%] left-[40%]'
+          />
+          <BackgroundLight
+            background='radial-gradient(circle at center, rgba(50, 130, 170, 0.6) 40%, transparent 70%)'
+            className='-top-[30%] left-[10%]'
+          />
+          <BackgroundLight
+            background='radial-gradient(circle at center, rgba(32, 214, 240, 0.4) 40%, transparent 70%)'
+            className='-right-[10%] top-[10%]'
+          />
+          <BackgroundLight
+            background='radial-gradient(circle at center, rgba(30, 206, 162, 0.5) 40%, transparent 70%)'
+            className='-left-[10%] bottom-0'
+          />
           <Header />
           <SlippageProvider>
             <EarnProvider products={products}>
-              <LightEffect page='earn' />
-              <main className='z-10'>{children}</main>
+              <main className='z-10 flex-1 overflow-y-auto'>{children}</main>
             </EarnProvider>
           </SlippageProvider>
         </div>
