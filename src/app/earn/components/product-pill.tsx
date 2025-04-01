46af30223afeff54b53801e78bf61aa1fce382ed
Toki <tokdaniel4@gmail.com>
2025-04-01 10:47:13 +0200

    increase font size of smartloop

--- src/app/earn/components/product-pill.tsx
+++ src/app/earn/components/product-pill.tsx
@@ -10,7 +10,7 @@ export const ProductTitlePill = ({
   <div className='bg-ic-pill-teal flex items-center rounded-md border-[0.5px] border-black border-opacity-20 px-2 py-1'>
     <div className='rounded-full bg-neutral-50 p-0.5'>{icon}</div>
 
-    <p className='ml-2 w-full text-[8px] uppercase tracking-tight text-white'>
+    <p className='ml-2 w-full text-[10px] uppercase tracking-tight text-white'>
       {text}
     </p>
   </div>
