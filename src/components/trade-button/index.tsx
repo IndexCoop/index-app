2efc7ac722bf3b9cd65354d2e20766d1f5efbd26
Toki <tokdaniel4@gmail.com>
2025-04-07 17:51:56 +0200

    add zinc-800 for disabled tradebtn

--- src/components/trade-button/index.tsx
+++ src/components/trade-button/index.tsx
@@ -28,7 +28,7 @@ export const TradeButton = ({
           'text-ic-white w-full rounded-3xl px-6 py-4 font-semibold transition duration-300 ease-in-out dark:rounded-3xl dark:text-zinc-800',
           disabled ? 'shadow-none' : 'shadow-[0.5px_1px_2px_0_rgba(0,0,0,0.3)]',
           disabled
-            ? 'bg-ic-gray-500'
+            ? 'bg-ic-gray-500 dark:bg-zinc-800'
             : 'bg-ic-blue-600 dark:bg-neutral-50 dark:hover:bg-neutral-200',
         )}
         disabled={disabled}
