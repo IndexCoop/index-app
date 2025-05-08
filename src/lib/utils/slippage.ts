ec43f46c0525caf1703f883cb3cd0fa3a10516a2
JD <jann.driessen@gmx.de>
2025-05-08 08:50:13 +0200

    chore: update slippage default of iceth

--- src/lib/utils/slippage.ts
+++ src/lib/utils/slippage.ts
@@ -1,11 +1,11 @@
 import { slippageDefault, slippageMap } from '@/constants/slippage'
 
-export function getSlippageOverrideOrNull(tokenSymbol: string): number | null {
-  return slippageMap.get(tokenSymbol) ?? null
+export function getSlippageOverrideOrNull(address: string): number | null {
+  return slippageMap.get(address) ?? null
 }
 
-export function selectSlippage(slippage: number, indexSymbol: string): number {
-  const slippageOverrride = getSlippageOverrideOrNull(indexSymbol)
+export function selectSlippage(slippage: number, address: string): number {
+  const slippageOverrride = getSlippageOverrideOrNull(address)
   if (slippageOverrride && slippage < slippageOverrride)
     return slippageOverrride
   if (slippageOverrride && slippage > slippageOverrride)
