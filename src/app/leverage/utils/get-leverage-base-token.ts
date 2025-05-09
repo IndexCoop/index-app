e0e467f216724cfdf0a43a2d03776cbca8c90366
JD <jann.driessen@gmx.de>
2025-05-09 08:18:47 +0200

    update get leverage base token

--- src/app/leverage/utils/get-leverage-base-token.ts
+++ src/app/leverage/utils/get-leverage-base-token.ts
@@ -8,7 +8,9 @@ const uSui = getTokenByChainAndSymbol(base.id, 'uSUI')
 
 export function getLeverageBaseToken(symbol: string) {
   if (symbol.toLowerCase() === 'uSOL2x'.toLowerCase()) return uSol
+  if (symbol.toLowerCase() === 'uSOL3x'.toLowerCase()) return uSol
   if (symbol.toLowerCase() === 'uSUI2x'.toLowerCase()) return uSui
+  if (symbol.toLowerCase() === 'uSUI3x'.toLowerCase()) return uSui
   if (symbol.toLowerCase() === 'BTC2xETH'.toLowerCase()) return BTC
   if (symbol.toLowerCase() === 'ETH2xBTC'.toLowerCase()) return ETH
   if (symbol.includes('BTC')) {
