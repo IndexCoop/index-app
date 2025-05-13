d834d19114a9196406ecf956924a2b5cc2536e90
JD <jann.driessen@gmx.de>
2025-05-13 09:28:16 +0200

    add custom slippage for wsteth15x

--- src/constants/slippage.ts
+++ src/constants/slippage.ts
@@ -1,7 +1,9 @@
 import { getTokenByChainAndSymbol } from '@indexcoop/tokenlists'
+import { base } from 'viem/chains'
 
 const hyETH = getTokenByChainAndSymbol(1, 'hyETH')
 const icETH = getTokenByChainAndSymbol(1, 'icETH')
+const wstETH15x = getTokenByChainAndSymbol(base.id, 'wstETH15x')
 
 // Slippage default hard coded to 0.5%
 export const slippageDefault = 0.5
@@ -9,4 +11,5 @@ export const slippageDefault = 0.5
 export const slippageMap = new Map<string, number>([
   [hyETH.address, 0.05],
   [icETH.address, 0.15],
+  [wstETH15x.address, 0.2],
 ])
