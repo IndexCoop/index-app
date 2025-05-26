1932fb7b30bb252c7638f7e5be03e83178aec200
JD <jann.driessen@gmx.de>
2025-05-26 17:12:02 +0200

    update layout and fix image widths

--- src/app/leverage/components/leverage-onboarding-popup.tsx
+++ src/app/leverage/components/leverage-onboarding-popup.tsx
@@ -19,7 +19,7 @@ export function LeverageOnboardingPopup({
       <DialogBackdrop className='fixed inset-0 bg-black/60 backdrop-blur-sm' />
 
       <div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
-        <DialogPanel className='w-full max-w-[480px] rounded-3xl border border-[rgba(255,255,255,0.1)] bg-zinc-900 p-6 pb-3 text-neutral-50 shadow-[0px_20px_40px_0px_rgba(0,0,0,0.10),1px_1px_4.4px_0px_rgba(255,255,255,0.06)_inset]'>
+        <DialogPanel className='max-w-[480px] rounded-3xl border border-[rgba(255,255,255,0.1)] bg-zinc-900 p-6 pb-3 text-neutral-50 shadow-[0px_20px_40px_0px_rgba(0,0,0,0.10),1px_1px_4.4px_0px_rgba(255,255,255,0.06)_inset]'>
           <div className='space-y-6'>
             <div className='flex-col space-y-6'>
               <h2 className='text-1xl font-semibold'>
@@ -30,61 +30,57 @@ export function LeverageOnboardingPopup({
                 built-in liquidation protection.
               </p>
             </div>
-            <div className='space-y-8'>
-              <div className='flex items-center justify-between'>
+            <div className='space-y-4'>
+              <div className='flex items-center justify-between gap-4'>
                 <p className='text-xs font-medium text-gray-400'>
                   <span className='text-neutral-50'>1. Set up your trade</span>-
                   Select your market, target leverage and input amount.
                 </p>
-                <div className='hidden md:block'>
+                <div className='hidden w-[170px] shrink-0 md:block'>
                   <Image
                     src='/assets/onboarding-setup-trade.png'
                     alt='Setup your trade interface'
                     width={170}
                     height={86}
-                    className='rounded-[4px]'
+                    className='!w-full rounded-[4px]'
+                    style={{ width: '170px' }}
                   />
                 </div>
               </div>
-
-              <div className='space-y-2'>
-                <h3 className='font-semibold'>
-                  <span className='text-[#6366F1]'>2.</span> Under the hood
-                </h3>
-                <p className='flex text-sm text-gray-400'>
+              <div className='flex items-center justify-between gap-4'>
+                <p className='text-xs font-medium text-gray-400'>
+                  <span className='text-neutral-50'>2. Under the hood</span>-
                   When you use an Index Coop product, Index automatically
                   creates a leveraged position using collateral and debt
-                  positions on a DeFi lending market. Liquidation protection and
-                  low costs gives your trade time to win.
+                  positions on a DeFi lending market.
                 </p>
-                <div className='hidden md:block'>
+                <div className='hidden w-[170px] shrink-0 md:block'>
                   <Image
                     src='/assets/onboarding-under-the-hood.png'
                     alt='Under the hood illustration'
-                    width={400}
-                    height={225}
-                    className='w-full rounded-lg'
+                    width={170}
+                    height={86}
+                    className='!w-full rounded-[4px]'
+                    style={{ width: '170px' }}
                   />
                 </div>
               </div>
-
-              <div className='flex space-y-2'>
-                <h3 className='font-semibold'>
-                  <span className='text-[#6366F1]'>3.</span> Monitor your
-                  positions
-                </h3>
-                <p className='text-sm text-gray-400'>
-                  Check the &apos;Your Positions&apos; widget to monitor your
-                  profits and losses, and keep track of your actions in the
-                  history tab.
+              <div className='flex items-center justify-between gap-4'>
+                <p className='text-xs font-medium text-gray-400'>
+                  <span className='text-neutral-50'>
+                    3. Monitor your positions
+                  </span>
+                  - Check the 'Your Positions' widget to monitor your profits
+                  and losses, and keep track of your actions in the history tab.
                 </p>
-                <div className='hidden md:block'>
+                <div className='hidden w-[170px] shrink-0 md:block'>
                   <Image
                     src='/assets/onboarding-monitor-your-positions.png'
                     alt='Positions monitoring interface'
-                    width={400}
-                    height={225}
-                    className='w-full rounded-lg'
+                    width={170}
+                    height={86}
+                    className='!w-full rounded-[4px]'
+                    style={{ width: '170px' }}
                   />
                 </div>
               </div>
