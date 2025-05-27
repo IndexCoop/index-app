3e64f348b18f1603472493c07b2ba6368cfb5e2c
JD <jann.driessen@gmx.de>
2025-05-27 15:30:18 +0200

    add protection util

---
+++ src/lib/utils/protection.ts
@@ -0,0 +1,30 @@
+export async function getProtection(headers: Headers, address?: string) {
+  console.log(headers, address)
+  const path = address
+    ? `/v2/protections?${new URLSearchParams({ address }).toString()}`
+    : '/v2/protections'
+  const url = `https://api.indexcoop.com${path}`
+  console.log(url)
+  const res = await fetch(url, {
+    headers: {
+      ...headers,
+      'ic-ip-address':
+        headers.get('cf-connecting-ip') ??
+        headers.get('x-forwarded-for') ??
+        undefined,
+      'ic-ip-country':
+        headers.get('cf-ipcountry') ??
+        headers.get('x-vercel-ip-country') ??
+        undefined,
+    },
+  })
+  const { isForbiddenAddress, isNewUser, isRestrictedCountry, isUsingVpn } =
+    await res.json()
+
+  return {
+    isForbiddenAddress,
+    isNewUser,
+    isRestrictedCountry,
+    isUsingVpn,
+  }
+}
