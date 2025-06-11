3247ee212c3a503627a3c43f46d1a04cce197abb
JD <jann.driessen@gmx.de>
2025-06-11 11:42:30 +0200

    remove logs

--- src/app/api/price/coingecko/simple/token-price/route.ts
+++ src/app/api/price/coingecko/simple/token-price/route.ts
@@ -12,8 +12,6 @@ export async function GET(request: NextRequest) {
       vs_currencies: searchParams.get('vs_currencies') ?? '',
     })
 
-    console.log(res.data)
-
     return NextResponse.json({
       ...res.data,
     })
