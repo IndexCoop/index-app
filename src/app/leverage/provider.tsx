728a1d4b6b01ec7447a2e66dcd7f90ef3d419ee9
Toki <tokdaniel4@gmail.com>
2025-10-09 18:29:55 +0200

    Release ETH3x and BTC3x on mainnet, and AAVE2x LINK2x, and ARB2x on arbitrum

--- src/app/leverage/provider.tsx
+++ src/app/leverage/provider.tsx
@@ -230,9 +230,9 @@ export function LeverageProvider(props: { children: any }) {
   }, [chainId, indexTokensBasedOnSymbol, isMinting])
 
   const market = useMemo(() => {
-    // if (indexToken.symbol === 'AAVE2x') return 'AAVE / USD'
-    // if (indexToken.symbol === 'ARB2x') return 'ARB / USD'
-    // if (indexToken.symbol === 'LINK2x') return 'LINK / USD'
+    if (indexToken.symbol === 'AAVE2x') return 'AAVE / USD'
+    if (indexToken.symbol === 'ARB2x') return 'ARB / USD'
+    if (indexToken.symbol === 'LINK2x') return 'LINK / USD'
     if (indexToken.symbol === 'GOLD3x') return 'XAUT / USD'
     if (
       indexToken.symbol ===
